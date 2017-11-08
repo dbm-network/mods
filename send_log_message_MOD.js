29baa2c211c42e96ed1150de33d6d9291a8387af
Lasse Niermann <lasse.niermann@icloud.com>
2017-11-08 23:04:57 +0100

    1.6.7: Send message to log and Store Ping in min/secs
    Added send message to bot log/console and added the option to store the bots uptime in seconds and minutes instead of milliseconds. That is in Store Bot Client Info btw...

---
+++ send_log_message_MOD.js
@@ -0,0 +1,118 @@
+module.exports = {
+
+//---------------------------------------------------------------------
+// Action Name
+//
+// This is the name of the action displayed in the editor.
+//---------------------------------------------------------------------
+
+name: "Send Message to Console",
+
+//---------------------------------------------------------------------
+// Action Section
+//
+// This is the section the action will fall into.
+//---------------------------------------------------------------------
+
+section: "Mods by Lasse",
+
+//---------------------------------------------------------------------
+// Action Subtitle
+//
+// This function generates the subtitle displayed next to the name.
+//---------------------------------------------------------------------
+
+subtitle: function(data) {
+	return `${data.code}`;
+},
+
+//---------------------------------------------------------------------
+// Action Storage Function
+//
+// Stores the relevant variable info for the editor.
+//---------------------------------------------------------------------
+
+variableStorage: function(data, varType) {
+	const type = parseInt(data.storage);
+	if(type !== varType) return;
+	return ([data.code, 'Unknown Type']);
+},
+
+//---------------------------------------------------------------------
+// Action Fields
+//
+// These are the fields for the action. These fields are customized
+// by creating elements with corresponding IDs in the HTML. These
+// are also the names of the fields stored in the action's JSON data.
+//---------------------------------------------------------------------
+
+fields: ["code"],
+
+//---------------------------------------------------------------------
+// Command HTML
+//
+// This function returns a string containing the HTML used for
+// editting actions.
+//
+// The "isEvent" parameter will be true if this action is being used
+// for an event. Due to their nature, events lack certain information,
+// so edit the HTML to reflect this.
+//
+// The "data" parameter stores constants for select elements to use.
+// Each is an array: index 0 for commands, index 1 for events.
+// The names are: sendTargets, members, roles, channels,
+//                messages, servers, variables
+//---------------------------------------------------------------------
+
+html: function(isEvent, data) {
+	return `
+<div>
+	<p>
+		<u>Note:</u><br>
+		No Variables are working at the moment! I'll try to fix that!
+	</p>
+</div><br>
+<div style="padding-top: 8px;">
+	Message to send:<br>
+	<textarea id="code" rows="4" style="width: 99%; font-family: monospace; white-space: nowrap; resize: none;"></textarea>
+</div>`
+},
+
+//---------------------------------------------------------------------
+// Action Editor Init Code
+//
+// When the HTML is first applied to the action editor, this code
+// is also run. This helps add modifications or setup reactionary
+// functions for the DOM elements.
+//---------------------------------------------------------------------
+
+init: function() {},
+
+//---------------------------------------------------------------------
+// Action Bot Function
+//
+// This is the function for the action within the Bot's Action class.
+// Keep in mind event calls won't have access to the "msg" parameter,
+// so be sure to provide checks for variable existance.
+//---------------------------------------------------------------------
+
+action: function(cache) {
+	const data = cache.actions[cache.index];
+	let code;
+	code = this.evalMessage(console.log([data.code]));
+	this.callNextAction(cache);
+},
+
+//---------------------------------------------------------------------
+// Action Bot Mod
+//
+// Upon initialization of the bot, this code is run. Using the bot's
+// DBM namespace, one can add/modify existing functions if necessary.
+// In order to reduce conflictions between mods, be sure to alias
+// functions you wish to overwrite.
+//---------------------------------------------------------------------
+
+mod: function(DBM) {
+}
+
+}; // End of module
