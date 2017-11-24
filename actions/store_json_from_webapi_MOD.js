ecca792ef82b0b732c7f072c37776b7e040508c4
Generalwrex <bjdroot@gmail.com>
2017-11-24 04:15:48 -0800

    Fixed Store JSON From WebAPI from being in the wrong category.

--- actions/store_json_from_webapi_MOD.js
+++ actions/store_json_from_webapi_MOD.js
@@ -26,7 +26,7 @@ module.exports = {
 	// This is the section the action will fall into.
 	//---------------------------------------------------------------------
 	
-	section: "JSON WebAPI Parsing",
+	section: "Mods by General Wrex",
 	
 	//---------------------------------------------------------------------
     // Dependencies Section
