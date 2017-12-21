22c82454638a04bbcb3cd7774383b0dfe86d78a3
Lasse Niermann <lasse.niermann@icloud.com>
2017-12-21 18:20:54 +0100

    Don't download! - Added infofor mod manager

--- actions/store_json_from_webapi_MOD.js
+++ actions/store_json_from_webapi_MOD.js
@@ -15,6 +15,27 @@ module.exports = {
 	// in this example the variable would contain "[DEV] Version 1.2.2.3"
 	//---------------------------------------------------------------------
 
+	//---------------------------------------------------------------------
+		 // DBM Mods Manager Variables (Optional but nice to have!)
+		 //
+		 // These are variables that DBM Mods Manager uses to show information
+		 // about the mods for people to see in the list.
+		 //---------------------------------------------------------------------
+
+		 // Who made the mod (If not set, defaults to "DBM Mods")
+		 author: "General Wrex",
+
+		 // The version of the mod (Defaults to 1.0.0)
+		 version: "1.8.2",
+
+		 // A short description to show on the mod line for this mod (Must be on a single line)
+		 short_description: "Interact with APIs",
+
+		 // If it depends on any other mods by name, ex: WrexMODS if the mod uses something from WrexMods
+		 depends_on_mods: ["WrexMODS"],
+
+		 //---------------------------------------------------------------------
+
 
 
 	//---------------------------------------------------------------------
