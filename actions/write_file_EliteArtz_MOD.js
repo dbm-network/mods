d2255a838e726c4b3190f3611fe0c0351f23f053
LasseNiermann <lasse.niermann@icloud.com>
2018-02-02 01:04:16 +0100

    Added the fogotten mods

--- actions/write_file_EliteArtz_MOD.js
+++ actions/write_file_EliteArtz_MOD.js
@@ -6,7 +6,7 @@ module.exports = {
 // This is the name of the action displayed in the editor.
 //---------------------------------------------------------------------
 
-name: "Mod Information",
+name: "Write File",
 
 //---------------------------------------------------------------------
 // Action Section
@@ -14,7 +14,7 @@ name: "Mod Information",
 // This is the section the action will fall into.
 //---------------------------------------------------------------------
 
-section: "#Mod Information",
+section: "Other Stuff",
 
 //---------------------------------------------------------------------
 // Action Subtitle
@@ -23,37 +23,29 @@ section: "#Mod Information",
 //---------------------------------------------------------------------
 
 subtitle: function(data) {
-	return `Does nothing - Click "Edit" for more information`;
+	return `${data.filename}${data.format}`;
 },
 
 //---------------------------------------------------------------------
-	 // DBM Mods Manager Variables (Optional but nice to have!)
-	 //
-	 // These are variables that DBM Mods Manager uses to show information
-	 // about the mods for people to see in the list.
-	 //---------------------------------------------------------------------
+    // DBM Mods Manager Variables (Optional but nice to have!)
+    //
+    // These are variables that DBM Mods Manager uses to show information
+    // about the mods for people to see in the list.
+    //---------------------------------------------------------------------
 
-	 // Who made the mod (If not set, defaults to "DBM Mods")
-	 author: "Lasse",
+    // Who made the mod (If not set, defaults to "DBM Mods")
+    author: "EliteArtz",
 
-	 // The version of the mod (Defaults to 1.0.0)
-	 version: "1.8.4",
+    // The version of the mod (Defaults to 1.0.0)
+    version: "1.8.4",
 
-	 // A short description to show on the mod line for this mod (Must be on a single line)
-	 short_description: "Information about the Mod Collection.",
+    // A short description to show on the mod line for this mod (Must be on a single line)
+    short_description: "Creates a File with your File name and File format + including your Text you wan't to.",
 
 	 // If it depends on any other mods by name, ex: WrexMODS if the mod uses something from WrexMods
 
 
 	 //---------------------------------------------------------------------
-
-//---------------------------------------------------------------------
-// Action Storage Function
-//
-// Stores the relevant variable info for the editor.
-//---------------------------------------------------------------------
-
-//variableStorage: function(data, varType) {},
 
 //---------------------------------------------------------------------
 // Action Fields
@@ -63,47 +55,47 @@ subtitle: function(data) {
 // are also the names of the fields stored in the action's JSON data.
 //---------------------------------------------------------------------
 
-fields: [],
+fields: ["input", "format", "filename"],
 
 //---------------------------------------------------------------------
 // Command HTML
 //
 // This function returns a string containing the HTML used for
-// editting actions.
+// editting actions. 
 //
 // The "isEvent" parameter will be true if this action is being used
-// for an event. Due to their nature, events lack certain information,
+// for an event. Due to their nature, events lack certain information, 
 // so edit the HTML to reflect this.
 //
-// The "data" parameter stores constants for select elements to use.
+// The "data" parameter stores constants for select elements to use. 
 // Each is an array: index 0 for commands, index 1 for events.
-// The names are: sendTargets, members, roles, channels,
+// The names are: sendTargets, members, roles, channels, 
 //                messages, servers, variables
 //---------------------------------------------------------------------
 
 html: function(isEvent, data) {
 	return `
 <div>
-	<p>
-		<u>Welcome!</u><br>
-		Thank you for using the DBM Mod Collection!<br>
-		If you want to tell us something, join the Discord Guild below.<br>
-		And if something doesn't work feel free to create an issue on GitHub<br>
-		or open #support and describe your problem.<br>
-		<u>Discord:</u><br>
-		Join the Discord Guild to stay updated and be able to suggest things.<br>
-		<a href="https://discord.gg/Y4fPBnZ" target="_blank">Join now</a><br>
-		<u>Your version:</u><br>
-		1.8.4 ~ beta<br>
-		<u>Changelog:</u><br>
-		Click here to see the latest updates:<br>
-		<a href="https://github.com/Discord-Bot-Maker-Mods/DBM-Mods/releases" target="_blank">Open Changelog</a><br>
-		<u>GitHub:</u><br>
-		Visit us on GitHub! The whole mod collection is on GitHub<br>
-		and everyone is invited to join us developing new mods!<br>
-		Copy and paste the link to view the site in your browser.<br>
-		<a href="https://github.com/Discord-Bot-Maker-Mods/DBM-Mods/" target="_blank">https://github.com/Discord-Bot-Maker-Mods/DBM-Mods/</a><br>
-	</p>
+    <p>
+        <u>Mod Info:</u><br>
+        Made by EliteArtz<br>
+    </p>
+	<div style="float: left; width: 30%;">
+		File Format:<br>
+		<select id="format" class="round">
+			<option value=".json">json File</option>
+			<option value=".txt" selected>txt File</option>
+			<option value=".js">js File</option>
+		</select>
+	</div><br>
+    <div style="float: left; width: 99%">
+        File name:<br>
+        <textarea id="filename" class="round" style="width 50%; resize: none;" type="textarea" rows="1" cols="30"></textarea><br>
+    </div>
+	<div style="float: left; width: 99%;">
+		Input Text:<br>
+		<textarea id="input" class="round" style="width: 99%; resize: none;" type="textarea" rows="5" cols="35"></textarea><br>
+	</div>
 </div>`
 },
 
@@ -121,12 +113,26 @@ init: function() {},
 // Action Bot Function
 //
 // This is the function for the action within the Bot's Action class.
-// Keep in mind event calls won't have access to the "msg" parameter,
+// Keep in mind event calls won't have access to the "msg" parameter, 
 // so be sure to provide checks for variable existance.
 //---------------------------------------------------------------------
 
-action: function(cache) {
-	this.callNextAction(cache);
+action: function (cache) {
+    const data = cache.actions[cache.index];
+
+    try {
+        const fileNAME = this.evalMessage(data.filename, cache);
+        const fs = require('fs');
+        if (fileNAME) {
+            const inputtext = this.evalMessage(data.input, cache);
+            fs.writeFileSync(fileNAME + `${data.format}`, inputtext, console.log(`${data.filename}${data.format} File was written.`));
+        } else {
+        console.log(`File name is missing.`);
+        }
+    } catch (err) {
+        console.log("ERROR!" + err.stack ? err.stack : err);
+    }
+    this.callNextAction(cache);
 },
 
 //---------------------------------------------------------------------
