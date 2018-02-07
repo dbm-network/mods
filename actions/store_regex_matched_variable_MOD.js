1bf52093c34a9ecb417bbce8b86a0f7e467b636d
Generalwrex <bjdroot@gmail.com>
2018-02-06 22:43:21 -0800

    [WEBAPI FIXES] Merged valid-url into wrexmods, this should repair any future issues with valid-url. New Section, Variable Things: For things that modify variables! Moved Store Regex Matched Variable into variable things. WrexMODS Node Module Installer now properly installs the module

--- actions/store_regex_matched_variable_MOD.js
+++ actions/store_regex_matched_variable_MOD.js
@@ -14,7 +14,7 @@ module.exports = {
         // This is the section the action will fall into.
         //---------------------------------------------------------------------
 
-        section: "JSON Things",
+        section: "Variable Things",
 
         //---------------------------------------------------------------------
         // Action Subtitle
