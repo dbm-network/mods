527c7bac1b5ff84534a02dd472f1e13ea9fca176
Generalwrex <bjdroot@gmail.com>
2018-02-18 00:49:31 -0800

    Added: Custom Headers to Store JSON From WebAPI. New Mods: Store/Parse HTML From Webpage

---
+++ actions/store_html_from_webpage_MOD.js
@@ -0,0 +1,265 @@
+	// Created by General Wrex
+	// My Patreons have made creating this script possible @ https://www.patreon.com/generalwrex
+	// At the time of editing this script, they are:
+	// - MitchDaGamer
+	//
+	// Thanks so much guys, you allow me to continue to do what i love!
+	// Add URL to json object,
+	// add path to the object you want from the json
+	// set your variable type and name
+	// example URL: https://api.github.com/repos/HellionCommunity/HellionExtendedServer/releases/latest
+	// example Path: name
+	// will return object.name
+	// in this example the variable would contain "[DEV] Version 1.2.2.3"
+	//---------------------------------------------------------------------
+
+module.exports = {
+
+	//---------------------------------------------------------------------
+	// DBM Mods Manager Variables (Optional but nice to have!)
+	//
+	// These are variables that DBM Mods Manager uses to show information
+	// about the mods for people to see in the list.
+	//---------------------------------------------------------------------
+
+	// Who made the mod (If not set, defaults to "DBM Mods")
+	author: "General Wrex",
+
+	// The version of the mod (Defaults to 1.0.0)
+	version: "1.0.0",
+
+    // A short description to show on the mod line for this mod (Must be on a single line)
+	short_description: "Stores HTML data into a variable to be parsed.",
+
+	// If it depends on any other mods by name, ex: WrexMODS if the mod uses something from WrexMods
+	// {name:'WrexMods',path:'aaa_wrexmods_dependencies_MOD.js'}
+	depends_on_mods: [
+		{name:'WrexMODS',path:'aaa_wrexmods_dependencies_MOD.js'}
+	],
+
+	//---------------------------------------------------------------------
+	
+	//---------------------------------------------------------------------
+	// Action Name
+	//
+	// This is the name of the action displayed in the editor.
+	//---------------------------------------------------------------------
+
+	name: "Store HTML From Webpage",
+
+
+	//---------------------------------------------------------------------
+	// Action Section
+	//
+	// This is the section the action will fall into.
+	//---------------------------------------------------------------------
+
+	section: "HTML/XML Things",
+
+	//---------------------------------------------------------------------
+	// Action Subtitle
+	//
+	// This function generates the subtitle displayed next to the name.
+	//---------------------------------------------------------------------
+
+	subtitle: function(data) {
+		return `URL: ${data.url}`;
+	},
+
+	//---------------------------------------------------------------------
+	// Action Storage Function
+	//
+	// Stores the relevant variable info for the editor.
+	//---------------------------------------------------------------------
+
+	variableStorage: function(data, varType) {
+		const type = parseInt(data.storage);
+		if(type !== varType) return;
+		return ([data.varName, 'HTML Webpage']);
+	},
+
+	//---------------------------------------------------------------------
+	// Action Fields
+	//
+	// These are the fields for the action. These fields are customized
+	// by creating elements with corresponding IDs in the HTML. These
+	// are also the names of the fields stored in the action's JSON data.
+	//---------------------------------------------------------------------
+
+	fields: ["url","storage", "varName"],
+
+	//---------------------------------------------------------------------
+	// Command HTML
+	//
+	// This function returns a string containing the HTML used for
+	// editing actions.
+	//
+	// The "isEvent" parameter will be true if this action is being used
+	// for an event. Due to their nature, events lack certain information,
+	// so edit the HTML to reflect this.
+	//
+	// The "data" parameter stores constants for select elements to use.
+	// Each is an array: index 0 for commands, index 1 for events.
+	// The names are: sendTargets, members, roles, channels,
+	//                messages, servers, variables
+	//---------------------------------------------------------------------
+
+	html: function(isEvent, data) {
+		return `	
+
+		<div id ="wrexdiv" style="width: 550px; height: 350px; overflow-y: scroll;">
+		<div>
+			<p>
+				<u>Mod Info:</u><br>
+				Created by General Wrex!<br>
+				This can also be used to save any website data (except images and files) to a variable.
+			</p>
+			<p>
+				<u>Instructions:</u><br>
+					1. Input a url into the Webpage URL textarea<br>
+					2. Click valid URL to check if the url is valid!
+			</p>
+		</div>	
+		<div style="float: left; width: 95%;">
+			Webpage URL: <br>
+			<textarea id="url" class="round" style="width: 99%; resize: none;" type="textarea" rows="4" cols="20"></textarea><br>    
+		</div>	
+	  <div>
+		<button class="tiny compact ui labeled icon button" onclick="glob.checkURL(this)"><i class="plus icon"></i>Check URL</button><br>
+		Valid: <text id="valid" style="color: red">Input A Url</text>
+	  </div><br>
+		<div style="float: left; width: 35%;">
+			Store In:<br>
+			<select id="storage" class="round" onchange="glob.variableChange(this, 'varNameContainer')">
+				${data.variables[0]}
+			</select>
+		</div>	
+		<div id="varNameContainer" style="display: ; float: right; width: 60%;">
+			Storage Variable Name:<br>
+			<input id="varName" class="round" type="text">
+		</div>	
+	</div>	
+	`
+	},
+
+	//---------------------------------------------------------------------
+	// Action Editor Init Code
+	//
+	// When the HTML is first applied to the action editor, this code
+	// is also run. This helps add modifications or setup reactionary
+	// functions for the DOM elements.
+	//---------------------------------------------------------------------
+
+	init: function() {
+		const {glob, document} = this;
+		
+	
+			function evalMessage(content) {
+				if(!content) return '';
+				if(!content.match(/\$\{.*\}/im)) return content;
+				return content.replace(/\$\{.*\}/im,"CUSTOMVALUE");
+			}
+	
+			try {
+	
+				var WrexMODS = require(require("path").join(require("path").resolve("Actions"), 'aaa_wrexmods_dependencies_MOD.js')).getWrexMODS();
+							
+				var valid = document.getElementById("valid");
+				var url = document.getElementById("url");
+	
+				glob.checkURL = function(element){
+	
+					const pUrl = url.value;
+	
+					const checkedUrl = WrexMODS.checkURL(encodeURI(evalMessage(pUrl)));
+					
+					if(checkedUrl && pUrl){
+						valid.innerHTML = "Valid URL Format!";
+						valid.style.color = "green";
+					}else{
+						valid.innerHTML = "Invalid URL Format!";
+						valid.style.color = "red";
+					}
+					
+				}
+	
+																												
+			} catch (error) {
+				// write any init errors to errors.txt in dbm's main directory
+				require("fs").appendFile("errors.txt", error.stack ? error.stack : error + "\r\n"); 
+			}
+	
+			glob.variableChange(document.getElementById('storage'), 'varNameContainer');
+
+	},
+
+	//---------------------------------------------------------------------
+	// Action Bot Function
+	//
+	// This is the function for the action within the Bot's Action class.
+	// Keep in mind event calls won't have access to the "msg" parameter,
+	// so be sure to provide checks for variable existance.
+	//---------------------------------------------------------------------
+
+	action: function(cache) {
+
+		try {
+
+			var WrexMODS = this.getWrexMods();
+
+			const data = cache.actions[cache.index];
+
+			const varName = this.evalMessage(data.varName, cache);
+			const storage = parseInt(data.storage);
+
+			var url = this.evalMessage(data.url, cache);
+
+
+
+			if(!WrexMODS.checkURL(url)){
+				url = encodeURI(url);
+			}
+
+
+			if(WrexMODS.checkURL(url)){
+			
+			    // making sure all the required node modules are installed
+				var xpath = WrexMODS.require('xpath')
+				, dom = WrexMODS.require('xmldom').DOMParser
+				, request = WrexMODS.require('request')
+				
+							
+				request(url, function(err, res, html) { 	
+					
+					if(err) throw err;
+					
+					this.storeValue(html.trim(), storage, varName, cache)	;	
+					this.callNextAction(cache);	 	
+
+				}.bind(this));
+							
+			}else{
+				throw Error('HTML Parser - URL ['+url+'] Is Not Valid');
+			}
+
+		} catch (error) {
+			console.error("API Things:  Error: " + error.stack ? error.stack : error);
+		}
+		
+	},
+
+	//---------------------------------------------------------------------
+	// Action Bot Mod
+	//
+	// Upon initialization of the bot, this code is run. Using the bot's
+	// DBM namespace, one can add/modify existing functions if necessary.
+	// In order to reduce conflictions between mods, be sure to alias
+	// functions you wish to overwrite.
+	//---------------------------------------------------------------------
+
+	mod: function(DBM) {
+
+
+	 }
+
+}; // End of module
--- actions/store_json_from_webapi_MOD.js
+++ actions/store_json_from_webapi_MOD.js
@@ -27,7 +27,7 @@ module.exports = {
 	author: "General Wrex",
 
 	// The version of the mod (Defaults to 1.0.0)
-	version: "1.0.2",
+	version: "1.0.3",
 
     // A short description to show on the mod line for this mod (Must be on a single line)
 	short_description: "Stores JSON from a webapi into a variable.",
@@ -36,9 +36,6 @@ module.exports = {
 	depends_on_mods: ["WrexMODS"],
 
 	//---------------------------------------------------------------------
-
-
-
 
 	//---------------------------------------------------------------------
 	// Action Name
@@ -87,7 +84,7 @@ module.exports = {
 	// are also the names of the fields stored in the action's JSON data.
 	//---------------------------------------------------------------------
 
-	fields: ["behavior", "token", "user", "pass" ,"url", "path", "storage", "varName"],
+	fields: ["token", "user", "pass" ,"url", "path", "storage", "varName", "debugMode", "showAuth", "headers"],
 
 	//---------------------------------------------------------------------
 	// Command HTML
@@ -109,48 +106,65 @@ module.exports = {
 		return `
 		<div id ="wrexdiv" style="width: 550px; height: 350px; overflow-y: scroll;">
 		<div>
-			<p>
-				<u>Mod Info:</u><br>
-				Created by General Wrex!
-			</p>
+		   <p>
+			  <u>Mod Info:</u><br>
+			  Created by General Wrex!
+		   </p>
 		</div>
-	<div>
-	<div style="float: left; width: 95%;">
-	<div>
-		End Behavior:<br>
-		<select id="behavior" class="round">
-			<option value="0" selected>Call Next Action Automatically</option>
-			<option value="1">Do Not Call Next Action</option>
-		</select>
-	<div><br><br>
-    WebAPI URL: <br>
-		<textarea id="url" class="round" style="width: 99%; resize: none;" type="textarea" rows="4" cols="20"></textarea><br>
-    Bearer Token ( If a bearer token is required, put it here!)<br>
-		<textarea id="token" class="round" placeholder="blank if none"   style="width: 99%; resize: none;" type="textarea" rows="4" cols="20"></textarea><br><br>
-    Username( If a password is required, put it here!)<br>
-		<input id="user" class="round"  placeholder="blank if none" style="width: 99%; resize: none;" ><br>
-		Password ( If a password is required, put it here!)<br>
-		<input id="pass" class="round"  placeholder="blank if none"  style="width: 99%; resize: none;" ><br>
-	</div>
-	</div><br>
-		Initial JSON Path: (Leave blank to store everything, supports the usage of <a href="http://goessner.net/articles/JsonPath/index.html#e2" target="_blank">JSON Path (Regex)</a>)<br>
-		<input id="path" class="round"; style="width: 75%;" type="text">
-	<div><br><br>
-	<div style="float: left; width: 35%;">
-		Store In:<br>
-		<select id="storage" class="round" onchange="glob.variableChange(this, 'varNameContainer')">
-			${data.variables[0]}
-		</select>
-	</div>
-	<div id="varNameContainer" style="display: ; float: right; width: 60%;">
-		JSON Storage Variable Name:<br>
-		<input id="varName" class="round" type="text">
-	</div>
-	</div>
-</div>
-<!-- Remember to copy back the provided div here with your html to add the scrollbar-->
-
-`
+		<div style="float: left; width: 95%;">
+		   WebAPI URL: <br>
+		   <textarea id="url" class="round" style="width: 99%; resize: none;" type="textarea" rows="4" cols="20"></textarea>
+		   <br>
+		   Headers (By default 'User-Agent: Other' is applied, It's 1 per line, (<b>key:value</b>))<br>
+		   <textarea id="headers" class="round" placeholder="User-Agent: Other" style="width: 99%; resize: none;" type="textarea" rows="4" cols="20"></textarea><br>  
+		</div>
+		<div class="ui toggle checkbox" >
+		   <input type="checkbox" name="public" id="toggleAuth" onclick="glob.checkBox(this, 'auth');">
+		   <label><font color="white">Show Authentication Options</font></label>		
+		   <text style="font-size: 60%;">Show/Hide Auth Options</text>
+		</div>
+		<div id="authSection" style="display: ;">
+		   <br>Bearer Token<br>
+		   <textarea id="token" class="round" placeholder="blank if none" style="width: 99%; resize: none;" type="textarea" rows="4" cols="20">      
+		   </textarea><br>
+		   Username<br>
+		   <input id="user" class="round"  placeholder="blank if none" style="width: 99%; resize: none;" ><br>
+		   Password <br>
+		   <input id="pass" class="round"  placeholder="blank if none"  style="width: 99%; resize: none;" ><br>    
+		</div>
+	 
+	 <br><br>
+	 Initial JSON Path: (Leave blank to store everything)<br> 
+	 Supports the usage of JSON Path (Regex)<br> 
+	 More info here <br>
+	 http://goessner.net/articles/JsonPath/index.html#e2<br><br>
+	 <input id="path" class="round"; style="width: 75%;" type="text">
+	 <div>
+		<br><br>
+		<div style="float: left; width: 35%;">
+		   Store In:<br>
+		   <select id="storage" class="round" onchange="glob.variableChange(this, 'varNameContainer')">
+		   ${data.variables[0]}
+		   </select><br>
+		</div>
+		<div id="varNameContainer" style="display: ; float: right; width: 60%;">
+		   JSON Storage Variable Name:<br>
+		   <input id="varName" class="round" type="text"><br>
+		</div>
+	 </div>
+	 <br>
+	 <div style="float: left; width: 70%;">
+		<br>   
+		<div class="ui toggle checkbox">
+		   <input type="checkbox" id="debugModeBox" onclick="glob.checkBox(this, 'debug');">
+		   <label><font color="white">Debug Mode</font></label>		
+		   <text style="font-size: 60%;">Enables verbose printing to the console, disable to stop all but error printing</text>
+		</div>
+	 </div>
+	 <input type="hidden" id="debugMode" name="debugMode">
+	 <input type="hidden" id="showAuth" name="showAuth">
+	 </div>
+	 `
 	},
 
 	//---------------------------------------------------------------------
@@ -163,7 +177,28 @@ module.exports = {
 
 	init: function() {
 		const {glob, document} = this;
+
 		glob.variableChange(document.getElementById('storage'), 'varNameContainer');
+		
+		glob.checkBox = function(element, type){ 
+			if(type === "debug") { 
+			  document.getElementById("debugMode").value = element.checked ? "1" : "0"; 
+			}
+			if(type === "auth"){
+				document.getElementById("authSection").style.display = element.checked  ? "" : "none";
+				document.getElementById("showAuth").value = element.checked ? "1" : "0"; 
+			}
+		};
+
+		// idk why but need a timeout with checkboxes or it wont set when the ui loads, the on ready event doesn't work
+		setTimeout(() => {
+			glob.checkBox(document.getElementById("toggleAuth"), "auth");
+			glob.checkBox(document.getElementById("debugModeBox"), "debug");
+
+			document.getElementById("debugModeBox").checked = document.getElementById("debugMode").value == "1" ? true : false;
+			document.getElementById("toggleAuth").checked = document.getElementById("showAuth").value == "1" ? true : false;
+		}, 500); 
+			
 	},
 
 	//---------------------------------------------------------------------
@@ -175,48 +210,89 @@ module.exports = {
 	//---------------------------------------------------------------------
 
 	action: function(cache) {
-		var _this = this;
+		const data = cache.actions[cache.index];
 
 		var WrexMODS = this.getWrexMods();
+		var request = WrexMODS.require("request");
 
-		const data = cache.actions[cache.index];
+		const _DEBUG = parseInt(data.debugMode);
+
+		const storage = parseInt(data.storage);
 		const varName = this.evalMessage(data.varName, cache);
+
+		var url = this.evalMessage(data.url, cache);
+		var path = this.evalMessage(data.path, cache);
 
 		const token = this.evalMessage(data.token, cache);
 		const user = this.evalMessage(data.user, cache);
 		const pass = this.evalMessage(data.pass, cache);
 
-		const storage = parseInt(data.storage);
-		var url = this.evalMessage(data.url, cache);
-		const path = this.evalMessage(data.path, cache);
 
+		let headers = this.evalMessage(data.headers, cache);
+
+		let setHeaders = {};
+
+		// set default required header
+		setHeaders["User-Agent"] = "Other";
+
+		// Because headers are a dictionary ;)
+		if(headers){
+			var lines = String(headers).split('\n');
+			for(var i = 0;i < lines.length;i++){
+				var header = lines[i].split(':');
+
+				if(lines[i].includes(':') && header.length > 0){
+					var key = header[0] || "Unknown";
+					var value = header[1] || "Unknown";
+					setHeaders[key] = value;
+
+					if(_DEBUG) console.log("Applied Header: " + lines[i])
+				}else{
+					console.error(`WebAPI: Error: Custom Header line ${lines[i]} is wrongly formatted. You must split the key from the value with a colon (:)`);
+				}				
+			}
+		}
+				
+		// if it fails the check, try to re-encode the url
 		if(!WrexMODS.checkURL(url)){
 			url = encodeURI(url);
-		};
+		}
 
 		if(WrexMODS.checkURL(url)){
 
-			WrexMODS.runPublicRequest(url, true, function(error, statusCode, jsonData){
-
-				try {
-
+			try {
+			   
+				request.get({
+					url: url,
+					json: true,
+					headers: setHeaders,
+					auth: {
+						bearer: token,
+						user: user,
+						pass: pass,
+						sendImmediately: false
+					},
+				}, function(error, res, jsonData) {    
+			
+					var statusCode = res ? res.statusCode : 200;
+							
 					if(error){
 						var errorJson = JSON.stringify({error, statusCode})
-						_this.storeValue(errorJson, storage, varName, cache);
+						this.storeValue(errorJson, storage, varName, cache);
 
 						console.error("WebAPI: Error: " + errorJson + " stored to: ["+ varName+"]");
-					}
-					else{
+					}else{
+
 						if(path){
 							var outData = WrexMODS.jsonPath(jsonData, path);
 
-							console.log(outData);
+							if(_DEBUG) console.dir(outData);
 
 							try {
 								var test = JSON.parse(JSON.stringify(outData));
 							} catch (error) {
 								var errorJson = JSON.stringify({error: error, statusCode: statusCode, success: false})
-								_this.storeValue(errorJson, storage, varName, cache);
+								this.storeValue(errorJson, storage, varName, cache);
 								console.error(error.stack ? error.stack : error);
 							}
 
@@ -224,35 +300,31 @@ module.exports = {
 
 							if(outData.success != null){
 								var errorJson = JSON.stringify({error: error, statusCode: statusCode, success: false})
-								_this.storeValue(errorJson, storage, varName, cache);
+								this.storeValue(errorJson, storage, varName, cache);
 								console.log("WebAPI: Error Invalid JSON, is the Path set correctly? [" + path + "]");
 							}else{
 								if(outValue.success != null || !outValue){
 									var errorJson = JSON.stringify({error: error, statusCode: statusCode, success: false})
-									_this.storeValue(errorJson, storage, varName, cache);
+									this.storeValue(errorJson, storage, varName, cache);
 									console.log("WebAPI: Error Invalid JSON, is the Path set correctly? [" + path + "]");
 								}else{
-									_this.storeValue(outValue, storage, varName, cache);
-									console.log("WebAPI: JSON Data values starting from ["+ path +"] stored to: ["+ varName+"]");
+									this.storeValue(outValue, storage, varName, cache);
+									if(_DEBUG) console.log("WebAPI: JSON Data values starting from ["+ path +"] stored to: ["+ varName+"]");
 								}
-
 							}
 
 						}else{
-							_this.storeValue(jsonData, storage, varName, cache);
-							console.log("WebAPI: JSON Data Object stored to: ["+ varName+"]");
+							if(_DEBUG) console.dir(jsonData);
+							this.storeValue(jsonData, storage, varName, cache);
+							if(_DEBUG) console.log("WebAPI: JSON Data Object stored to: ["+ varName+"]");
 						}
 					}
-
-					if(data.behavior === "0") {
-						_this.callNextAction(cache);
-					}
-				} catch (err) {
-					console.error(err.stack ? err.stack : err);
-				}
-
-
-			}, token, user, pass);
+					this.callNextAction(cache);
+				}.bind(this));	
+				
+			} catch (err) {
+				console.error(err.stack ? err.stack : err);
+			}
 		}
 		else{
 			console.error('URL ['+url+'] Is Not Valid');
