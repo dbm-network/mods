9d835017cb10f15578a99fec4213fcf7ddcb311f
EGGSY <35194040+eggsywashere@users.noreply.github.com>
2018-03-07 22:15:31 +0300

    Added "Node JS Version"
    Lol

--- actions/store_bot_client_info_MOD.js
+++ actions/store_bot_client_info_MOD.js
@@ -22,31 +22,31 @@ module.exports = {
 	// This function generates the subtitle displayed next to the name.
 	//---------------------------------------------------------------------
 
-	subtitle: function(data) {
-	const info = ['Uptime in Milliseconds', 'Ready At?', 'Ping', 'Guild Amount', 'User Amount', 'Rounded Ping', 'Uptime in Seconds', 'Uptime in Minutes', 'Bot\'s Token', 'Voice Connections Amount', 'Total Amount of Channels', 'Total Amount of Emojis', 'Bot\'s Previous Pings', 'Uptime in Days', 'Uptime in Days (Rounded)', 'Memory (RAM) Usage', 'Bot Guilds Objects', 'Bot Guilds Names', 'Bot Guilds IDs', 'Bot Current Prefix', 'Bot Client ID', 'Discord JS Version', 'Uptime in Hours', 'Refreshing Uptime in Days', 'Refreshing Uptime in Hours', 'Refreshing Uptime in Minutes', 'Refreshing Uptime in Seconds', 'Memory (RAM) Usage in MB', 'Bot\'s OS (Process Platform)', 'CPU Usage in MB', 'Bot\'s Directory'];
+	subtitle: function (data) {
+		const info = ['Uptime in Milliseconds', 'Ready At?', 'Ping', 'Guild Amount', 'User Amount', 'Rounded Ping', 'Uptime in Seconds', 'Uptime in Minutes', 'Bot\'s Token', 'Voice Connections Amount', 'Total Amount of Channels', 'Total Amount of Emojis', 'Bot\'s Previous Pings', 'Uptime in Days', 'Uptime in Days (Rounded)', 'Memory (RAM) Usage', 'Bot Guilds Objects', 'Bot Guilds Names', 'Bot Guilds IDs', 'Bot Current Prefix', 'Bot Client ID', 'Discord JS Version', 'Uptime in Hours', 'Refreshing Uptime in Days', 'Refreshing Uptime in Hours', 'Refreshing Uptime in Minutes', 'Refreshing Uptime in Seconds', 'Memory (RAM) Usage in MB', 'Bot\'s OS (Process Platform)', 'CPU Usage in MB', 'Bot\'s Directory', 'Node JS Version'];
 		return `Bot Client - ${info[parseInt(data.info)]}`;
 	},
 
 	//---------------------------------------------------------------------
-		 // DBM Mods Manager Variables (Optional but nice to have!)
-		 //
-		 // These are variables that DBM Mods Manager uses to show information
-		 // about the mods for people to see in the list.
-		 //---------------------------------------------------------------------
+	// DBM Mods Manager Variables (Optional but nice to have!)
+	//
+	// These are variables that DBM Mods Manager uses to show information
+	// about the mods for people to see in the list.
+	//---------------------------------------------------------------------
 
-		 // Who made the mod (If not set, defaults to "DBM Mods")
-		 author: "Lasse, EliteArtz and EGGSY",
+	// Who made the mod (If not set, defaults to "DBM Mods")
+	author: "Lasse, EliteArtz and EGGSY",
 
-		 // The version of the mod (Defaults to 1.0.0)
-		 version: "1.8.5 - Hotfix!",
+	// The version of the mod (Defaults to 1.0.0)
+	version: "1.8.6",
 
-		 // A short description to show on the mod line for this mod (Must be on a single line)
-		 short_description: "Stores Bot Information like Ping, Total Members or Guilds...",
+	// A short description to show on the mod line for this mod (Must be on a single line)
+	short_description: "Stores Bot Information like Ping, Total Members or Guilds...",
 
-		 // If it depends on any other mods by name, ex: WrexMODS if the mod uses something from WrexMods
+	// If it depends on any other mods by name, ex: WrexMODS if the mod uses something from WrexMods
 
 
-		 //---------------------------------------------------------------------
+	//---------------------------------------------------------------------
 
 	//---------------------------------------------------------------------
 	// Action Storage Function
@@ -54,12 +54,12 @@ module.exports = {
 	// Stores the relevant variable info for the editor.
 	//---------------------------------------------------------------------
 
-	variableStorage: function(data, varType) {
+	variableStorage: function (data, varType) {
 		const type = parseInt(data.storage);
-		if(type !== varType) return;
+		if (type !== varType) return;
 		const info = parseInt(data.info);
 		let dataType = 'Unknown Type';
-		switch(info) {
+		switch (info) {
 			case 0:
 				dataType = "Number";
 				break;
@@ -153,6 +153,9 @@ module.exports = {
 			case 30:
 				dataType = "Directory";
 				break;
+			case 31:
+				dataType = "Version Number";
+				break;
 		}
 		return ([data.varName2, dataType]);
 	},
@@ -183,7 +186,7 @@ module.exports = {
 	//                messages, servers, variables
 	//---------------------------------------------------------------------
 
-	html: function(isEvent, data) {
+	html: function (isEvent, data) {
 		return `
 		<div>
 			<p>
@@ -219,6 +222,7 @@ module.exports = {
 			<option value="28">Bot OS (Process Platform)</option>
 			<option value="30">Bot Directory</option>
 			<option value="8">Bot Token</option>
+			<option value="31">Node JS Version</option>
 		</select>
 	</div>
 	<div>
@@ -243,8 +247,7 @@ module.exports = {
 	// functions for the DOM elements.
 	//---------------------------------------------------------------------
 
-	init: function() {
-	},
+	init: function () {},
 
 	//---------------------------------------------------------------------
 	// Action Bot Function
@@ -254,19 +257,19 @@ module.exports = {
 	// so be sure to provide checks for variable existance.
 	//---------------------------------------------------------------------
 
-	action: function(cache) {
+	action: function (cache) {
 		const botClient = this.getDBM().Bot.bot;
 		const dibiem = this.getDBM(); //EliteArtz... really???? Ugh you guys are meme
 		const data = cache.actions[cache.index];
 		const info = parseInt(data.info);
-		const msToDay = (1000*60*60*24); // Really? Lasse? Did you really forget this? - :blobshh:
+		const msToDay = (1000 * 60 * 60 * 24); // Really? Lasse? Did you really forget this? - :blobshh:
 		const usedMEMORY = process.memoryUsage().heapUsed / 1024 / 1024; // Sorry about all const things but this is needed, you know.
 		const usedCPU = process.cpuUsage().user / 1024 / 1024;
-		if(!botClient) {
+		if (!botClient) {
 			this.callNextAction(cache);
 			return;
 		}
-		switch(info) {
+		switch (info) {
 			case 0:
 				result = botClient.uptime; //Deprecated in 1.8.5
 				break;
@@ -286,10 +289,10 @@ module.exports = {
 				result = Math.round(botClient.ping);
 				break;
 			case 6:
-				result = Math.floor(botClient.uptime/1000); //Deprecated in 1.8.5
+				result = Math.floor(botClient.uptime / 1000); //Deprecated in 1.8.5
 				break;
 			case 7:
-				result = Math.floor(botClient.uptime/1000/60); //Deprecated in 1.8.5
+				result = Math.floor(botClient.uptime / 1000 / 60); //Deprecated in 1.8.5
 				break;
 			case 8:
 				result = botClient.token;
@@ -307,10 +310,10 @@ module.exports = {
 				result = botClient.pings;
 				break;
 			case 13:
-				result = botClient.uptime/msToDay; //Deprecated in 1.8.5
+				result = botClient.uptime / msToDay; //Deprecated in 1.8.5
 				break;
 			case 14:
-				result = Math.floor(botClient.uptime/msToDay); //Deprecated in 1.8.5
+				result = Math.floor(botClient.uptime / msToDay); //Deprecated in 1.8.5
 				break;
 			case 15:
 				result = ((process.memoryUsage().heapUsed / 1024) / 1024).toFixed(2) + "%";
@@ -334,7 +337,7 @@ module.exports = {
 				result = dibiem.DiscordJS.version;
 				break;
 			case 22:
-				result = Math.floor(botClient.uptime/1000/60/60); //Deprecated in 1.8.5
+				result = Math.floor(botClient.uptime / 1000 / 60 / 60); //Deprecated in 1.8.5
 				break;
 			case 23:
 				result = Math.floor((process.uptime() % 31536000) / 86400);
@@ -352,15 +355,15 @@ module.exports = {
 				result = Math.round((usedMEMORY * 100) / 100) + " MB";
 				break;
 			case 28:
-				if(process.platform) {
+				if (process.platform) {
 					const platform = process.platform;
-					if(platform === 'win32') result = 'Windows';
-					else if(platform === 'aix') result = 'Aix';
-					else if(platform === 'linux') result = 'Linux';
-					else if(platform === 'darwin') result = 'Darwin';
-					else if(platform === 'openbsd') result = 'OpenBSD';
-					else if(platform === 'sunos') result = 'Solaris';
-					else if(platform === 'freebsd') result = 'FreeBSD';
+					if (platform === 'win32') result = 'Windows';
+					else if (platform === 'aix') result = 'Aix';
+					else if (platform === 'linux') result = 'Linux';
+					else if (platform === 'darwin') result = 'Darwin';
+					else if (platform === 'openbsd') result = 'OpenBSD';
+					else if (platform === 'sunos') result = 'Solaris';
+					else if (platform === 'freebsd') result = 'FreeBSD';
 				}
 				break;
 			case 29:
@@ -369,10 +372,13 @@ module.exports = {
 			case 30:
 				result = process.cwd();
 				break;
+			case 31:
+				result = process.versions.node;
+				break;
 			default:
-			break;
+				break;
 		}
-		if(result !== undefined) {
+		if (result !== undefined) {
 			const storage = parseInt(data.storage);
 			const varName2 = this.evalMessage(data.varName2, cache);
 			this.storeValue(result, storage, varName2, cache);
@@ -389,7 +395,6 @@ module.exports = {
 	// functions you wish to overwrite.
 	//---------------------------------------------------------------------
 
-	mod: function(DBM) {
-	}
+	mod: function (DBM) {}
 
-	}; // End of module
+}; // End of module
