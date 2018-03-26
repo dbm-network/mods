dfa5e62551fad15e27f8f022f7bb1e9c63928005
EGGSY <35194040+eggsywashere@users.noreply.github.com>
2018-03-26 20:27:14 +0300

    Changed mod's name to "YouTube Search" in DBM.
    Sorry for this.

--- actions/youtube_search_MOD.js
+++ actions/youtube_search_MOD.js
@@ -6,7 +6,7 @@ module.exports = {
 	// This is the name of the action displayed in the editor.
 	//---------------------------------------------------------------------
 
-	name: "YouTube",
+	name: "YouTube Search",
 
 	//---------------------------------------------------------------------
 	// Action Section
