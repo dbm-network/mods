abb779f910203747111d24fc5b31ac08df783cac
LasseNiermann <lasse.niermann@icloud.com>
2018-09-29 01:13:35 +0200

    Beautified and added mod installer info to Set Bot Activity

--- actions/set_bot_activity_MOD.js
+++ actions/set_bot_activity_MOD.js
@@ -16,6 +16,26 @@ module.exports = {
   section: "Bot Client Control",
 
   //---------------------------------------------------------------------
+	// DBM Mods Manager Variables (Optional but nice to have!)
+	//
+	// These are variables that DBM Mods Manager uses to show information
+	// about the mods for people to see in the list.
+	//---------------------------------------------------------------------
+
+	// Who made the mod (If not set, defaults to "DBM Mods")
+	author: "Lasse",
+
+	// The version of the mod (Defaults to 1.0.0)
+	version: "1.9", //Added in 1.8.9
+
+	// A short description to show on the mod line for this mod (Must be on a single line)
+	short_description: "Set activity and status for our bot!!",
+
+  // If it depends on any other mods by name, ex: WrexMODS if the mod uses something from WrexMods
+  
+
+
+  //---------------------------------------------------------------------
   // Action Subtitle
   //
   // This function generates the subtitle displayed next to the name.
@@ -245,8 +265,6 @@ module.exports = {
           };
         }
       }
-
-      console.log(obj);
 
       botClient
         .setPresence(obj)
