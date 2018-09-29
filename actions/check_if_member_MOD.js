da9c7e461818631a37ea9c0a3546e20f8e701557
MrGold <aestheticsserverbot@gmail.com>
2018-09-29 12:40:17 +0100

    Check If Member updated

--- actions/check_if_member_MOD.js
+++ actions/check_if_member_MOD.js
@@ -24,10 +24,10 @@ section: "Conditions",
 //---------------------------------------------------------------------
 
 // Who made the mod (If not set, defaults to "DBM Mods")
-author: "Lasse",
+author: "Lasse & MrGold",
 
 // The version of the mod (Defaults to 1.0.0)
-version: "1.8.8", //Added in 1.8.8
+version: "1.X.X", //Added in 1.8.8
 
 // A short description to show on the mod line for this mod (Must be on a single line)
 short_description: "Check if a member meets the conditions.",
@@ -87,10 +87,12 @@ html: function(isEvent, data) {
 	<div style="float: left; width: 35%;">
 		Check if Member:<br>
 		<select id="info" class="round">
-			<option value="0" selected>Is Bot</option>
-			<option value="2">Is Kickable</option>
-			<option value="1">Is Bannable</option>
-			<option value="4">Is In Voice Channel</option>
+			<option value="0" selected>Is Bot?</option>
+			<option value="2">Is Kickable?</option>
+			<option value="1">Is Bannable?</option>
+			<option value="4">Is In Voice Channel?</option>
+			<option value="5">Is User Manageable?</option>
+			<option value="6">Is Bot Owner?</option>
 		</select>
 	</div>
 	<div id="varNameContainer2" style="display: none; float: right; width: 60%;">
@@ -137,6 +139,8 @@ action: function(cache) {
 	const type2 = parseInt(data.role);
 	const varName2 = this.evalMessage(data.varName2, cache);
 	const info = parseInt(data.info);
+	
+	const dibiem = this.getDBM();
 
 	let result = false;
 	switch(info) {
@@ -154,6 +158,16 @@ action: function(cache) {
 		// 	break; //Do not ask me why this is not working... ~Lasse
 		case 4:
 			if(member.voiceChannelID !== undefined) {
+				result = true;
+			} else {
+				result = false;
+			}
+			break;
+		case 5:
+			result = member.manageable;
+			break;
+		case 6:
+			if(member.id == dibiem.Files.data.settings.ownerId) {
 				result = true;
 			} else {
 				result = false;
