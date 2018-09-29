7884a0c95e2f5aa6fcd3e88891cfb82cfd823fa1
Lasse Niermann <lasse.niermann@icloud.com>
2018-09-29 22:20:04 +0200

    Update find_invite_MOD.js

--- actions/find_invite_MOD.js
+++ actions/find_invite_MOD.js
@@ -37,7 +37,7 @@ subtitle: function(data) {
 author: "MrGold",
 
 // The version of the mod (Defaults to 1.0.0)
-version: "1.X.X", //Added in 1.X.X
+version: "1.9", //Added in 1.9
 
 // A short description to show on the mod line for this mod (Must be on a single line)
 short_description: "Finds an invite by code or URL",
