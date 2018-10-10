0406fe0055249443125d47b2b0b7a5b98cab1a61
Lasse Niermann <lasse.niermann@icloud.com>
2018-10-10 17:08:13 +0200

    Update get_lyrics_MOD.js

--- actions/get_lyrics_MOD.js
+++ actions/get_lyrics_MOD.js
@@ -38,7 +38,7 @@ module.exports = {
 	author: "NetLuis",
 
 	// The version of the mod (Defaults to 1.0.0)
-	version: "1.9.1", //Added in 1.8.7
+	version: "1.9.1", //Added in 1.9.1
 
 	// A short description to show on the mod line for this mod (Must be on a single line)
 	short_description:  "Gets lyrics of any song.",
