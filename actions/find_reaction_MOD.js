ac43749e8911611eb8d2d706cb53ec4093b82c5c
MrGold <42754748+MrG0ld@users.noreply.github.com>
2018-10-11 20:55:33 +0100

    Reaction Modpack
    Made by MrGold
    
    This ModPack includes:
    Find Reaction
    Delete Reaction
    Check If User Reacted
    Store Reaction Info updated (Created by Lasse and updated by MrGold)

--- actions/find_reaction_MOD.js
+++ actions/find_reaction_MOD.js
@@ -6,7 +6,7 @@ module.exports = {
 // This is the name of the action displayed in the editor.
 //---------------------------------------------------------------------
 
-name: "Store Reaction Info",
+name: "Find Reaction",
 
 //---------------------------------------------------------------------
 // Action Section
@@ -23,32 +23,29 @@ section: "Reaction Control",
 //---------------------------------------------------------------------
 
 subtitle: function(data) {
-	const reaction = ['You cheater!', 'Temp Variable', 'Server Variable', 'Global Variable'];
-	const info = ['Message Object', 'Bot reacted?', 'User Who Reacted List', 'Emoji Name', 'Reaction Count', 'First User to React', 'Random User to React', 'Last User to React'];
-	return `${reaction[parseInt(data.reaction)]} - ${info[parseInt(data.info)]}`;
+	return `${data.find}`;
 },
 
 //---------------------------------------------------------------------
-// DBM Mods Manager Variables (Optional but nice to have!)
-//
-// These are variables that DBM Mods Manager uses to show information
-// about the mods for people to see in the list.
-//---------------------------------------------------------------------
+	 // DBM Mods Manager Variables (Optional but nice to have!)
+	 //
+	 // These are variables that DBM Mods Manager uses to show information
+	 // about the mods for people to see in the list.
+	 //---------------------------------------------------------------------
 
-// Who made the mod (If not set, defaults to "DBM Mods")
-author: "Lasse & MrGold",
+	 // Who made the mod (If not set, defaults to "DBM Mods")
+	 author: "MrGold",
 
-// The version of the mod (Defaults to 1.0.0)
-version: "1.9.1", //Added in 1.8.8
+	 // The version of the mod (Defaults to 1.0.0)
+	 version: "1.9.1", //Added in 1.9.1
 
-// A short description to show on the mod line for this mod (Must be on a single line)
-short_description: "Stores Messages Reaction information",
+	 // A short description to show on the mod line for this mod (Must be on a single line)
+	 short_description: "Finds a reaction",
 
-// If it depends on any other mods by name, ex: WrexMODS if the mod uses something from WrexMods
-depends_on_mods: [
-{name:'WrexMods',path:'aaa_wrexmods_dependencies_MOD.js'}
-],
+	 // If it depends on any other mods by name, ex: WrexMODS if the mod uses something from WrexMods
 
+
+	 //---------------------------------------------------------------------
 
 //---------------------------------------------------------------------
 // Action Storage Function
@@ -59,35 +56,7 @@ depends_on_mods: [
 variableStorage: function(data, varType) {
 	const type = parseInt(data.storage);
 	if(type !== varType) return;
-	const info = parseInt(data.info);
-	let dataType = 'Unknown Type';
-	switch(info) {
-		case 0:
-			dataType = "Message";
-			break;
-		case 1:
-			dataType = "Boolean";
-			break;
-		case 2:
-			dataType = "List";
-			break;
-		case 3:
-			dataType = "String";
-			break;
-		case 4:
-			dataType = "Number";
-			break;
-		case 5:
-			dataType = "User";
-			break;
-		case 6:
-			dataType = "User";
-			break;
-		case 7:
-			dataType = "User";
-			break;
-	}
-	return ([data.varName2, dataType]);
+	return ([data.varName2, 'Reaction']);
 },
 
 //---------------------------------------------------------------------
@@ -98,21 +67,21 @@ variableStorage: function(data, varType) {
 // are also the names of the fields stored in the action's JSON data.
 //---------------------------------------------------------------------
 
-fields: ["reaction", "varName", "info", "storage", "varName2"],
+fields: ["message", "varName", "info", "find", "storage", "varName2"],
 
 //---------------------------------------------------------------------
 // Command HTML
 //
 // This function returns a string containing the HTML used for
-// editting actions.
+// editting actions. 
 //
 // The "isEvent" parameter will be true if this action is being used
-// for an event. Due to their nature, events lack certain information,
+// for an event. Due to their nature, events lack certain information, 
 // so edit the HTML to reflect this.
 //
-// The "data" parameter stores constants for select elements to use.
+// The "data" parameter stores constants for select elements to use. 
 // Each is an array: index 0 for commands, index 1 for events.
-// The names are: sendTargets, members, roles, channels,
+// The names are: sendTargets, members, roles, channels, 
 //                messages, servers, variables
 //---------------------------------------------------------------------
 
@@ -121,37 +90,35 @@ html: function(isEvent, data) {
 	<div>
 		<p>
 			<u>Mod Info:</u><br>
-			Created by Lasse & MrGold!
+			Created by MrGold
 		</p>
 	</div><br>
 <div>
 	<div style="float: left; width: 35%;">
-		Source Reaction:<br>
-		<select id="reaction" class="round" onchange="glob.refreshVariableList(this)">
-			${data.variables[1]}
+		Source Message:<br>
+		<select id="message" class="round" onchange="glob.messageChange(this, 'varNameContainer')">
+			${data.messages[isEvent ? 1 : 0]}
 		</select>
 	</div>
-	<div id="varNameContainer" style="float: right; width: 60%;">
+	<div id="varNameContainer" style="display: none; float: right; width: 60%;">
 		Variable Name:<br>
 		<input id="varName" class="round" type="text" list="variableList"><br>
 	</div>
-</div><br><br><br>
+</div><br><br><br><br>
 <div>
-	<div style="padding-top: 8px; width: 70%;">
-		Source Info:<br>
+	<div style="float: left; width: 40%;">
+		Source Emoji:<br>
 		<select id="info" class="round">
-			<option value="0" selected>Message Object</option>
-			<option value="5">First User to React</option>
-			<option value="6">Random User to React</option>
-			<option value="7">Last User to React</option>
-			<option value="1">Bot Reacted?</option>
-			<option value="2">User Who Reacted List</option>
-			<option value="3">Emoji Name</option>
-			<option value="4">Reaction Count</option>
+			<option value="0" selected>Emoji ID</option>
+			<option value="1">Emoji Name</option>
 		</select>
 	</div>
-</div><br>
-<div>
+	<div style="float: right; width: 55%;">
+		Search Value:<br>
+		<input id="find" class="round" type="text">
+	</div>
+</div><br><br><br><br>
+<div style="padding-top: 8px;">
 	<div style="float: left; width: 35%;">
 		Store In:<br>
 		<select id="storage" class="round">
@@ -160,11 +127,10 @@ html: function(isEvent, data) {
 	</div>
 	<div id="varNameContainer2" style="float: right; width: 60%;">
 		Variable Name:<br>
-		<input id="varName2" class="round" type="text"><br>
-	</div>
-</div>`
+		<input id="varName2" class="round" type="text">
+	</div>`
 },
-//display: none;
+
 //---------------------------------------------------------------------
 // Action Editor Init Code
 //
@@ -176,61 +142,37 @@ html: function(isEvent, data) {
 init: function() {
 	const {glob, document} = this;
 
-	glob.refreshVariableList(document.getElementById('reaction'));
+	glob.messageChange(document.getElementById('message'), 'varNameContainer')
 },
 
 //---------------------------------------------------------------------
 // Action Bot Function
 //
 // This is the function for the action within the Bot's Action class.
-// Keep in mind event calls won't have access to the "msg" parameter,
+// Keep in mind event calls won't have access to the "msg" parameter, 
 // so be sure to provide checks for variable existance.
 //---------------------------------------------------------------------
 
 action: function(cache) {
 	const data = cache.actions[cache.index];
-	const reaction = parseInt(data.reaction);
+	const message = parseInt(data.message);
 	const varName = this.evalMessage(data.varName, cache);
+	const msg = this.getMessage(message, varName, cache);
 	const info = parseInt(data.info);
-	var WrexMods = this.getWrexMods(); //Find abb_custom_methods_MOD
-	const rea = WrexMods.getReaction(reaction, varName, cache); //Get Reaction
-	if(!WrexMods) return;
-	if(!rea) {
-		console.log('This is not a reaction'); //Variable is not a reaction -> Error
-		this.callNextAction(cache);
-	}
+	const emoji = this.evalMessage(data.find, cache);
+	
 	let result;
 	switch(info) {
 		case 0:
-			result = rea.message; //Message Object
+			result = msg.reactions.find(reaction => reaction.emoji.id == emoji);
 			break;
 		case 1:
-			result = rea.me; //This bot reacted?
-			break;
-		case 2:
-			result = rea.users.array(); //All users who reacted list
-			break;
-		case 3:
-			result = rea.emoji.name; //Emoji (/Reaction) name
-			break;
-		case 4:
-			result = rea.count; //Number (user+bots) who reacted like this
-			break;
-		case 5:
-			const lastid = rea.users.firstKey(); //Stores last user ID reacted
-			result = cache.server.members.find(element => element.id === lastid);
-			break;
-		case 6:
-			const lastid2 = rea.users.randomKey(); //Stores last user ID reacted
-			result = cache.server.members.find(element => element.id === lastid2);
-			break;
-		case 7:
-			const lastid3 = rea.users.lastKey(); //Stores last user ID reacted
-			result = cache.server.members.find(element => element.id === lastid3);
+			result = msg.reactions.find(reaction => reaction.emoji.name == emoji);
 			break;
 		default:
 			break;
 	}
+	
 	if(result !== undefined) {
 		const storage = parseInt(data.storage);
 		const varName2 = this.evalMessage(data.varName2, cache);
