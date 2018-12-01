271342a50434b886f86e09e985e6b4897ffb2184
Almeeida <Almeeida@noreply.com>
2018-12-01 18:16:55 +0000

    Added Run Console Command
    This mod runs a command to console, such as "npm i", etc.

---
+++ actions/run_console_command_MOD.js
@@ -0,0 +1,115 @@
+module.exports = {
+	//---------------------------------------------------------------------
+   // Action Name
+   //
+   // This is the name of the action displayed in the editor.
+   //---------------------------------------------------------------------
+	name: "Run Console Command",
+	//---------------------------------------------------------------------
+   // Action Section
+   //
+   // This is the section the action will fall into.
+   //---------------------------------------------------------------------
+	section: "Other Stuff",
+	//---------------------------------------------------------------------
+   // Action Subtitle
+   //
+   // This function generates the subtitle displayed next to the name.
+   //---------------------------------------------------------------------
+	subtitle: function(data) {
+	   return `${data.command}`;
+   },
+	//---------------------------------------------------------------------
+		// DBM Mods Manager Variables (Optional but nice to have!)
+		//
+		// These are variables that DBM Mods Manager uses to show information
+		// about the mods for people to see in the list.
+		//---------------------------------------------------------------------
+		 // Who made the mod (If not set, defaults to "DBM Mods")
+		author: "MrGold", //Idea by I Am Alien Pete & Original Code by NetLuis
+		 // The version of the mod (Defaults to 1.0.0)
+		version: "1.9.3", //Added in 1.9.3
+		 // A short description to show on the mod line for this mod (Must be on a single line)
+		short_description: "Runs a Command to the Console",
+		 // If it depends on any other mods by name, ex: WrexMODS if the mod uses something from WrexMods
+		 //---------------------------------------------------------------------
+	//---------------------------------------------------------------------
+   // Action Fields
+   //
+   // These are the fields for the action. These fields are customized
+   // by creating elements with corresponding IDs in the HTML. These
+   // are also the names of the fields stored in the action's JSON data.
+   //---------------------------------------------------------------------
+	fields: ["command"],
+	//---------------------------------------------------------------------
+   // Command HTML
+   //
+   // This function returns a string containing the HTML used for
+   // editting actions. 
+   //
+   // The "isEvent" parameter will be true if this action is being used
+   // for an event. Due to their nature, events lack certain information, 
+   // so edit the HTML to reflect this.
+   //
+   // The "data" parameter stores constants for select elements to use. 
+   // Each is an array: index 0 for commands, index 1 for events.
+   // The names are: sendTargets, members, roles, channels, 
+   //                messages, servers, variables
+   //---------------------------------------------------------------------
+	html: function(isEvent, data) {
+	   return `
+   <div>
+	   <p>
+		   <u>Mod Info:</u><br>
+         Created by NetLuis, MrGold<br>
+         Fixed by Almeida<br>
+	   </p>
+   </div><br>
+   <div style="padding-top: 8px;">
+	   Command to Run:<br>
+	   <textarea id="command" rows="9" placeholder="Insert command here..." style="width: 99%; font-family: monospace; white-space: nowrap; resize: none;"></textarea>
+   </div><br>
+   <div style="float: left; width: 88%; padding-top: 8px;">
+	   <p>
+		   <b>NOTE:</b> This mod will not work if you run your bot on DBM
+	   </p>
+   </div>`
+   },
+	//---------------------------------------------------------------------
+   // Action Editor Init Code
+   //
+   // When the HTML is first applied to the action editor, this code
+   // is also run. This helps add modifications or setup reactionary
+   // functions for the DOM elements.
+   //---------------------------------------------------------------------
+	init: function() {
+   },
+	//---------------------------------------------------------------------
+   // Action Bot Function
+   //
+   // This is the function for the action within the Bot's Action class.
+   // Keep in mind event calls won't have access to the "msg" parameter, 
+   // so be sure to provide checks for variable existance.
+   //---------------------------------------------------------------------
+	action: function(cache) {
+      const data = cache.actions[cache.index];
+	   const command = this.evalMessage(data.command, cache);
+	   const child = require('child_process');
+	      try {
+            child.execSync(command,{cwd: require('path').dirname(process.argv[1]),stdio:[0,1,2]})
+            this.callNextAction(cache);
+         } catch(e) {
+            console.log('An error occurred while executing the command: ' + e)
+      }
+   },
+	//---------------------------------------------------------------------
+   // Action Bot Mod
+   //
+   // Upon initialization of the bot, this code is run. Using the bot's
+   // DBM namespace, one can add/modify existing functions if necessary.
+   // In order to reduce conflictions between mods, be sure to alias
+   // functions you wish to overwrite.
+   //---------------------------------------------------------------------
+	mod: function(DBM) {
+   }
+	}; // End of module 
