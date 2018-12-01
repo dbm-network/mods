22b2ba3c8c171e829264bead4954ede4fc2b07b6
LasseNiermann <lasse.niermann@icloud.com>
2018-12-01 19:08:09 +0100

    Removed various mods and renamed some

--- actions/create_category_MOD.js
+++ actions/create_category_MOD.js
@@ -37,7 +37,7 @@ subtitle: function(data) {
 	 author: "EliteArtz",
 
 	 // The version of the mod (Defaults to 1.0.0)
-	 version: "1.8.3",
+	 version: "1.9.3", //Added in 1.8.3
 
 	 // A short description to show on the mod line for this mod (Must be on a single line)
 	 short_description: "Creates a Category Channel!",
