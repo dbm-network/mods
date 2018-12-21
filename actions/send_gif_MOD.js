ac21039613a12f02f3f924695d68cc6b19088bdf
lucasboss45 <l4f4go@gmail.com>
2018-12-21 21:17:52 +0100

    send_gif_MOD.js fixed version number

--- actions/send_gif_MOD.js
+++ actions/send_gif_MOD.js
@@ -38,7 +38,7 @@ subtitle: function(data) {
 	 author: "MrGold",
 
 	 // The version of the mod (Defaults to 1.0.0)
-	 version: "1.9.2", //Added in 1.9.2
+	 version: "1.9.4", //Added in 1.9.4
 
 	 // A short description to show on the mod line for this mod (Must be on a single line)
 	 short_description: "Sends a GIF",
