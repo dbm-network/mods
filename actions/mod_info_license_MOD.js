acae563d86632981e41d43c8954a38d0b444c2fb
Deus Corvi <43352312+dcorvus@users.noreply.github.com>
2019-02-09 16:21:03 -0600

    Fixed the copyright years.
    I hate Github Desktop.

--- actions/mod_info_license_MOD.js
+++ actions/mod_info_license_MOD.js
@@ -88,7 +88,7 @@ html: function(isEvent, data) {
 DBM Mods has no official affiliation with Discord or Discord Bot Maker.<br>
 <h2>MIT License</h2><br>
 
-Copyright (c) 2017-2018 DBM Network<br><br>
+Copyright (c) 2017-2019 DBM Network<br><br>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:<br><br>
 
