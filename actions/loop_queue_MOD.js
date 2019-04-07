7a2181c33de3c5c4aa58cdea604dacb209167ecb
ZockerNico <47316898+ZockerNico@users.noreply.github.com>
2019-04-07 12:30:53 +0200

    Updated DBM Mods music functions
    This is a needed updated for [Loop Queue] because it caused an error if the current playing item wasn't a youtube video. The music function will now store every current playing item.

--- actions/loop_queue_MOD.js
+++ actions/loop_queue_MOD.js
@@ -23,7 +23,7 @@ section: "Audio Control",
 //---------------------------------------------------------------------
 
 subtitle: function(data) {
-	const actions = ["Loop Queue", "Loop Current Item"];
+	const actions = ["Loop Whole Queue", "Loop Current Item"];
 	return `${actions[parseInt(data.loop)]}`;
 },
 
@@ -91,9 +91,14 @@ html: function(isEvent, data) {
 <div style="float: right; width: 50%; padding-top: 8px;">
 	Loop Operation:<br>
 	<select id="loop" class="round">
-		<option value="0" selected>Loop Queue</option>
+		<option value="0" selected>Loop Whole Queue</option>
 		<option value="1">Loop Current Item</option>
-	</select>
+	</select><br>
+</div>
+<div style="float: left; width: 100%; padding-top: 8px;">
+	<p>
+		Please put the Welcome action into a Bot Initalization event to be able to store the current song!
+	</p>
 </div>`;
 },
 
@@ -159,7 +164,7 @@ action: function(cache) {
 //---------------------------------------------------------------------
 
 mod: function(DBM) {
-	//Check for PlayingNow Data Object
+//Check for PlayingNow Data Object
 	if(DBM.Audio.playingnow === undefined) {
 		DBM.Audio.playingnow = [];
 	};
@@ -207,7 +212,7 @@ mod: function(DBM) {
 			};
 		};
 	};
-
+	
 	DBM.Audio.playItem = function(item, id) {
 		if(!this.connections[id]) return;
 		if(this.dispatchers[id]) {
@@ -219,9 +224,11 @@ mod: function(DBM) {
 		switch(type) {
 			case 'file':
 				setupDispatcher = this.playFile(item[2], item[1], id);
+				this.playingnow[id] = item;
 				break;
 			case 'url':
 				setupDispatcher = this.playUrl(item[2], item[1], id);
+				this.playingnow[id] = item;
 				break;
 			case 'yt':
 				setupDispatcher = this.playYt(item[2], item[1], id);
