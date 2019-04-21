c4205ff8304093a674ee2d2393b65a960d91c257
Two <40973130+TotallynotTwo@users.noreply.github.com>
2019-04-20 21:39:27 -0400

    Update and rename find_member.js to find_mem_or_user_MOD.js

--- actions/find_mem_or_user_MOD.js
+++ actions/find_mem_or_user_MOD.js
@@ -6,7 +6,7 @@ module.exports = {
 // This is the name of the action displayed in the editor.
 //---------------------------------------------------------------------
 
-name: "Find Member",
+name: "Find Member/User",
 
 //---------------------------------------------------------------------
 // Action Section
@@ -23,8 +23,9 @@ section: "Member Control",
 //---------------------------------------------------------------------
 
 subtitle: function(data) {
-	const info = ['Member ID', 'Member Username', 'Member Display Name', 'Member Tag', 'Member Color'];
-	return `Find Member by ${info[parseInt(data.info)]}`;
+	const op1 = ['Member', 'User']
+	const info = [' ID', ' Username', ' Display Name', ' Tag', ' Color'];
+	return `Find ${op1[parseInt(data.find2)]} by ${op1[parseInt(data.find2)]}${info[parseInt(data.info)]}`;
 },
 
 
@@ -72,7 +73,7 @@ variableStorage: function(data, varType) {
 // are also the names of the fields stored in the action's JSON data.
 //---------------------------------------------------------------------
 
-fields: ["info", "find", "storage", "varName"],
+fields: ["info", "find", "storage", "varName", "find2"],
 
 //---------------------------------------------------------------------
 // Command HTML
@@ -92,35 +93,42 @@ fields: ["info", "find", "storage", "varName"],
 
 html: function(isEvent, data) {
 	return `
-<div><p>This action has been modified by DBM Mods.</p></div><br>
-<div>
-	<div style="float: left; width: 40%;">
-		Source Field:<br>
-		<select id="info" class="round">
-			<option value="0" selected>Member ID</option>
-			<option value="1">Member Username</option>
-			<option value="2">Member Display Name</option>
-			<option value="3">Member Tag</option>
-			<option value="4">Member Color</option>
-		</select>
+	<div><p>This action has been modified by DBM Mods.</p></div><br>
+	<!-- Added by Two -->
+	<div style="float: left;">
+	<select id="find2" onchange="glob.change()">
+	<option value="0" selected>Find Member (current server only)</option>
+	<option value="1">Find User (all servers)</option>
+	</select></div><br><br>
+	<div>
+		<div style="float: left; width: 40%;">
+			Source Field:<br>
+			<select id="info" class="round">
+				<option value="0" selected>Member ID</option>
+				<option value="1">Member Username</option>
+				<option value="2">Member Display Name</option>
+				<option value="3">Member Tag</option>
+				<option value="4">Member Color</option> 
+			</select>
+		</div>
+		<div style="float: right; width: 55%;">
+			Search Value:<br>
+			<input id="find" class="round" type="text">
+		</div>
+	</div><br><br><br>
+	<div style="padding-top: 8px;">
+		<div style="float: left; width: 35%;">
+			Store In:<br>
+			<select id="storage" class="round">
+				${data.variables[1]}
+			</select>
+		</div>
+		<div id="varNameContainer" style="float: right; width: 60%;">
+			Variable Name:<br>
+			<input id="varName" class="round" type="text">
+		</div>
 	</div>
-	<div style="float: right; width: 55%;">
-		Search Value:<br>
-		<input id="find" class="round" type="text">
-	</div>
-</div><br><br><br>
-<div style="padding-top: 8px;">
-	<div style="float: left; width: 35%;">
-		Store In:<br>
-		<select id="storage" class="round">
-			${data.variables[1]}
-		</select>
-	</div>
-	<div id="varNameContainer" style="float: right; width: 60%;">
-		Variable Name:<br>
-		<input id="varName" class="round" type="text">
-	</div>
-</div>`
+`
 },
 
 //---------------------------------------------------------------------
@@ -132,6 +140,30 @@ html: function(isEvent, data) {
 //---------------------------------------------------------------------
 
 init: function() {
+	const {glob, document} = this;
+	 glob.change = function(event){
+	try{
+	var sel = document.getElementById("find2")
+	var option = document.getElementById("info")
+	var x = document.getElementById("info")
+	if(sel.value == "0"){
+		
+	for(var i = 0; i < option.length; i++){
+	option[i].disabled = false;
+	 option[i].innerHTML = option[i].innerHTML.replace(/[^\s]*/, "Member")   
+	     }
+	}else if(sel.value == "1"){
+		option[2].disabled = true;
+		option[3].disabled = true;
+		option[4].disabled = true;
+	for(var i = 0; i < option.length; i++){
+	option[i].innerHTML = option[i].innerHTML.replace(/[^\s]*/, "User")
+	}
+	}
+	}catch(err){alert(err)}
+	}
+	glob.change(document.getElementById('find'));
+    glob.change()
 },
 
 //---------------------------------------------------------------------
@@ -151,7 +183,7 @@ action: function(cache) {
 	const data = cache.actions[cache.index];
 	const info = parseInt(data.info);
 	const find = this.evalMessage(data.find, cache);
-	
+	const find2 = parseInt(data.find2);
 	//DBM Mods ~ Lasse
 	//Checks if server is large and caches all users to verify that offline users are tracked.
 	if(server.large == true) {
@@ -162,23 +194,29 @@ action: function(cache) {
 	let result;
 	switch(info) {
 		case 0:
-			result = server.members.find(element => element.id === find);
+		   
+			result = find2 == 0 ? server.members.find(element => element.id === find) : this.getDBM().Bot.bot.users.find(element => element.id === find)
+			
 			break;
 		case 1:
-			result = server.members.find(function(mem) {
+		result = find2 == 0 ? server.members.find(function(mem) {
 				return mem.user ? mem.user.username === find : false;
-			});
+			}) : this.getDBM().Bot.bot.users.find(function(mem) {
+				return mem ? mem.username === find : false;
+			})
 			break;
 		case 2:
-			result = server.members.find(element => element.displayName === find);
+		result = find2 == 0 ? server.members.find(element => element.displayName === find) : this.getDBM().Bot.bot.users.find(element => element.displayName === find)
 			break
 		case 3:
-			result = server.members.find(function(mem) {
+		result = find2 == 0 ? server.members.find(function(mem) {
 				return mem.user ? mem.user.tag === find : false;
-			});
+			}) : this.getDBM().Bot.bot.users.find(function(mem) {
+				return mem ? mem.tag === find : false;
+			})
 			break;
 		case 4:
-			result = server.members.find(element => element.displayColor === find);
+		result = find2 == 0 ? server.members.find(element => element.displayColor === find) : this.getDBM().Bot.bot.users.find(element => element.displayColor === find)
 			break;;
 		default:
 			break;
