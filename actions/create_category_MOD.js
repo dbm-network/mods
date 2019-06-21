1cd71c22dfc798cec4825bb2af0ea031a4827344
TheMonDon <bforster48@gmail.com>
2019-06-21 13:35:48 -0500

    Fix depreciation warning

--- actions/create_category_MOD.js
+++ actions/create_category_MOD.js
@@ -139,7 +139,7 @@ action: function(cache) {
 	if(server && server.createChannel) {
 		const name = this.evalMessage(data.channelName, cache);
 		const storage = parseInt(data.storage);
-		server.createChannel(name, 'category').then(function(channel) {
+		server.createChannel(name, {type: 'category'}).then(function(channel) {
 			const channelData = {};
 			if(data.position) {
 				channelData.position = parseInt(data.position);
