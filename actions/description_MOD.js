d207b5acc69e339fba8badd8ebca62c278d898d5
hazelpy <59073660+hazelpy@users.noreply.github.com>
2020-03-21 20:09:59 -0400

    Command Description mods.
    Added two mods:
    Command Description
    Get Command Description
    - - -
    Command Description is an action that holds a description for a command. This is used along with Get Command Description to make it easier to make help/info commands.

---
+++ actions/description_MOD.js
@@ -0,0 +1,27 @@
+module.exports = {
+    name: "Command Description",
+    section: "Other Stuff",
+    author: "Hazelpy",
+    version: "0.0.1",
+    short_description: "Set command description and restrictions. Useful for help/info commands.",
+    subtitle: function(data) { return `Command Description`; },
+    fields: ["description", "restrictedTo"],
+    html: (isEvent, data) => {
+        return `<div width="540" height="360" overflow-y="scroll">
+            <div style="padding-top: 8px;">
+	            Description:<br>
+	            <textarea id="description" rows="9" placeholder="Insert description here." style="width: 99%; font-family: monospace; white-space: nowrap; resize: none;"></textarea>
+            </div><br>
+            <div style="padding-top: 8px;">
+                Restricted to:<br>
+                <select id="restrictedTo" class="round">
+                    <option value="none" selected>Everybody</option>
+                    <option value="botOwner">Bot Owner</option>
+                </select>
+            </div>
+        </div>
+        `;
+    },
+    action: function(cache) { this.callNextAction(cache); },
+    mod: function(DBM) { }
+}
