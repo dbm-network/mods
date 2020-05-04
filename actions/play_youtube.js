b32e86e10a8710ce45be990bb72e62236f4d2d49
Mindlesscargo <39963404+Mindlesscargo@users.noreply.github.com>
2020-05-04 16:22:19 -0500

    Update Play Youtube
    Check if msg exists to avoid `TypeError: Cannot read property 'author' of undefined` when used in an event

--- actions/play_youtube.js
+++ actions/play_youtube.js
@@ -161,7 +161,11 @@ module.exports = {
             } else {
                 options.bitrate = 'auto';
             }
-            options.requester = msg.author;
+            if (msg) {
+                options.requester = msg.author;
+            } else {
+                options.requester = 'None';
+            }
 
             const video = await getInfoAsync(url)
                 .catch((err) => {
