88e11c0a68d68b420dc5a6b629455550be9dd63f
Almeeida <rodrigofigueire19@gmail.com>
2020-06-16 11:32:11 +0100

    switching up the folders

---
+++ not_converted/play_youtube_playlist_MOD.js
@@ -0,0 +1,94 @@
+module.exports = {
+	name: "Play YouTube Playlist",
+	section: "Audio Control",
+
+	requiresAudioLibraries: true,
+
+	subtitle: function(data) {
+		return `${data.url}`;
+	},
+
+	fields: ["url", "apikey", "seek", "volume", "passes", "bitrate"],
+
+	html: function(isEvent, data) {
+		return `
+	<div style="float: left; width: 105%;">
+		YouTube Playlist URL:<br>
+		<input id="url" class="round" type="text" value="https://www.youtube.com/playlist?list=PLkfg3Bt9RE055BeP8DeDZSUCYxeSLnobe"><br>
+	</div>
+	<div style="float: left; width: 105%;">
+		API Key:<br>
+		<input id="apikey" class="round" type="text" placeholder="Insert your YouTube Data V3 API Key..."><br>
+	</div>
+	<div style="float: left; width: 49%;">
+		Video Seek Positions:<br>
+		<input id="seek" class="round" type="text" value="0"><br>
+		Video Volumes:<br>
+		<input id="volume" class="round" type="text" placeholder="Leave blank for automatic..."><br>
+	</div>
+	<div style="float: right; width: 49%;">
+		Video Passes:<br>
+		<input id="passes" class="round" type="text" value="1"><br>
+		Video Bitrates:<br>
+		<input id="bitrate" class="round" type="text" placeholder="Leave blank for automatic..."><br>
+	</div>`;
+	},
+
+	init: function() {},
+
+	action: function(cache) {
+		const data = cache.actions[cache.index];
+		const Audio = this.getDBM().Audio;
+		const WrexMODS = this.getWrexMods();
+		const ytapi = WrexMODS.require("simple-youtube-api");
+		const apikey = this.evalMessage(data.apikey, cache);
+		const playlist = this.evalMessage(data.url, cache);
+		const options = {};
+
+		//Check Input
+		if(playlist === undefined || playlist === "") {
+			return console.log("Please insert a playlist url!");
+		}
+		if(apikey === undefined || playlist === "") {
+			return console.log("Please insert an valid api key!");
+		}
+
+		//Check Options
+		if(data.seek) {
+			options.seek = parseInt(this.evalMessage(data.seek, cache));
+		}
+		if(data.volume) {
+			options.volume = parseInt(this.evalMessage(data.volume, cache)) / 100;
+		} else if(cache.server) {
+			options.volume = Audio.volumes[cache.server.id] || 0.5;
+		} else {
+			options.volume = 0.5;
+		}
+		if(data.passes) {
+			options.passes = parseInt(this.evalMessage(data.passes, cache));
+		}
+		if(data.bitrate) {
+			options.bitrate = parseInt(this.evalMessage(data.bitrate, cache));
+		} else {
+			options.bitrate = "auto";
+		}
+
+		//Load playlist
+		const YouTube = new ytapi(`${apikey}`);
+
+		YouTube.getPlaylist(`${playlist}`).then((playlist) => {
+			playlist.getVideos().then((videos) => {
+				videos.forEach((video) => {
+					const info = ["yt", options, `https://www.youtube.com/watch?v=${video.id}`];
+					if(video.id !== undefined) {
+						Audio.addToQueue(info, cache);
+					}
+				});
+			});
+		});
+
+		this.callNextAction(cache);
+	},
+
+	mod: function() {}
+};
---
+++ not_converted/set_member_voice_channel_perms_MOD.js
@@ -0,0 +1,94 @@
+module.exports = {
+	name: "Set Member Voice Channel Perms",
+	section: "Channel Control",
+
+	subtitle: function(data) {
+		const names = ["Command Author's Voice Ch.", "Mentioned User's Voice Ch.", "Default Voice Channel", "Temp Variable", "Server Variable", "Global Variable"];
+		const index = parseInt(data.vchannel);
+		return index < 3 ? `${names[index]}` : `${names[index]} - ${data.varName}`;
+	},
+
+	fields: ["vchannel", "varName", "member", "varName2", "permission", "state"],
+
+	html: function(isEvent, data) {
+		return `
+<div>
+	<div style="float: left; width: 35%;">
+		Source Voice Channel:<br>
+		<select id="vchannel" class="round" onchange="glob.voiceChannelChange(this, 'varNameContainer')">
+			${data.voiceChannels[isEvent ? 1 : 0]}
+		</select>
+	</div>
+	<div id="varNameContainer" style="display: none; float: right; width: 60%;">
+		Variable Name:<br>
+		<input id="varName" class="round" type="text" list="variableList"><br>
+	</div>
+</div><br><br><br>
+<div style="padding-top: 8px;">
+	<div style="float: left; width: 35%;">
+		Source Member:<br>
+		<select id="member" name="second-list" class="round" onchange="glob.memberChange(this, 'varNameContainer2')">
+			${data.members[isEvent ? 1 : 0]}
+		</select>
+	</div>
+	<div id="varNameContainer2" style="display: none; float: right; width: 60%;">
+		Variable Name:<br>
+		<input id="varName2" class="round" type="text" list="variableList2"><br>
+	</div>
+</div><br><br><br>
+<div style="padding-top: 8px;">
+	<div style="float: left; width: 45%;">
+		Permission:<br>
+		<select id="permission" class="round">
+			${data.permissions[1]}
+		</select>
+	</div>
+	<div style="padding-left: 5%; float: left; width: 55%;">
+		Change To:<br>
+		<select id="state" class="round">
+			<option value="0" selected>Allow</option>
+			<option value="1">Inherit</option>
+			<option value="2">Disallow</option>
+		</select>
+	</div>
+</div>`;
+	},
+
+	init: function() {
+		const { glob, document } = this;
+
+		glob.voiceChannelChange(document.getElementById("vchannel"), "varNameContainer");
+		glob.memberChange(document.getElementById("member"), "varNameContainer2");
+	},
+
+	action: function(cache) {
+		const data = cache.actions[cache.index];
+		const storage = parseInt(data.vchannel);
+		const varName = this.evalMessage(data.varName, cache);
+		const channel = this.getChannel(storage, varName, cache);
+
+		const storage2 = parseInt(data.member);
+		const varName2 = this.evalMessage(data.varName2, cache);
+		const member = this.getMember(storage2, varName2, cache);
+
+		const options = {};
+		options[data.permission] = data.state === "0" ? true : (data.state === "2" ? false : null);
+		if(member && member.id) {
+			if(Array.isArray(channel)) {
+				this.callListFunc(channel, "overwritePermissions", [member.id, options]).then(function() {
+					this.callNextAction(cache);
+				}.bind(this));
+			} else if(channel && channel.overwritePermissions) {
+				channel.overwritePermissions(member.id, options).then(function() {
+					this.callNextAction(cache);
+				}.bind(this)).catch(this.displayError.bind(this, data, cache));
+			} else {
+				this.callNextAction(cache);
+			}
+		} else {
+			this.callNextAction(cache);
+		}
+	},
+
+	mod: function() {}
+};
