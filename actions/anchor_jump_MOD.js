d9472436646a1ad69e0ac148ad7f83ebeb1bc98f
LeonZ2019 <10143361+LeonZ2019@users.noreply.github.com>
2020-06-17 13:26:19 +0800

    Update anchor_jump_MOD.js
    Add function for run script

--- actions/anchor_jump_MOD.js
+++ actions/anchor_jump_MOD.js
@@ -48,3 +48,63 @@ module.exports = {
 
 	mod: function() {}
 };
+module.exports = {
+	name: "Jump to Anchor",
+	section: "Other Stuff",
+
+	subtitle: function(data) {
+		return data.description ? `<font color="${data.color}">${data.description}</font>` : `Jump to ${data.jump_to_anchor ? `the "<font color="${data.color}">${data.jump_to_anchor}</font>" anchor in your command if it exists!` : "an anchor!"}`;
+	},
+
+	fields: ["description", "jump_to_anchor", "color"],
+
+	html: function(isEvent, data) {
+		return `
+	<div>
+		<p>
+			<u>Mod Info:</u><br>
+			This mod will jump to the specified anchor point<br>
+			without requiring you to edit any other skips or jumps.<br>
+			<b>This is sensitive and must be exactly the same as your anchor name.</b>
+		</p>
+	</div><br>
+	<div style="float: left; width: 74%;">
+		Jump to Anchor ID:<br>
+		<input type="text" class="round" id="jump_to_anchor"><br>
+	</div>
+	<div style="float: left; width: 24%;">
+		Anchor Color:<br>
+		<input type="color" id="color"><br>
+	</div>
+	<div style="float: left; width: 98%;">
+		Description:<br>
+		<input type="text" class="round" id="description"><br>
+	</div>
+	`;
+	},
+
+	init: function() {},
+
+	action: function(cache) {
+		const id = cache.actions[cache.index].jump_to_anchor;
+		this.anchorJump(id,cache);
+	},
+
+	mod: function(DBM) {
+		DBM.Actions.anchorJump = (id, cache) => {
+			const anchorIndex = cache.actions.findIndex(a => a.name === "Create Anchor" && a.anchor_id === id);
+			if (anchorIndex === -1) throw new Error('There was not an anchor found with that exact anchor ID!');
+			cache.index = anchorIndex - 1;
+			this.callNextAction(cache);
+		};
+
+		DBM.Actions.anchorExist = function(id,cache) {
+			const anchorIndex = cache.actions.findIndex((a) => a.name === "Create Anchor" && a.anchor_id === id);
+			if (anchorIndex === -1) {
+				return false;
+			} else {
+				return true;
+			};
+		};
+	};
+};
