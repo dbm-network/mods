f883a99fba0069f7d63064096094e78ae02e3b70
Mindlesscargo <39963404+Mindlesscargo@users.noreply.github.com>
2020-06-18 16:46:46 -0500

    Update parse_from_stored_json_MOD.js (#374)
    Fix the variable type bug

--- actions/parse_from_stored_json_MOD.js
+++ actions/parse_from_stored_json_MOD.js
@@ -16,68 +16,79 @@ module.exports = {
 		}
 	},
 
-	fields: ["behavior", "jsonObjectVarName", "path", "storage", "varName"],
+	fields: ["behavior", "varStorage", "jsonObjectVarName", "path", "storage", "varName"],
 
 	html: function(isEvent, data) {
 		return `
 <div style="margin: 0; overflow-y: none;">
-	<div style="width: 80%;">
-		<div><br>
-			<label for="jsonObjectVarName">
-				<font color="white">Stored JSON Variable Name:</font>
-			</label>
-			<input id="jsonObjectVarName" class="round" type="text"><br>
-		</div>
-		<div>
-			JSON Path: (supports the usage of <a href="http://goessner.net/articles/JsonPath/index.html#e2" target="_blank">JSON Path (Regex)</a>)<br>
-			<input id="path" class="round" ;" type="text"><br>
-		</div>
-	</div>
-	<div style="width: 80%;">
-		<div style="float: left; width: 30%;">
-			<label for="storage">
-				<font color="white">Store In:</font>
-			</label>
-			<select id="storage" class="round" onchange="glob.variableChange(this, 'varNameContainer')">
-				${data.variables[1]}
-			</select>
-		</div>
-		<div id="varNameContainer" style="margin-left: 10px; float: left; width: 65%;">
-			<label for="varName">
-				<font color="white">Variable Name:</font>
-			</label>
-			<input id="varName" class="round" type="text">
-		</div>
-	</div>
-	<div>
-		<div style="float: left;">
-			<br>
-			<label for="behavior">
-				<font color="white">End Behavior:</font>
-			</label>
-			<select id="behavior" class="round" ;>
-				<option value="0" selected>Call Next Action Automatically</option>
-				<option value="1">Do Not Call Next Action</option>
-			</select>
-		</div>
-		<div style="float: left; margin-left: 10px; width: 30%;">
-			<br>
-			<label for="debugMode">
-				<font color="white">Debug Mode:</font>
-			</label>
-			<select id="debugMode" class="round">
-				<option value="0" selected>Disabled</option>
-				<option value="1">Enabled</option>
-			</select>
-		</div>
-	</div>
+    <div>
+        <p>
+        <u>Mod Info:</u><br>
+        Authors: ${this.author}
+        </p>
+    </div><br>
+    <div style="width: 80%;">    
+        <div style="float: left; width: 35%;">
+              Variable:<br>
+              <select id="varStorage" class="round" onchange="glob.refreshVariableList(this)">
+                  ${data.variables[1]}
+              </select>
+        </div>
+        <div id="jsonObjectVarNameContainer" style="float: right; width: 60%;">
+            Variable Name:<br>
+            <input id="jsonObjectVarName" class="round" type="text" list="variableList">
+        </div><br><br><br>
+        <div id="pathContainer" tyle="padding-top: 8px;">
+            JSON Path: (supports the usage of <a href="http://goessner.net/articles/JsonPath/index.html#e2" target="_blank">JSON Path (Regex)</a>)<br>
+            <input id="path" class="round" ;" type="text"><br>
+        </div>
+    </div>
+    <div style="width: 80%;">
+        <div style="float: left; width: 30%;">
+            <label for="storage">
+                <font color="white">Store In:</font>
+            </label>
+            <select id="storage" class="round" onchange="glob.variableChange(this, 'varNameContainer')">
+                ${data.variables[1]}
+            </select>
+        </div>
+        <div id="varNameContainer" style="margin-left: 10px; float: left; width: 65%;">
+            <label for="varName">
+                <font color="white">Variable Name:</font>
+            </label>
+            <input id="varName" class="round" type="text">
+        </div>
+    </div>
+    <div>
+        <div style="float: left;">
+            <br>
+            <label for="behavior">
+                <font color="white">End Behavior:</font>
+            </label>
+            <select id="behavior" class="round" ;>
+                <option value="0" selected>Call Next Action Automatically</option>
+                <option value="1">Do Not Call Next Action</option>
+            </select>
+        </div>
+        <div style="float: left; margin-left: 10px; width: 30%;">
+            <br>
+            <label for="debugMode">
+                <font color="white">Debug Mode:</font>
+            </label>
+            <select id="debugMode" class="round">
+                <option value="0" selected>Disabled</option>
+                <option value="1">Enabled</option>
+            </select>
+        </div>
+    </div>
 </div>
 `;
 	},
 
 	init: function() {
 		const { glob, document } = this;
-		glob.variableChange(document.getElementById("storage"), "varNameContainer");
+        glob.variableChange(document.getElementById("storage"), "varNameContainer");
+        glob.refreshVariableList(document.getElementById("storage"));
 	},
 
 	action: function(cache) {
@@ -85,10 +96,11 @@ module.exports = {
 		const data = cache.actions[cache.index];
 		let result;
 		const varName = this.evalMessage(data.varName, cache);
-		const storage = parseInt(data.storage);
+        const storage = parseInt(data.storage);
+        const type = parseInt(data.varStorage);
 		const jsonObjectVarName = this.evalMessage(data.jsonObjectVarName, cache);
 		const path = this.evalMessage(data.path, cache);
-		const jsonRaw = this.getVariable(storage, jsonObjectVarName, cache);
+		const jsonRaw = this.getVariable(type, jsonObjectVarName, cache);
 		const DEBUG = parseInt(data.debugMode);
 
 		let jsonData = jsonRaw;
