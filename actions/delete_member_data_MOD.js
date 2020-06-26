e4d019adc4120d8c860483f406b0930dbeb83526
LeonZ2019 <10143361+LeonZ2019@users.noreply.github.com>
2020-06-26 22:55:15 +0800

    Add some Features
    1. Add function delData() for guild member, user and guild. Example : GuildMember.delData(dataName)
    2. Add Globals variable, allow to control global data for run script. Function includes : data(dataName,defaultValue) setData(dataName,value) addData(dataName,value) delData(dataName)

--- actions/delete_member_data_MOD.js
+++ actions/delete_member_data_MOD.js
@@ -1,62 +1,87 @@
 module.exports = {
-	name: "Delete Member Data MOD",
+
+	name: "Delete Member Data",
+
 	section: "Deprecated",
 
 	subtitle: function(data) {
-		const members = ["Mentioned User", "Command Author", "Temp Variable", "Server Variable", "Global Variable"];
+		const members = ['Mentioned User', 'Command Author', 'Temp Variable', 'Server Variable', 'Global Variable'];
 		return `${members[parseInt(data.member)]} - ${data.dataName}`;
 	},
+
+	github: "LeonZ2019",
+	author: "LeonZ",
+	version: "1.1.1",
 
 	fields: ["member", "varName", "dataName"],
 
 	html: function(isEvent, data) {
 		return `
-<div>
-	<div style="float: left; width: 35%;">
-		Member:<br>
-		<select id="member" class="round" onchange="glob.memberChange(this, 'varNameContainer')">
-			${data.members[isEvent ? 1 : 0]}
-		</select>
-	</div>
-	<div id="varNameContainer" style="display: none; float: right; width: 60%;">
-		Variable Name:<br>
-		<input id="varName" class="round" type="text" list="variableList">
-	</div>
-</div><br><br><br>
-<div style="padding-top: 8px;">
-	<div style="float: left; width: 80%;">
-		Data Name:<br>
-		<input id="dataName" class="round" placeholder="Leave it blank to delete all data" type="text">
-	</div>
-</div>`;
+	<div>
+		<div style="float: left; width: 35%;">
+			Member:<br>
+			<select id="member" class="round" onchange="glob.memberChange(this, 'varNameContainer')">
+				${data.members[isEvent ? 1 : 0]}
+			</select>
+		</div>
+		<div id="varNameContainer" style="display: none; float: right; width: 60%;">
+			Variable Name:<br>
+			<input id="varName" class="round" type="text" list="variableList">
+		</div>
+	</div><br><br><br>
+	<div style="padding-top: 8px;">
+		<div style="float: left; width: 80%;">
+			Data Name:<br>
+			<input id="dataName" class="round" placeholder="Leave it blank to delete all data" type="text">
+		</div>
+	</div>`
 	},
 
 	init: function() {
-		const { glob, document } = this;
+		const {glob, document} = this;
 
-		glob.memberChange(document.getElementById("member"), "varNameContainer");
+		glob.memberChange(document.getElementById('member'), 'varNameContainer');
 	},
 
 	action: function(cache) {
-		const Files = this.getDBM().Files;
 		const data = cache.actions[cache.index];
 		const type = parseInt(data.member);
 		const varName = this.evalMessage(data.varName, cache);
 		const member = this.getMember(type, varName, cache);
-		const id = member.id;
-		if(member && member.data) {
-			const dataName = this.evalMessage(data.dataName, cache);
-			if(dataName === undefined) {
-				Files.data.players[id] = {};
-			} else {
-				const memberData = Files.data.players[id];
-				delete memberData[dataName];
-				Files.data.players[id] = memberData;
-			}
-			Files.saveData("players");
-		}
+		const dataName = this.evalMessage(data.dataName, cache);
+		member.delData(dataName);
 		this.callNextAction(cache);
 	},
 
-	mod: function() {}
+	mod: function(DBM) {
+		DBM.Actions["Delete Member Data MOD"] = DBM.Actions["Delete Member Data"];
+		DBM.DiscordJS.Structures.extend("GuildMember", (GuildMember) => class extends GuildMember {
+			constructor(client, data, guild) {
+				super(client, data, guild);
+			}
+			delData(name) {
+				let players = DBM.Files.data.players;
+				if (players[this.id] && name && players[this.id][name]) {
+					delete players[this.id][name];
+					DBM.Files.saveData("players");
+				} else if (!name) {
+					delete players[this.id];
+					DBM.Files.saveData("players");
+				};
+			};
+		});
+		DBM.DiscordJS.Structures.extend("User", (User) => class extends User {
+			delData(name) {
+				let players = DBM.Files.data.players;
+				if (players[this.id] && name && players[this.id][name]) {
+					delete players[this.id][name];
+					DBM.Files.saveData("players");
+				} else if (!name) {
+					delete players[this.id];
+					DBM.Files.saveData("players");
+				};
+			};
+		});
+	}
+
 };
