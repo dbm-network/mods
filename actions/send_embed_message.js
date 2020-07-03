6b8f8bcc1ed831706dffdab50f8916098dc99e23
LeonZ <leon_yy99@hotmail.com>
2020-07-04 00:43:53 +0800

    Add anchor options (#410)

--- actions/send_embed_message.js
+++ actions/send_embed_message.js
@@ -61,6 +61,7 @@ module.exports = {
 							<option value="1">Stop Action Sequence</option>
 							<option value="2">Jump To Action</option>
 							<option value="3">Skip Next Actions</option>
+							<option value="4">Jump To Anchor</option>
 				</select>
 			</div>
 			<div id="iffalseContainer" style="display: none; float: right; width: 60%;">
@@ -70,7 +71,26 @@ module.exports = {
 
 	init: function() {
 		const { glob, document } = this;
-
+		glob.onChangeFalse = function(event) {
+			switch (parseInt(event.value)) {
+				case 0:
+				case 1:
+					document.getElementById("iffalseContainer").style.display = "none";
+					break;
+				case 2:
+					document.getElementById("iffalseName").innerHTML = "Action Number";
+					document.getElementById("iffalseContainer").style.display = null;
+					break;
+				case 3:
+					document.getElementById("iffalseName").innerHTML = "Number of Actions to Skip";
+					document.getElementById("iffalseContainer").style.display = null;
+					break;
+				case 4:
+					document.getElementById("iffalseName").innerHTML = "Anchor ID";
+					document.getElementById("iffalseContainer").style.display = null;
+					break;
+			}
+		};
 		glob.sendTargetChange(document.getElementById("channel"), "varNameContainer2");
 		glob.variableChange(document.getElementById("storage3"), "varNameContainer3");
 		glob.onChangeFalse(document.getElementById("iffalse"));
--- actions/send_message.js
+++ actions/send_message.js
@@ -18,51 +18,77 @@ module.exports = {
 	html: function(isEvent, data) {
 		return `
 	<div style="width: 550px; height: 350px; overflow-y: scroll;">
-	<div><p>This action has been modified by DBM Mods.</p></div><br>
-<div>
-	<div style="float: left; width: 35%;">
-		Send To:<br>
-		<select id="channel" class="round" onchange="glob.sendTargetChange(this, 'varNameContainer')">
-			${data.sendTargets[isEvent ? 1 : 0]}
-		</select>
-	</div>
-	<div id="varNameContainer" style="display: none; float: right; width: 60%;">
-		Variable Name:<br>
-		<input id="varName" class="round" type="text" list="variableList"><br>
-	</div>
-</div><br><br><br>
-<div style="padding-top: 8px;">
-	Message:<br>
-	<textarea id="message" rows="9" placeholder="Insert message here..." style="width: 99%; font-family: monospace; white-space: nowrap; resize: none;"></textarea>
-</div><br>
-<div>
-	<div style="float: left; width: 35%;">
-		Store In:<br>
-		<select id="storage" class="round" onchange="glob.variableChange(this, 'varNameContainer2')">
-			${data.variables[0]}
-		</select>
-	</div>
-	<div id="varNameContainer2" style="display: none; float: right; width: 60%;">
-		Variable Name:<br>
-		<input id="varName2" class="round" type="text">
-	</div>
-</div><br><br><br>
-<div style="padding-top: 8px;">
-        <div style="float: left; width: 35%;">
-            If Message Delivery Fails:<br>
-            <select id="iffalse" class="round" onchange="glob.onChangeFalse(this)">
-				<option value="0" selected>Continue Actions</option>
-				<option value="1">Stop Action Sequence</option>
-				<option value="2">Jump To Action</option>
-				<option value="3">Skip Next Actions</option>
-		 </select>
+		<div><p>This action has been modified by DBM Mods.</p></div><br>
+		<div>
+			<div style="float: left; width: 35%;">
+				Send To:<br>
+				<select id="channel" class="round" onchange="glob.sendTargetChange(this, 'varNameContainer')">
+					${data.sendTargets[isEvent ? 1 : 0]}
+				</select>
+			</div>
+			<div id="varNameContainer" style="display: none; float: right; width: 60%;">
+				Variable Name:<br>
+				<input id="varName" class="round" type="text" list="variableList"><br>
+			</div>
+		</div><br><br><br>
+		<div style="padding-top: 8px;">
+			Message:<br>
+			<textarea id="message" rows="9" placeholder="Insert message here..." style="width: 99%; font-family: monospace; white-space: nowrap; resize: none;"></textarea>
+		</div><br>
+		<div>
+			<div style="float: left; width: 35%;">
+				Store In:<br>
+				<select id="storage" class="round" onchange="glob.variableChange(this, 'varNameContainer2')">
+					${data.variables[0]}
+				</select>
+			</div>
+			<div id="varNameContainer2" style="display: none; float: right; width: 60%;">
+				Variable Name:<br>
+				<input id="varName2" class="round" type="text">
+			</div>
+		</div><br><br><br>
+		<div style="padding-top: 8px;">
+			<div style="float: left; width: 35%;">
+				If Message Delivery Fails:<br>
+				<select id="iffalse" class="round" onchange="glob.onChangeFalse(this)">
+					<option value="0" selected>Continue Actions</option>
+					<option value="1">Stop Action Sequence</option>
+					<option value="2">Jump To Action</option>
+					<option value="3">Skip Next Actions</option>
+					<option value="4">Jump To Anchor</option>
+				</select>
+			</div>
+			<div id="iffalseContainer" style="display: none; float: right; width: 60%;">
+				<span id="iffalseName">Action Number</span>:<br>
+				<input id="iffalseVal" class="round" type="text">
+			</div>
 		</div>
-		<div id="iffalseContainer" style="display: none; float: right; width: 60%;"><span id="iffalseName">Action Number</span>:<br><input id="iffalseVal" class="round" type="text"></div>`;
+	</div>`;
 	},
 
 	init: function() {
 		const { glob, document } = this;
 
+		glob.onChangeFalse = function(event) {
+			switch (parseInt(event.value)) {
+				case 0:
+				case 1:
+					document.getElementById("iffalseContainer").style.display = "none";
+					break;
+				case 2:
+					document.getElementById("iffalseName").innerHTML = "Action Number";
+					document.getElementById("iffalseContainer").style.display = null;
+					break;
+				case 3:
+					document.getElementById("iffalseName").innerHTML = "Number of Actions to Skip";
+					document.getElementById("iffalseContainer").style.display = null;
+					break;
+				case 4:
+					document.getElementById("iffalseName").innerHTML = "Anchor ID";
+					document.getElementById("iffalseContainer").style.display = null;
+					break;
+			}
+		};
 		glob.sendTargetChange(document.getElementById("channel"), "varNameContainer");
 		glob.variableChange(document.getElementById("storage"), "varNameContainer2");
 		glob.onChangeFalse(document.getElementById("iffalse"));
