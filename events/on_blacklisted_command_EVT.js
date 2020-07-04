1b3d153152c6435bde4afb88c2c2846a339779ae
noahalma <40533028+noahalma@users.noreply.github.com>
2020-07-04 14:38:37 -0400

    Create onBlacklist_EVT.js (#21)
    * Create onBlacklist_EVT.js
    
    * Rename onBlacklist_EVT.js to on_blacklisted_command_EVT.js
    
    Co-authored-by: Almeida <almeidx@pm.me>

---
+++ events/on_blacklisted_command_EVT.js
@@ -0,0 +1,25 @@
+module.exports = {
+  name: "On Blacklisted Command",
+  isEvent: true,
+  fields: ["User Who Used Command", "Command Message"],
+  mod: function(DBM) {
+    DBM.RigidKeK = DBM.RigidKeK || {};
+    DBM.RigidKeK.blacklistedUserUse = function(user, message) {
+      const { Bot, Actions } = DBM;
+      const events = Bot.$evts["On Blacklisted Command"];
+      if (!events) return;
+      for (const event of events) {
+        const temp = {}
+        if(event.temp) temp[event.temp] = user;
+        if(event.temp2) temp[event.temp2] = message;
+        const server = user.guild || null;
+        Actions.invokeEvent(event, server, temp)
+      }
+    }
+  const onReady = DBM.Bot.onReady;
+  DBM.Bot.onReady = function(...params) {
+    DBM.Bot.bot.on('blacklistUserUse', DBM.RigidKeK.blacklistedUserUse);
+    onReady.apply(this, ...params);
+  }
+}
+};
