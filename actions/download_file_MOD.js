62c3993888e8727f88436528812879db01fdb0f4
RigidStudios <34319439+RigidStudios@users.noreply.github.com>
2020-07-08 18:05:20 +0200

    WIP

--- actions/download_file_MOD.js
+++ actions/download_file_MOD.js
@@ -67,94 +67,15 @@ module.exports = {
   action (cache) {
     const data = cache.actions[cache.index]
 
-    const url = this.evalMessage(data.url, cache)
+    let url = this.evalMessage(data.url, cache)
     let fileName = this.evalMessage(data.fileName, cache)
     let fileFormat = this.evalMessage(data.fileFormat, cache)
     const filePath = this.evalMessage(data.filePath, cache)
-
-    if (!url) {
-      console.log(`Action: #${cache.index + 1} | Download File ERROR: Web URL has nothing`)
-      this.callNextAction(cache)
-      return
-    }
-
-    if (!fileName || !fileFormat) {
-      const x = url.replace(/(\/|\\)+$/, '').split('/')
-      const y = x[x.length - 1]
-
-      const arrayy = []
-      const regex = new RegExp(/\./, 'g')
-      let rE
-      while (rE = regex.exec(y)) {
-        arrayy.push(rE)
-      }
-
-      if (arrayy.length === 0 || !y.substring(arrayy[arrayy.length - 1].index + 1)) {
-        if (!fileName && !fileFormat) {
-          console.log(`Action: #${cache.index + 1} | Download File ERROR: File Name and File Format has nothing`)
-          this.callNextAction(cache)
-          return
-        } if (!fileName) {
-          console.log(`Action: #${cache.index + 1} | Download File ERROR: File Name has nothing`)
-          this.callNextAction(cache)
-          return
-        } if (!fileFormat) {
-          console.log(`Action: #${cache.index + 1} | Download File ERROR: File Format has nothing`)
-          this.callNextAction(cache)
-          return
-        }
-      } else {
-        const fN = y.substring(0, arrayy[arrayy.length - 1].index)
-        const fF = y.substring(arrayy[arrayy.length - 1].index + 1)
-
-        if (!fileName && !fileFormat) {
-          fileName = fN
-          fileFormat = fF
-        } else if (!fileName) {
-          fileName = fN
-        } else if (!fileFormat) {
-          fileFormat = fF
-        }
-      }
-    }
-
-    if (!filePath) {
-      console.log(`Action: #${cache.index + 1} | Download File ERROR: File Path has nothing`)
-      this.callNextAction(cache)
-      return
-    }
-
-    function gR (input) {
-      const illegalRe = /[/?<>\\:*|":]/g
-      // eslint-disable-next-line no-control-regex
-      const controlRe = /[\x00-\x1f\x80-\x9f]/g
-      const reservedRe = /^\.+$/
-      const windowsReservedRe = /^(con|prn|aux|nul|com[0-9]|lpt[0-9])(\..*)?$/i
-      const windowsTrailingRe = /[. ]+$/
-      const rG = input
-        .replace(illegalRe, '')
-        .replace(controlRe, '')
-        .replace(reservedRe, '')
-        .replace(windowsReservedRe, '')
-        .replace(windowsTrailingRe, '')
-      return rG
-    }
-    fileName = gR(fileName)
-    fileFormat = gR(fileFormat)
-
     const Mods = this.getMods()
-    const request = Mods.require('request')
-    const path = require('path')
-    const fs = require('fs')
-
-    if (!fs.existsSync(filePath)) {
-      fs.mkdirSync(filePath)
+    
+    if (!Mods.checkURL(url)) {
+      url = encodeURI(url);
     }
-
-    request.get(url).on('error', (err) => {
-      console.error(`Action: #${cache.index + 1} | Download File ERROR: Web URL not found...\n`, err)
-    }).pipe(fs.createWriteStream(path.resolve(filePath, `${fileName}.${fileFormat}`)))
-    this.callNextAction(cache)
   },
 
   mod () {}
