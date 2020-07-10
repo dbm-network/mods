9255f85d30edfdaa6cb9f90721d620d84d26546d
RigidStudios <34319439+RigidStudios@users.noreply.github.com>
2020-07-10 17:58:49 +0200

    Refactored Download File (#431)
    * WIP
    
    * Update download_file_MOD.js

--- actions/download_file_MOD.js
+++ actions/download_file_MOD.js
@@ -3,31 +3,31 @@ module.exports = {
   section: 'File Stuff',
 
   subtitle (data) {
-    return `${data.url}`
+    return `From: ${data.url} to ${data.filePath}/${data.fileName || "download"}.${data.fileFormat || "txt"}`
   },
 
   fields: ['url', 'fileName', 'fileFormat', 'filePath'],
 
   html () {
     return `
-<div style="float: left;">
-  Web URL:<br>
-  <input id="url" class="round" type="text" style="width: 522px" oninput="glob.onInput1(this)"><br>
-</div><br><br><br>
-<div style="float: left;">
-  <div style="float: left; width: 60%;">
-  File Name:<br>
-  <input id="fileName" class="round" type="text" style="width: 400px"><br>
-  </div>
-  <div style="float: left; width: 35%; padding-left: 100px;">
-  File Format:<br>
-  <input id="fileFormat" class="round" type="text" style="width: 100px"><br>
-  </div>
-</div><br><br><br><br>
-<div style="float: left;">
-  File Path:<br>
-  <input id="filePath" class="round" type="text" style="width: 522px" value="./downloads"><br>
-</div><br><br><br><br>
+  <div style="float: left;">
+    Web URL:<br>
+    <input id="url" class="round" type="text" style="width: 522px" oninput="glob.onInput1(this)"><br>
+  </div><br><br><br>
+  <div style="float: left;">
+    <div style="float: left; width: 60%;">
+    File Name:<br>
+    <input id="fileName" class="round" type="text" style="width: 400px"><br>
+    </div>
+    <div style="float: left; width: 35%; padding-left: 100px;">
+    File Format:<br>
+    <input id="fileFormat" class="round" type="text" style="width: 100px"><br>
+    </div>
+  </div><br><br><br><br>
+  <div style="float: left;">
+    File Path:<br>
+    <input id="filePath" class="round" type="text" style="width: 522px" value="./downloads"><br>
+  </div><br><br><br><br>
 <p>
   <u><b><span style="color: white;">NOTE:</span></b></u><br>
   In File Path, "./" represents the path to your bot folder<br>
@@ -67,94 +67,34 @@ module.exports = {
   action (cache) {
     const data = cache.actions[cache.index]
 
-    const url = this.evalMessage(data.url, cache)
+    let url = this.evalMessage(data.url, cache)
     let fileName = this.evalMessage(data.fileName, cache)
     let fileFormat = this.evalMessage(data.fileFormat, cache)
     const filePath = this.evalMessage(data.filePath, cache)
-
-    if (!url) {
-      console.log(`Action: #${cache.index + 1} | Download File ERROR: Web URL has nothing`)
-      this.callNextAction(cache)
-      return
-    }
-
-    if (!fileName || !fileFormat) {
-      const x = url.replace(/(\/|\\)+$/, '').split('/')
-      const y = x[x.length - 1]
-
-      const arrayy = []
-      const regex = new RegExp(/\./, 'g')
-      let rE
-      while (rE = regex.exec(y)) {
-        arrayy.push(rE)
-      }
-
-      if (arrayy.length === 0 || !y.substring(arrayy[arrayy.length - 1].index + 1)) {
-        if (!fileName && !fileFormat) {
-          console.log(`Action: #${cache.index + 1} | Download File ERROR: File Name and File Format has nothing`)
-          this.callNextAction(cache)
-          return
-        } if (!fileName) {
-          console.log(`Action: #${cache.index + 1} | Download File ERROR: File Name has nothing`)
-          this.callNextAction(cache)
-          return
-        } if (!fileFormat) {
-          console.log(`Action: #${cache.index + 1} | Download File ERROR: File Format has nothing`)
-          this.callNextAction(cache)
-          return
-        }
-      } else {
-        const fN = y.substring(0, arrayy[arrayy.length - 1].index)
-        const fF = y.substring(arrayy[arrayy.length - 1].index + 1)
-
-        if (!fileName && !fileFormat) {
-          fileName = fN
-          fileFormat = fF
-        } else if (!fileName) {
-          fileName = fN
-        } else if (!fileFormat) {
-          fileFormat = fF
-        }
-      }
-    }
-
-    if (!filePath) {
-      console.log(`Action: #${cache.index + 1} | Download File ERROR: File Path has nothing`)
-      this.callNextAction(cache)
-      return
-    }
-
-    function gR (input) {
-      const illegalRe = /[/?<>\\:*|":]/g
-      // eslint-disable-next-line no-control-regex
-      const controlRe = /[\x00-\x1f\x80-\x9f]/g
-      const reservedRe = /^\.+$/
-      const windowsReservedRe = /^(con|prn|aux|nul|com[0-9]|lpt[0-9])(\..*)?$/i
-      const windowsTrailingRe = /[. ]+$/
-      const rG = input
-        .replace(illegalRe, '')
-        .replace(controlRe, '')
-        .replace(reservedRe, '')
-        .replace(windowsReservedRe, '')
-        .replace(windowsTrailingRe, '')
-      return rG
-    }
-    fileName = gR(fileName)
-    fileFormat = gR(fileFormat)
-
     const Mods = this.getMods()
-    const request = Mods.require('request')
-    const path = require('path')
+    const http = require('https')
     const fs = require('fs')
-
-    if (!fs.existsSync(filePath)) {
-      fs.mkdirSync(filePath)
+    const path = require('path')
+    const fullFilePath = `${filePath}/${fileName || "download"}.${fileFormat || "txt"}`
+    
+    // Check/Re-Encode URL
+    if (!Mods.checkURL(url)) {
+      url = encodeURI(url);
     }
-
-    request.get(url).on('error', (err) => {
-      console.error(`Action: #${cache.index + 1} | Download File ERROR: Web URL not found...\n`, err)
-    }).pipe(fs.createWriteStream(path.resolve(filePath, `${fileName}.${fileFormat}`)))
-    this.callNextAction(cache)
+    
+    // Check if File Exists, if not, create it.
+    if (!fs.existsSync(fullFilePath)) {
+      fs.writeFileSync(fullFilePath, '')
+    }
+    
+    // Initiate Write Stream
+    let ws = fs.createWriteStream(fullFilePath)
+    ws.on('open', (s) => {
+      const request = http.get(url, (response) => {
+        // Write Data
+        response.pipe(ws)
+      })
+    })
   },
 
   mod () {}
