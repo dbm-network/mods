c30075183af323bd878028f0c3826f831a75d163
Almeeida <almeidx@pm.me>
2020-07-10 19:12:01 +0100

    fixed up download file

--- actions/download_file_MOD.js
+++ actions/download_file_MOD.js
@@ -3,31 +3,31 @@ module.exports = {
   section: 'File Stuff',
 
   subtitle (data) {
-    return `From: ${data.url} to ${data.filePath}/${data.fileName || "download"}.${data.fileFormat || "txt"}`
+    return `From: ${data.url} to ${data.filePath}/${data.fileName || 'download'}.${data.fileFormat || 'txt'}`
   },
 
   fields: ['url', 'fileName', 'fileFormat', 'filePath'],
 
   html () {
     return `
-  <div style="float: left;">
-    Web URL:<br>
-    <input id="url" class="round" type="text" style="width: 522px" oninput="glob.onInput1(this)"><br>
-  </div><br><br><br>
-  <div style="float: left;">
-    <div style="float: left; width: 60%;">
-    File Name:<br>
-    <input id="fileName" class="round" type="text" style="width: 400px"><br>
-    </div>
-    <div style="float: left; width: 35%; padding-left: 100px;">
-    File Format:<br>
-    <input id="fileFormat" class="round" type="text" style="width: 100px"><br>
-    </div>
-  </div><br><br><br><br>
-  <div style="float: left;">
-    File Path:<br>
-    <input id="filePath" class="round" type="text" style="width: 522px" value="./downloads"><br>
-  </div><br><br><br><br>
+<div style="float: left;">
+  Web URL:<br>
+  <input id="url" class="round" type="text" style="width: 522px"><br>
+</div><br><br><br>
+<div style="float: left;">
+  <div style="float: left; width: 60%;">
+  File Name:<br>
+  <input id="fileName" class="round" type="text" style="width: 400px"><br>
+  </div>
+  <div style="float: left; width: 35%; padding-left: 100px;">
+  File Format:<br>
+  <input id="fileFormat" class="round" type="text" style="width: 100px"><br>
+  </div>
+</div><br><br><br><br>
+<div style="float: left;">
+  File Path:<br>
+  <input id="filePath" class="round" type="text" style="width: 522px" value="./downloads"><br>
+</div><br><br><br><br>
 <p>
   <u><b><span style="color: white;">NOTE:</span></b></u><br>
   In File Path, "./" represents the path to your bot folder<br>
@@ -41,32 +41,25 @@ module.exports = {
     const data = cache.actions[cache.index]
 
     let url = this.evalMessage(data.url, cache)
-    let fileName = this.evalMessage(data.fileName, cache)
-    let fileFormat = this.evalMessage(data.fileFormat, cache)
+    const fileName = this.evalMessage(data.fileName, cache)
+    const fileFormat = this.evalMessage(data.fileFormat, cache)
     const filePath = this.evalMessage(data.filePath, cache)
     const Mods = this.getMods()
     const http = require('https')
     const fs = require('fs')
-    const path = require('path')
-    const fullFilePath = `${filePath}/${fileName || "download"}.${fileFormat || "txt"}`
-    
-    // Check/Re-Encode URL
+    const path = `${filePath}/${fileName || 'download'}.${fileFormat || 'txt'}`
+
     if (!Mods.checkURL(url)) {
-      url = encodeURI(url);
+      url = encodeURI(url)
     }
-    
-    // Check if File Exists, if not, create it.
-    if (!fs.existsSync(fullFilePath)) {
-      fs.writeFileSync(fullFilePath, '')
+
+    if (!fs.existsSync(path)) {
+      fs.writeFileSync(path, '')
     }
-    
-    // Initiate Write Stream
-    let ws = fs.createWriteStream(fullFilePath)
-    ws.on('open', (s) => {
-      const request = http.get(url, (response) => {
-        // Write Data
-        response.pipe(ws)
-      })
+
+    const ws = fs.createWriteStream(path)
+    ws.on('open', () => {
+      http.get(url, (res) => res.pipe(ws))
     })
   },
 
