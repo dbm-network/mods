a9cb3c16ec6080ac1d5363f589b90efa1467fd9f
Almeida <almeidx@pm.me>
2020-07-19 00:34:06 +0100

    fix: store message params not joining the parameters properly

--- actions/store_message_params_MOD.js
+++ actions/store_message_params_MOD.js
@@ -200,9 +200,9 @@ module.exports = {
         break
       case 1:
         if (data.count) {
-          result = msg.content.split(new RegExp(separator, 'g')).slice(ParamN).slice(0, count).join(separator) || undefined
+          result = msg.content.split(new RegExp(separator, 'g')).slice(ParamN).slice(0, count).join(new RegExp(separator, 'g')) || undefined
         } else {
-          result = msg.content.split(new RegExp(separator, 'g')).slice(ParamN).join(separator) || undefined
+          result = msg.content.split(new RegExp(separator, 'g')).slice(ParamN).join(new RegExp(separator, 'g')) || undefined
         }
         break
       case 2:
