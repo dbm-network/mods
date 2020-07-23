3195b5f5b1be9b6bf976e173a823ecc24c40a946
RigidStudios <34319439+RigidStudios@users.noreply.github.com>
2020-07-23 16:08:14 +0200

    fix: RegExp separator (#466)
    * fix: RegExp separator
    
    * fix separator
    
    Co-authored-by: Almeida <almeidx@pm.me>

--- actions/store_message_params_MOD.js
+++ actions/store_message_params_MOD.js
@@ -200,9 +200,9 @@ module.exports = {
         break
       case 1:
         if (data.count) {
-          result = msg.content.split(new RegExp(separator, 'g')).slice(ParamN).slice(0, count).join(new RegExp(separator, 'g')) || undefined
+          result = msg.content.split(new RegExp(separator, 'g')).slice(ParamN).slice(0, count).join(' ') || undefined
         } else {
-          result = msg.content.split(new RegExp(separator, 'g')).slice(ParamN).join(new RegExp(separator, 'g')) || undefined
+          result = msg.content.split(new RegExp(separator, 'g')).slice(ParamN).join(' ') || undefined
         }
         break
       case 2:
