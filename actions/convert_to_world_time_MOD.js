2ec80635d06f6d06f973fe6e2b6abfb08dcf10f9
aman78600 <yellowpandaday@gmail.com>
2020-07-27 00:47:30 +0400

    Update convert_to_world_time_MOD.js

--- actions/convert_to_world_time_MOD.js
+++ actions/convert_to_world_time_MOD.js
@@ -3,7 +3,7 @@ module.exports = {
   section: 'Other Stuff',
 
   subtitle: function (data) {
-    return 'Input a timezone and retrieve its current time.' 
+    return 'Input a timezone and retrieve its current time.'
   },
 
   variableStorage: function (data, varType) {
@@ -46,14 +46,14 @@ module.exports = {
     const Mods = this.getMods()
     var moment = Mods.require('moment-timezone')
     const str = this.evalMessage(data.textbox, cache)
-    const timec = moment().tz(str).format("dddd, MMMM Do YYYY, h:mm:ss a")
+    const timec = moment().tz(str).format('dddd, MMMM Do YYYY, h:mm:ss a')
 
     const storage = parseInt(data.storage)
     const varName = this.evalMessage(data.varName, cache)
     this.storeValue(timec, storage, varName, cache)
     this.callNextAction(cache)
 
-  },
+},
 
   mod: function () { }
 }
