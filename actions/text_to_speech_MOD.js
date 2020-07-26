18c8fb04412613b95bded1accb9820a174dfdf9f
TheRealPikachu <66538588+TheRealPikachu@users.noreply.github.com>
2020-07-26 18:12:40 +0800

    Update text_to_speech_MOD.js

--- actions/text_to_speech_MOD.js
+++ actions/text_to_speech_MOD.js
@@ -1,40 +1,40 @@
 module.exports = {
   name: 'Text To Speech',
   section: 'Messaging',
- 
-  subtitle: function(data) {
-    return `Make your Discord bot talk.`;
-  },
 
-  variableStorage: function(data, varType) {
+  subtitle: function(data){
+    return 'Make your Discord bot talk.'
+},
+
+  variableStorage: function(data, varType){
     const type = parseInt(data.storage)
-    if (type !== varType) return;
+    if (type !== varType) return
     const dataType = 'Audio URL'
     return ([data.varName, dataType])
-  },
+},
 
   fields: ['text', 'storage', 'varName'],
 
-  html: function(isEvent, data) {
-  return `
+  html: function(isEvent, data){
+    return `
     <div><p>Please pair this with Join Voice Channel & Play URL. Store Audio URL In stores an Audio URL. Please paste this in Play URL for full effect.</p></div><br>
     <div style="width: 90%;">
         Message (to be converted to speech):<br>
         <input id="text" class="round" type="text">
     </div><br>
     </div>
-	<div style="float: left; width: 35%;">
+  <div style="float: left; width: 35%;">
         Store Audio URL In:<br>
         <select id="storage" class="round">
         ${data.variables[1]}
-		</select>
+  </select>
     </div>
     <div id="varNameContainer" style="float: right; width: 60%;">
         Variable Name:<br>
         <input id="varName" class="round" type="text">
     </div>
     </div>`;
-  },
+},
   
   init: function () {},
 
