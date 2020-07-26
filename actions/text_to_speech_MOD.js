123856a57b9bece2717515745913193267d1808a
Almeida <almeidx@pm.me>
2020-07-26 12:23:03 +0100

    fix html syntax errors

--- actions/text_to_speech_MOD.js
+++ actions/text_to_speech_MOD.js
@@ -16,26 +16,24 @@ module.exports = {
   fields: ['text', 'storage', 'varName'],
 
   html: function (isEvent, data) {
-    return 
-    <div><p>Please pair this with Join Voice Channel & Play URL. Store Audio URL In stores an Audio URL. Please paste this in Play URL for full effect.</p>
-    </div>
-    <br>
-    <div style="width: 90%;">
-        Message (to be converted to speech):<br>
-        <input id="text" class="round" type="text">
-    </div><br>
-    </div>
+    return `
+<div>
+  <p>Please pair this with Join Voice Channel & Play URL. Store Audio URL In stores an Audio URL. Please paste this in Play URL for full effect.</p>
+</div><br>
+<div style="width: 90%;">
+  Message (to be converted to speech):<br>
+  <input id="text" class="round" type="text">
+</div><br>
 <div style="float: left; width: 35%;">
-        Store Audio URL In:<br>
-        <select id="storage" class="round">
-        ${data.variables[1]}
-    </select>
-    </div>
-    <div id="varNameContainer" style="float: right; width: 60%;">
-        Variable Name:<br>
-        <input id="varName" class="round" type="text">
-    </div>
-    </div>;
+  Store Audio URL In:<br>
+  <select id="storage" class="round">
+    ${data.variables[1]}
+  </select>
+</div>
+<div id="varNameContainer" style="float: right; width: 60%;">
+  Variable Name:<br>
+  <input id="varName" class="round" type="text">
+</div>`;
   },
 
   init: function () { },
