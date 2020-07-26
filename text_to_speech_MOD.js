bfd98da437580eba6c55226580f9db460f557fd8
TheRealPikachu <66538588+TheRealPikachu@users.noreply.github.com>
2020-07-26 12:59:53 +0800

    Create text_to_speech_MOD.js

---
+++ text_to_speech_MOD.js
@@ -0,0 +1,54 @@
+module.exports = {
+  name: 'Text To Speech',
+  section: 'Messaging',
+  
+  subtitle: function(data) {
+    const info = ['Item 1', 'Item 2', 'Item 3'];
+    return `Make your Discord bot talk.`;
+  },
+
+  variableStorage: function(data, varType) {
+    const type = parseInt(data.storage);
+    if (type !== varType) return;
+    const dataType = 'Audio URL';
+    return ([data.varName, dataType]);
+  },
+
+  fields: ['text', 'info', 'storage', 'varName'],
+
+  html: function(isEvent, data) {
+  return `
+    <div><p>Please pair this with Join Voice Channel & Play URL. Store Audio URL In stores an Audio URL. Please paste this in Play URL for full effect.</p></div><br>
+    <div style="width: 90%;">
+        Message (to be converted to speech):<br>
+        <input id="text" class="round" type="text">
+    </div><br>
+    </div>
+	<div style="float: left; width: 35%;">
+        Store Audio URL In:<br>
+        <select id="storage" class="round">
+        ${data.variables[1]}
+		</select>
+    </div>
+    <div id="varNameContainer" style="float: right; width: 60%;">
+        Variable Name:<br>
+        <input id="varName" class="round" type="text">
+    </div>
+    </div>`;
+  },
+  
+  init: function () {},
+
+  action: async function(cache) {
+    const data = cache.actions[cache.index];
+    const storage = parseInt(data.storage);
+    const varName = this.evalMessage(data.varName, cache);
+    const text = this.evalMessage(data.text, cache);
+    const Mods = this.getMods();
+    const tts = Mods.require('google-tts-api');
+	let play = await tts(text, 'en', 1);
+	this.storeValue(play, storage, varName, cache);
+	this.callNextAction(cache);
+  },
+  mod: function () {},
+};
