0f5b033eb6dfddb8e972e0ee66e44a9c3c290ef0
TheRealPikachu <66538588+TheRealPikachu@users.noreply.github.com>
2020-07-26 19:28:06 +0800

    Update text_to_speech_MOD.js

--- actions/text_to_speech_MOD.js
+++ actions/text_to_speech_MOD.js
@@ -16,24 +16,28 @@ module.exports = {
   fields: ['text', 'storage', 'varName'],
 
   html: function (isEvent, data) {
-    return `
-<div>
-  <p>Please pair this with Join Voice Channel & Play URL. Store Audio URL In stores an Audio URL. Please paste this in Play URL for full effect.</p>
-</div><br>
-<div style="width: 90%;">
-  Message (to be converted to speech):<br>
-  <input id="text" class="round" type="text">
-</div><br>
+    return 
+  <div>  
+  <div>
+    <div><p>Please pair this with Join Voice Channel & Play URL. Store Audio URL In stores an Audio URL. Please paste this in Play URL for full effect.</p>
+    </div>
+    <br>
+    <div style="width: 90%;">
+        Message (to be converted to speech):<br>
+        <input id="text" class="round" type="text">
+    </div><br>
+    </div>
 <div style="float: left; width: 35%;">
-  Store Audio URL In:<br>
-  <select id="storage" class="round">
-    ${data.variables[1]}
-  </select>
-</div>
-<div id="varNameContainer" style="float: right; width: 60%;">
-  Variable Name:<br>
-  <input id="varName" class="round" type="text">
-</div>`;
+        Store Audio URL In:<br>
+        <select id="storage" class="round">
+        ${data.variables[1]}
+    </select>
+    </div>
+    <div id="varNameContainer" style="float: right; width: 60%;">
+        Variable Name:<br>
+        <input id="varName" class="round" type="text">
+    </div>
+    </div>;
   },
 
   init: function () { },
