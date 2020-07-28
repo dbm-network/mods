7278f3a88ad08f8503f6d75ed337e74999889337
TheRealPikachu <66538588+TheRealPikachu@users.noreply.github.com>
2020-07-28 22:23:23 +0800

    Update store_youtube_channel_info_MOD.js

--- actions/store_youtube_channel_info_MOD.js
+++ actions/store_youtube_channel_info_MOD.js
@@ -1,18 +1,20 @@
 module.exports = {
-  name: 'Store YouTube Channel Info',
+
+  name: 'Store YouTube Channel Info 2',
   section: 'Audio Control',
 
   subtitle: function (data) {
     // Each item corresponds to each switch statement.
+    const info = ['Item 1', 'Item 2', 'Item 3'];
     // What user sees when previewing actions box on bottom.
-    return 'Store information about a YouTube channel.'
+    return `Store information about a YouTube channel.`;
   },
 
   variableStorage: function (data, varType) {
-    const type = parseInt(data.storage)
-    if (type !== varType) return
-    const dataType = 'YouTube Channel Info'
-    return ([data.varName, dataType])
+    const type = parseInt(data.storage);
+    if (type !== varType) return;
+    const dataType = 'YouTube Channel Info';
+    return ([data.varName, dataType]);
   },
 
   fields: ['query', 'info', 'storage', 'varName'],
@@ -27,6 +29,9 @@ module.exports = {
         Options:
         <select id="info" class="round">
             <option value="0" selected>Channel ID</option>
+            <option value="1">Channel Name</option>
+            <option value="2">Subscriber Count</option>
+            <option value="3">Video Count</option>
         </select>
       </div><br>
       <div style="padding-top: 8px;">
@@ -40,36 +45,48 @@ module.exports = {
           Variable Name:<br>
           <input id="varName" class="round" type="text">
         </div>
-      </div>`
+      </div>`;
   },
 
-  init: function () {},
-
+  init: function () { },
+  
   action: async function (cache) {
-    const data = cache.actions[cache.index]
-    const storage = parseInt(data.storage)
-    const varName = this.evalMessage(data.varName, cache)
-    const INFO = parseInt(data.info)
-    const query = this.evalMessage(data.query, cache)
+    const data = cache.actions[cache.index];
+    const storage = parseInt(data.storage);
+    const INFO = parseInt(data.info);
+    const varName = this.evalMessage(data.varName, cache);
+    const query = this.evalMessage(data.query, cache);
     const Mods = this.getMods()
-    const getYoutubeChannelId = Mods.require('get-youtube-channel-id')
+    const getYoutubeChannelId = Mods.require('get-youtube-channel-id');
     var url = query
     var reezult = false
     reezult = await getYoutubeChannelId(url)
-    console.log(reezult)
+    console.log(reezult);
     if (reezult !== false) {
       if (reezult.error) {
-        console.log('An error occurred, something is wrong with your action.')
+        console.log(`An error occurred, something is wrong with your action.`);
       } else {
-        console.log('Channel ID: ${reezult.id}')
+        console.log(`Channel ID: ${reezult.id}`);
       }
     } else {
-      console.log('Invalid youtube channel URL')
+      console.log('Invalid youtube channel URL');
     }
-
+    const test = Mods.require('yt-scraper')
+    const channelId = 'UCXuqSBlHAE6Xw-yeJA0Tunw'
+    const testresponse = await test.channelInfo(url)
+	console.log(testresponse.name)
     switch (INFO) {
       case 0:
-        const result = reezult.id
+        result = reezult.id;
+        break;
+      case 1:
+         result = testresponse.name;
+        break;
+      case 2:
+        result = testresponse.author;
+        break;
+      case 3:
+        result = videocount;
         break;
     }
 
@@ -77,5 +94,5 @@ module.exports = {
     this.callNextAction(cache);
   },
 
-  mod: function () {},
+  mod: function (DBM) { },
 };
