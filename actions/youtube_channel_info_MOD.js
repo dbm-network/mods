809d9cda26958683f7082cb91bfda9523efbf8dc
TheRealPikachu <66538588+TheRealPikachu@users.noreply.github.com>
2020-07-29 17:40:22 +0800

    Update youtube_channel_info_MOD.js

--- actions/youtube_channel_info_MOD.js
+++ actions/youtube_channel_info_MOD.js
@@ -3,14 +3,11 @@ module.exports = {
   name: 'Store YouTube Channel Info',
   section: 'Audio Control',
 
-  subtitle: function (data) {
-    // Each item corresponds to each switch statement.
-    const INFO = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6', 'Item 7'];
-    // What user sees when previewing actions box on bottom.
-    return 'Stre information about a YouTube channel.'
+  subtitle (data) {
+    return 'Store information about a YouTube channel.'
   },
 
-  variableStorage: function (data, varType) {
+  variableStorage (data, varType) {
     const type = parseInt(data.storage)
     if (type !== varType) return
     const dataType = 'YouTube Channel Info'
@@ -19,43 +16,43 @@ module.exports = {
 
   fields: ['query', 'info', 'storage', 'varName'],
 
-  html: function (isEvent, data) {
+  html (isEvent, data) {
     return `
-      <div style="width: 100%;">
-        YouTube Channel URL:<br>
-        <input id="query" class="round" type="text">
-      </div><br>
-      <div style="padding-top: 8px; width: 60%;">
-        Options:
-        <select id="info" class="round">
-            <option value="0" selected>Channel ID</option>
-            <option value="1">Channel Name</option>
-            <option value="2">Channel Creation Date</option>
-            <option value="3">Channel Location</option>
-            <option value="4">Channel Description</option>
-            <option value="5">Subscriber Count</option>
-            <option value="6">View Count</option>
-            <option value="7">Is Family Friendly?</option>
-            <option value="8">Channel Keywords</option>
-        </select>
-      </div><br>
-      <div style="padding-top: 8px;">
-        <div style="float: left; width: 35%;">
-          Store In:<br>
-          <select id="storage" class="round">
-            ${data.variables[1]}
-          </select>
-        </div>
-        <div id="varNameContainer" style="float: right; width: 60%;">
-          Variable Name:<br>
-          <input id="varName" class="round" type="text">
-        </div>
-      </div>`
+<div style="width: 100%;">
+  YouTube Channel URL:<br>
+  <input id="query" class="round" type="text">
+</div><br>
+<div style="padding-top: 8px; width: 60%;">
+  Options:
+  <select id="info" class="round">
+    <option value="0" selected>Channel ID</option>
+    <option value="1">Channel Name</option>
+    <option value="2">Channel Creation Date</option>
+    <option value="3">Channel Location</option>
+    <option value="4">Channel Description</option>
+    <option value="5">Subscriber Count</option>
+    <option value="6">View Count</option>
+    <option value="7">Is Family Friendly?</option>
+    <option value="8">Channel Keywords</option>
+   </select>
+</div><br>
+<div style="padding-top: 8px;">
+  <div style="float: left; width: 35%;">
+    Store In:<br>
+    <select id="storage" class="round">
+      ${data.variables[1]}
+    </select>
+  </div>
+  <div id="varNameContainer" style="float: right; width: 60%;">
+    Variable Name:<br>
+    <input id="varName" class="round" type="text">
+  </div>
+</div>`
   },
 
-  init: function () { },
+  init () { },
 
-  action: async function (cache) {
+  async action (cache) {
     const data = cache.actions[cache.index]
     const storage = parseInt(data.storage)
     const INFO = parseInt(data.info)
@@ -68,16 +65,16 @@ module.exports = {
     const testresponse = await ytscrape.channelInfo(url)
     switch (INFO) {
       case 0:
-        result = reezult.id;
+        result = reezult.id
         break;
       case 1:
-        result = testresponse.name;
+        result = testresponse.name
         break;
       case 2:
-        result = testresponse.joined;
+        result = testresponse.joined
         break;
       case 3:
-        result = testresponse.location;
+        result = testresponse.location
         break;
       case 4:
         result = testresponse.description
@@ -96,9 +93,9 @@ module.exports = {
         break;
     }
 
-    this.storeValue(result, storage, varName, cache);
-    this.callNextAction(cache);
+    this.storeValue(result, storage, varName, cache)
+    this.callNextAction(cache)
   },
 
-  mod: function (DBM) { },
+  mod () {},
 };
