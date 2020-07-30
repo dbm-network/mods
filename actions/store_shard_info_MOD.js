dcb10c7f3d8e73a008e6dba2c042bc530e1ce3d1
Savaşçı <39646922+savascipower@users.noreply.github.com>
2020-07-30 22:56:24 +0300

    Update store_shard_info_MOD.js

--- actions/store_shard_info_MOD.js
+++ actions/store_shard_info_MOD.js
@@ -1,5 +1,3 @@
-// This mod was created by Savaşçı#6666
-// For Discord.js v12
 module.exports = {
   name: 'Store Shard Info',
   section: 'Bot Client Control',
@@ -10,50 +8,24 @@ module.exports = {
   },
 
   variableStorage (data, varType) {
-    const type = parseInt(data.storage)
-    if (type !== varType) return
-    const info = parseInt(data.info)
-    let dataType = 'Unknown Type'
-    switch (info) {
-      case 0: // Total Number of Servers in All Shards
-        dataType = 'Number'
-        break
-      case 1: // Total Number of Members in All Shards
-        dataType = 'Number'
-        break
-      case 2: // Shard's Ping On The Current Server
-        dataType = 'Number'
-        break
-      case 3: // Shard's ID On The Current Server
-        dataType = 'Number'
-        break
-      case 4: // Total Number of Servers in Current Server's Shard
-        dataType = 'Number'
-        break
-      case 5: // Total Count of Members in Current Server's Shard
-        dataType = 'Number'
-        break
-      case 6: // Total Server's List On The Current Server's Shard
-    }
-    return ([data.varName2, dataType])
+    if (parseInt(data.storage) !== varType) return
+    return ([data.varName2, 'Number'])
   },
 
   fields: ['info', 'storage', 'varName2'],
 
   html (isEvent, data) {
     return `
-<p>This Mod Was Created By Savaşçı#6666</p>
-<p>For Discord.js v12</p>
 <div style="float: left; width: 80%; padding-top: 8px;">
   Source Info:<br>
   <select id="info" class="round">
-      <option value="0">Total Count of Servers (in All Shards)</option>
-      <option value="1">Total Count of Members (in All Shards)</option>
-      <option value="2">Shard's Ping (On The Current Server)</option>
-      <option value="3">Shard's ID (On The Current Server)</option>
-      <option value="4">Total Number of Servers (in Current Server's Shard)</option>
-      <option value="5">Total Count of Members (in Current Server's Shard)</option>
-      <option value="6">Total Server's List (On The Current Server's Shard)</option>
+     <option value="0">Total Count of Servers (in All Shards)</option>
+     <option value="1">Total Count of Members (in All Shards)</option>
+     <option value="2">Shard's Ping (On The Current Server)</option>
+     <option value="3">Shard's ID (On The Current Server)</option>
+     <option value="4">Total Number of Servers (in Current Server's Shard)</option>
+     <option value="5">Total Count of Members (in Current Server's Shard)</option>
+     <option value="6">Total Server's List (On The Current Server's Shard)</option>
   </select>
 </div><br><br><br>
 <div>
