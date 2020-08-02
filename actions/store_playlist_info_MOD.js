a44f34985881a526d1ddc5d2fb369ec93139fbf8
TheRealPikachu <66538588+TheRealPikachu@users.noreply.github.com>
2020-08-02 17:43:51 +0800

    Update store_playlist_info_MOD.js

--- actions/store_playlist_info_MOD.js
+++ actions/store_playlist_info_MOD.js
@@ -1,73 +1,23 @@
 module.exports = {
-  // ---------------------------------------------------------------------
-  // Action Name
-  //
-  // This is the name of the action displayed in the editor.
-  // ---------------------------------------------------------------------
 
   name: 'Store Playlist Info',
-
-  // ---------------------------------------------------------------------
-  // Action Section
-  //
-  // This is the section the action will fall into.
-  // ---------------------------------------------------------------------
-
   section: 'YouTube Tools',
 
-  // ---------------------------------------------------------------------
-  // Action Subtitle
-  //
-  // This function generates the subtitle displayed next to the name.
-  // ---------------------------------------------------------------------
-
-  subtitle: function (data) {
-    // Each item corresponds to each switch statement.
+  subtitle: (data) {
     const INFO = ['Item 1', 'Item 2', 'Item 3']
-    // What user sees when previewing actions box on bottom.
     return 'Store YouTube playlist information.'
   },
-
-  // ---------------------------------------------------------------------
-  // Action Storage Function
-  //
-  // Stores the relevant variable info for the editor.
-  // ---------------------------------------------------------------------
-
-  variableStorage: function (data, varType) {
+  
+  variableStorage: (data, varType) {
     const type = parseInt(data.storage)
     if (type !== varType) return
     const dataType = 'Playlist Info'
     return ([data.varName, dataType])
   },
 
-  // ---------------------------------------------------------------------
-  // Action Fields
-  //
-  // These are the fields for the action. These fields are customized
-  // by creating elements with corresponding IDs in the HTML. These
-  // are also the names of the fields stored in the action's JSON data.
-  // ---------------------------------------------------------------------
-
   fields: ['query', 'info', 'storage', 'varName'],
 
-  // ---------------------------------------------------------------------
-  // Command HTML
-  //
-  // This function returns a string containing the HTML used for
-  // editting actions.
-  //
-  // The "isEvent" parameter will be true if this action is being used
-  // for an event. Due to their nature, events lack certain information,
-  // so edit the HTML to reflect this.
-  //
-  // The "data" parameter stores constants for select elements to use.
-  // Each is an array: index 0 for commands, index 1 for events.
-  // The names are: sendTargets, members, roles, channels,
-  //                messages, servers, variables
-  // ---------------------------------------------------------------------
-
-  html: function (isEvent, data) {
+  html: (isEvent, data) {
     return `
 <div style="width: 90%;">
   Playlist URL:<br>
@@ -96,24 +46,8 @@ module.exports = {
   </div>
 </div>`
   },
-
-  // ---------------------------------------------------------------------
-  // Action Editor Init Code
-  //
-  // When the HTML is first applied to the action editor, this code
-  // is also run. This helps add modifications or setup reactionary
-  // functions for the DOM elements.
-  // ---------------------------------------------------------------------
-
-  init: function () {},
-
-  // ---------------------------------------------------------------------
-  // Action Bot Function
-  //
-  // This is the function for the action within the Bot's Action class.
-  // Keep in mind event calls won't have access to the "msg" parameter,
-  // so be sure to provide checks for variable existance.
-  // ---------------------------------------------------------------------
+  
+  init: () {},
 
   action: async function (cache) {
     const data = cache.actions[cache.index]
@@ -162,14 +96,5 @@ module.exports = {
     this.callNextAction(cache)
   },
 
-  // ---------------------------------------------------------------------
-  // Action Bot Mod
-  //
-  // Upon initialization of the bot, this code is run. Using the bot's
-  // DBM namespace, one can add/modify existing functions if necessary.
-  // In order to reduce conflictions between mods, be sure to alias
-  // functions you wish to overwrite.
-  // ---------------------------------------------------------------------
-
-  mod: function (DBM) {}
+  mod: (DBM) {}
 }
