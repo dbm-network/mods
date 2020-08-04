8058acf7d41313e8e0fc22dc70dc27446a9a4df6
Mindlesscargo <39963404+Mindlesscargo@users.noreply.github.com>
2020-08-03 22:27:26 -0500

    Update server_prefixes_EXT.js

--- extensions/server_prefixes/server_prefixes_EXT.js
+++ extensions/server_prefixes/server_prefixes_EXT.js
@@ -1,112 +1,110 @@
 module.exports = {
-    name: "Server Prefixes",
-    
-    isCommandExtension: false,
-    isEventExtension: true,
-    isEditorExtension: true,
+  name: 'Server Prefixes',
 
-    fields: ["debug"],
+  isCommandExtension: false,
+  isEventExtension: false,
+  isEditorExtension: true,
 
-    defaultFields: {
-        debug: 0
-    },
+  fields: ['debug'],
 
-    size() {
-        return { width: 500, height: 190};
-    },
+  defaultFields: {
+    debug: 0
+  },
 
-    html(data) {
-        return `
-		<div style="float: left; width: 99%; margin-left: auto; margin-right: auto; padding:10px; text-align: center;">
-			<h2>Server Prefixes</h2><hr>
-			<p>
-				Created by <b>Mindlesscargo</b><br>
-				Requires <b><a href="#" onclick="require('child_process').execSync('start https://www.github.com')">control server prefix</a></b>
-            </p>
-            <p>
-                <label for="debug">Debug Mode:</label>
-                <select id="debug" name="debug"><br>
-                    <option label="Disabled" value=0 ${parseInt(data.debug) === 0 ? "selected=true" : ""}>
-                    <option label="Enabled" value=1 ${parseInt(data.debug) === 1 ? "selected=true" : ""}>
-                </select>
-            </p>
-		</div>`
-    },
+  size () {
+    return { width: 500, height: 190 }
+  },
 
-    init() {}, 
+  html (data) {
+    return `
+<div style="float: left; width: 99%; margin-left: auto; margin-right: auto; padding:10px; text-align: center;">
+  <h2>Server Prefixes</h2><hr>
+  <p>
+    Created by <b>Mindlesscargo</b><br>
+    Requires <b><a href="#" onclick="require('child_process').execSync('start https://github.com/dbm-network/mods/tree/master/actions')">control server prefix</a></b>
+  </p>
+  <label for="debug">Debug Mode:</label>
+    <select id="debug" name="debug"><br>
+      <option label="Disabled" value=0 ${parseInt(data.debug) === 0 ? 'selected=true' : ''}>
+      <option label="Enabled" value=1 ${parseInt(data.debug) === 1 ? 'selected=true' : ''}>
+    </select>
+</div>`
+  },
 
-    close(document, data) {
-        data.debug = document.getElementById('debug').value;
-    },
+  init () {},
 
-    load() {},
+  close (document, data) {
+    data.debug = document.getElementById('debug').value
+  },
 
-    save() {
-    },
+  load () {},
 
-    mod(DBM) {
-        const fs = require("fs");
-        const path = require("path");
-        const { Bot, Files, Actions } = DBM;
-        const settingsPath = path.join("./", "data", "serverSettings.json");
+  save () {
+  },
 
-        const loadPrefixes = function(debug) {
-            const client = Bot.bot;
-            if (fs.existsSync(settingsPath)) {
-                console.log("Loading server prefixes...");
-               fs.readFile(settingsPath, function(err, data) {
-                   if (err) {
-                       console.log(err);
-                       return;
-                   }
-                    data = JSON.parse(data);
-                    const servers = Object.keys(data);
-                    for (let server of servers) {
-                        server = client.guilds.cache.get(server);
-                        server.prefix = data[server.id];
-                        if (parseInt(debug)) {
-                            console.log(`Loaded server ${server.id} with prefix ${server.prefix}`);
-                        }
-                    }
-                    console.log("Server prefixes loaded")
-               })
-            } else {
-                console.log("Creating server settings file")
-                fs.writeFile(settingsPath, JSON.stringify({}), (err) => {
-                    if (err) console.error(err);
-                });
+  mod (DBM) {
+    const fs = require('fs')
+    const path = require('path')
+    const { Bot, Files, Actions } = DBM
+    const settingsPath = path.join('./', 'data', 'serverSettings.json')
+
+    const loadPrefixes = function (debug) {
+      const client = Bot.bot
+      if (fs.existsSync(settingsPath)) {
+        console.log('Loading server prefixes...')
+        fs.readFile(settingsPath, function (err, data) {
+          if (err) {
+            console.log(err)
+            return
+          }
+          data = JSON.parse(data)
+          const servers = Object.keys(data)
+          for (let server of servers) {
+            server = client.guilds.cache.get(server)
+            server.prefix = data[server.id]
+            if (parseInt(debug)) {
+              console.log(`Loaded server ${server.id} with prefix ${server.prefix}`)
             }
-        }
-
-        Bot.checkTag = function(msg) {
-            const tag = msg.guild.prefix || Files.data.settings.tag;
-            const separator = Files.data.settings.separator || "\\s+";
-            const content = msg.content.split(new RegExp(separator))[0];
-            if(content.startsWith(tag)) {
-                return content.substring(tag.length);
-            }
-            return null;
-        }
-
-        Bot.checkCommand = function (msg) {
-            let command = this.checkTag(msg);
-            if(command) {
-                if(!this._caseSensitive) {
-                    command = command.toLowerCase();
-                }
-                const cmd = this.$cmds[command];
-                if(cmd) {
-                    Actions.preformActions(msg, cmd);
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        const onReady = Bot.onReady
-        Bot.onReady = function (...params) {
-          loadPrefixes(Files.data.settings ? Files.data.settings["Server Prefixes"].customData["Server Prefixes"].debug : 0);
-          onReady.apply(this, ...params)
-        }
+          }
+          console.log('Server prefixes loaded')
+        })
+      } else {
+        console.log('Creating server settings file')
+        fs.writeFile(settingsPath, JSON.stringify({}), (err) => {
+          if (err) console.error(err)
+        })
+      }
     }
+
+    Bot.checkTag = function (msg) {
+      const tag = msg.guild.prefix || Files.data.settings.tag
+      const separator = Files.data.settings.separator || '\\s+'
+      const content = msg.content.split(new RegExp(separator))[0]
+      if (content.startsWith(tag)) {
+        return content.substring(tag.length)
+      }
+      return null
+    }
+
+    Bot.checkCommand = function (msg) {
+      let command = this.checkTag(msg)
+      if (command) {
+        if (!this._caseSensitive) {
+          command = command.toLowerCase()
+        }
+        const cmd = this.$cmds[command]
+        if (cmd) {
+          Actions.preformActions(msg, cmd)
+          return true
+        }
+      }
+      return false
+    }
+
+    const onReady = Bot.onReady
+    Bot.onReady = function (...params) {
+      loadPrefixes(Files.data.settings ? Files.data.settings['Server Prefixes'].customData['Server Prefixes'].debug : 0)
+      onReady.apply(this, ...params)
+    }
+  }
 }
