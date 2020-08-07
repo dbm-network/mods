55c5664eb2f161568076576b43eea75bda090297
TheRealPikachu <66538588+TheRealPikachu@users.noreply.github.com>
2020-08-07 08:54:21 +0800

    Update send_file_to_webhook_MOD.js

--- actions/send_file_to_webhook_MOD.js
+++ actions/send_file_to_webhook_MOD.js
@@ -1,35 +1,11 @@
 module.exports = {
-  // ---------------------------------------------------------------------
-  // Action Name
-  //
-  // This is the name of the action displayed in the editor.
-  // ---------------------------------------------------------------------
 
   name: 'Send File To Webhook',
-
-  // ---------------------------------------------------------------------
-  // Action Section
-  //
-  // This is the section the action will fall into.
-  // ---------------------------------------------------------------------
-
   section: 'Webhook Control',
-
-  // ---------------------------------------------------------------------
-  // Action Subtitle
-  //
-  // This function generates the subtitle displayed next to the name.
-  // ---------------------------------------------------------------------
 
   subtitle (data) {
     return 'Send a file to a webhook.'
   },
-
-  // ---------------------------------------------------------------------
-  // Action Storage Function
-  //
-  // Stores the relevant variable info for the editor.
-  // ---------------------------------------------------------------------
 
   variableStorage (data, varType) {
     const type = parseInt(data.storage)
@@ -38,31 +14,7 @@ module.exports = {
     return ([data.varName, dataType])
   },
 
-  // ---------------------------------------------------------------------
-  // Action Fields
-  //
-  // These are the fields for the action. These fields are customized
-  // by creating elements with corresponding IDs in the HTML. These
-  // are also the names of the fields stored in the action's JSON data.
-  // ---------------------------------------------------------------------
-
   fields: ['url', 'filename'],
-
-  // ---------------------------------------------------------------------
-  // Command HTML
-  //
-  // This function returns a string containing the HTML used for
-  // editting actions.
-  //
-  // The "isEvent" parameter will be true if this action is being used
-  // for an event. Due to their nature, events lack certain information,
-  // so edit the HTML to reflect this.
-  //
-  // The "data" parameter stores constants for select elements to use.
-  // Each is an array: index 0 for commands, index 1 for events.
-  // The names are: sendTargets, members, roles, channels,
-  //                messages, servers, variables
-  // ---------------------------------------------------------------------
 
   html (isEvent, data) {
     return `
@@ -77,23 +29,7 @@ module.exports = {
 </div>`
   },
 
-  // ---------------------------------------------------------------------
-  // Action Editor Init Code
-  //
-  // When the HTML is first applied to the action editor, this code
-  // is also run. This helps add modifications or setup reactionary
-  // functions for the DOM elements.
-  // ---------------------------------------------------------------------
-
   init () {},
-
-  // ---------------------------------------------------------------------
-  // Action Bot Function
-  //
-  // This is the function for the action within the Bot's Action class.
-  // Keep in mind event calls won't have access to the "msg" parameter,
-  // so be sure to provide checks for variable existance.
-  // ---------------------------------------------------------------------
 
   action (cache) {
     const data = cache.actions[cache.index]
@@ -108,15 +44,6 @@ module.exports = {
 
     this.callNextAction(cache)
   },
-
-  // ---------------------------------------------------------------------
-  // Action Bot Mod
-  //
-  // Upon initialization of the bot, this code is run. Using the bot's
-  // DBM namespace, one can add/modify existing functions if necessary.
-  // In order to reduce conflictions between mods, be sure to alias
-  // functions you wish to overwrite.
-  // ---------------------------------------------------------------------
 
   mod (DBM) {}
 }
