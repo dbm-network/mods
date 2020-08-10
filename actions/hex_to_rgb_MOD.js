1d2ac71825b7948a1588135d5da55ce9a5895ee3
icomrade1 <66926663+icomrade1@users.noreply.github.com>
2020-08-11 00:33:20 +0300

    Update hex_to_rgb_MOD.js

--- actions/hex_to_rgb_MOD.js
+++ actions/hex_to_rgb_MOD.js
@@ -1,20 +1,20 @@
-  module.exports = {
+module.exports = {
   name: 'Hex to RGB',
   section: 'Other Stuff',
   subtitle (data) {
     return `${data.varName} - Hex to RGB`
-},
+  },
   fields: ['storage', 'varName', 'hexToConvert'],
   html (isEvent, data) {
     return `Hex to convert (example: #F0F0F0):<br><input id="hexToConvert" class="round" type="text" /><br><br>Store in:<br><select class="round" id="storage">${data.variables[0]}</select><br><br>Variable name:<br><input class="round" id="varName" type="text" />`
-    },
-    init () {
-    },
-    action (cache) {
-    const data = cache.actions[cache.index];
-    const hexToConvert = this.evalMessage(data.hexToConvert, cache);
-    const storage = parseInt(data.storage);
-    const varName = this.evalMessage(data.varName, cache);
+  },
+  init () {
+  },
+  action (cache) {
+    const data = cache.actions[cache.index]
+    const hexToConvert = this.evalMessage(data.hexToConvert, cache)
+    const storage = parseInt(data.storage)
+    const varName = this.evalMessage(data.varName, cache)
   function getRGB(color) {
     if (color.length == 7) {
         let r = parseInt(color.substr(1,2),16);
