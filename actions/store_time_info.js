2fd99eed1386e4c1c9d4d8398acbe4c49a3970f9
aman78600 <yellowpandaday@gmail.com>
2020-08-19 08:33:35 +0400

    Updated Store_time_info

--- actions/store_time_info.js
+++ actions/store_time_info.js
@@ -1,38 +1,14 @@
 module.exports = {
-	//---------------------------------------------------------------------
-	// Action Name
-	//
-	// This is the name of the action displayed in the editor.
-	//---------------------------------------------------------------------
 
 	name: "Store Time Info",
-
-	//---------------------------------------------------------------------
-	// Action Section
-	//
-	// This is the section the action will fall into.
-	//---------------------------------------------------------------------
-
 	section: "Other Stuff",
 
-	//---------------------------------------------------------------------
-	// Action Subtitle
-	//
-	// This function generates the subtitle displayed next to the name.
-	//---------------------------------------------------------------------
-
-	subtitle: function(data) {
+	subtitle (data) {
 		const time = ["Year", "Month", "Day of the Month", "Hour", "Minute", "Second", "Milisecond", "Month Text"];
-		return `${time[parseInt(data.type)]}`;
+		return '${time[parseInt(data.type)]}';
 	},
 
-	//---------------------------------------------------------------------
-	// Action Storage Function
-	//
-	// Stores the relevant variable info for the editor.
-	//---------------------------------------------------------------------
-
-	variableStorage: function(data, varType) {
+	variableStorage (data, varType) {
 		const type = parseInt(data.storage);
 		if(type !== varType) return;
 		let result = "Number";
@@ -42,33 +18,9 @@ module.exports = {
 		return ([data.varName, result]);
 	},
 
-	//---------------------------------------------------------------------
-	// Action Fields
-	//
-	// These are the fields for the action. These fields are customized
-	// by creating elements with corresponding IDs in the HTML. These
-	// are also the names of the fields stored in the action's JSON data.
-	//---------------------------------------------------------------------
-
 	fields: ["type", "storage", "varName"],
 
-	//---------------------------------------------------------------------
-	// Command HTML
-	//
-	// This function returns a string containing the HTML used for
-	// editting actions.
-	//
-	// The "isEvent" parameter will be true if this action is being used
-	// for an event. Due to their nature, events lack certain information,
-	// so edit the HTML to reflect this.
-	//
-	// The "data" parameter stores constants for select elements to use.
-	// Each is an array: index 0 for commands, index 1 for events.
-	// The names are: sendTargets, members, roles, channels,
-	//                messages, servers, variables
-	//---------------------------------------------------------------------
-
-	html: function(isEvent, data) {
+	html (isEvent, data) {
 		return `
 <div>
 	<div style="padding-top: 8px; width: 70%;">
@@ -99,25 +51,9 @@ module.exports = {
 </div>`;
 	},
 
-	//---------------------------------------------------------------------
-	// Action Editor Init Code
-	//
-	// When the HTML is first applied to the action editor, this code
-	// is also run. This helps add modifications or setup reactionary
-	// functions for the DOM elements.
-	//---------------------------------------------------------------------
+	init () {},
 
-	init: function() {},
-
-	//---------------------------------------------------------------------
-	// Action Bot Function
-	//
-	// This is the function for the action within the Bot's Action class.
-	// Keep in mind event calls won't have access to the "msg" parameter,
-	// so be sure to provide checks for variable existance.
-	//---------------------------------------------------------------------
-
-	action: function(cache) {
+	action (cache) {
 		const data = cache.actions[cache.index];
 		const type = parseInt(data.type);
 		const date = new Date();
@@ -159,14 +95,5 @@ module.exports = {
 		this.callNextAction(cache);
 	},
 
-	//---------------------------------------------------------------------
-	// Action Bot Mod
-	//
-	// Upon initialization of the bot, this code is run. Using the bot's
-	// DBM namespace, one can add/modify existing functions if necessary.
-	// In order to reduce conflictions between mods, be sure to alias
-	// functions you wish to overwrite.
-	//---------------------------------------------------------------------
-
-	mod: function() {}
-}; // End of module
+	mod () {}
+};
