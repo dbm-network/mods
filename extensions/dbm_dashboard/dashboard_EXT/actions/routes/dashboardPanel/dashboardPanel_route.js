ffc76121a3af8d51e84ff179cadd0f19eb870dc2
Great Plains Modding <46289624+greatplainsmodding@users.noreply.github.com>
2020-08-20 06:09:06 -0500

    Panel Update / Fixes (#552)
    * fixed issue number 513
    
    * Update README.md
    
    * fixed settings not being defined
    
    Co-authored-by: Almeida <almeidx@pm.me>

--- extensions/dbm_dashboard/dashboard_EXT/actions/routes/dashboardPanel/dashboardPanel_route.js
+++ extensions/dbm_dashboard/dashboard_EXT/actions/routes/dashboardPanel/dashboardPanel_route.js
@@ -1,73 +1,72 @@
 module.exports = {
 
-  // ----------------------------------------------------------------------------------
-  // Ran when the dashboard if first started
-  init: async (DBM, Dashboard) => {
-    Dashboard.app.post('/api/:serverID/execute/:command', (req, res) => {
-      if (!req.user) return res.redirect('/dashboard/@me')
+    //----------------------------------------------------------------------------------
+    // Ran when the dashboard if first started    
+    init: async (DBM, Dashboard) => {
+        Dashboard.app.post("/api/:serverID/execute/:command", (req, res) => {
+            if (!req.user) return res.redirect("/dashboard/@me");
 
-      const commandName = req.params.command.toLowerCase().replace(/ /g, '_')
-      const command = Dashboard.Actions.mods.get(commandName)
-      if (command && commandName) {
-        const path = require('path').join(__dirname, '../../mods', commandName, command.scriptFile)
-        const commandFound = require(path)
-        if (!commandFound) return res.status(500)
-        req.user.commandRan = true
+            let commandName = req.params.command.toLowerCase().replace(/ /g,"_");
+            let command = Dashboard.Actions.mods.get(commandName);
+            if (command && commandName) {
+                const path = require("path").join(__dirname, "../../mods", commandName, command.scriptFile);
+                const commandFound = require(path);
+                if (!commandFound) return res.status(500);
+                req.user.commandRan = true;
 
-        req.user.commandExecuted = commandFound.run(DBM, req, res, Dashboard)
-        res.redirect(`/dashboard/@me/servers/${req.params.serverID}`)
-      };
-    })
-  },
-  // ----------------------------------------------------------------------------------
+                req.user.commandExecuted = commandFound.run(DBM, req, res, Dashboard);
+                res.redirect(`/dashboard/@me/servers/${req.params.serverID}`);
+            };
+        });
+    },
+    //----------------------------------------------------------------------------------
 
-  run: (DBM, req, res, Dashboard) => {
-    const server = DBM.Bot.bot.guilds.cache.get(req.params.serverID)
-    if (!server) {
-      res.redirect(`https://discordapp.com/oauth2/authorize?client_id=${DBM.Bot.bot.user.id}&scope=bot&permissions=2146958591&guild_id=${req.params.serverID}`)
-      return {
-        skipRender: true
-      }
-    } else {
-      console.log(server.memberCount)
-      const owners = Dashboard.settings.owner
-      if (!owners.includes(req.user.id)) {
-        res.redirect('/dashboard/@me')
-        return {
-          skipRender: true
-        }
-      };
+    run: (DBM, req, res, next, Dashboard) => {
+        let server = DBM.Bot.bot.guilds.cache.get(req.params.serverID);
 
-      const sections = []
-      const panelMods = []
-      Dashboard.Actions.mods.forEach(mod => {
-        if (mod.dashboardMod) {
-          panelMods.push(mod)
-          sections.push(mod.section)
+        if (!server) {
+            res.redirect(`https://discordapp.com/oauth2/authorize?client_id=${DBM.Bot.bot.user.id}&scope=bot&permissions=2146958591&guild_id=${req.params.serverID}`);
+            return {
+                skipRender: true
+            };
+        } else {
+            if (!Dashboard.isUserGuildManager(req, res, next)) {
+                res.redirect('/dashboard/@me/servers/n');
+                return {
+                    skipRender: true
+                };
+            }
+    
+            let sections = [];
+            let panelMods = [];
+            Dashboard.Actions.mods.forEach(mod => {
+                if (mod.dashboardMod) {
+                    panelMods.push(mod);
+                    sections.push(mod.section);
+                };
+            });
+    
+            let extensions = [];
+            Dashboard.Actions.extensions.forEach(extension => {
+                if (extension.dashboardMod) {
+                    extensions.push(extension);
+                };
+            });
+    
+            return {
+                "user": req.user,
+                "settings": Dashboard.settings,
+                "client": DBM.Bot.bot,
+                "theme": Dashboard.settings.theme,
+                "mods": panelMods,
+                "sections": sections,
+                "extensions": extensions,
+                "commandData": req.user.commandExecuted,
+                "path": require("path"),
+                "dirname": __dirname,
+                "server": server,
+                "commands": DBM.Files.data.commands
+            };
         };
-      })
-
-      const extensions = []
-      Dashboard.Actions.extensions.forEach(extension => {
-        if (extension.dashboardMod) {
-          extensions.push(extension)
-        };
-      })
-
-      return {
-        user: req.user,
-        settings: Dashboard.settings,
-        client: DBM.Bot.bot,
-        theme: Dashboard.settings.theme,
-        mods: panelMods,
-        sections,
-        extensions,
-        commandData: req.user.commandExecuted,
-        path: require('path'),
-        dirname: __dirname,
-        server,
-        commands: DBM.Files.data.commands
-      }
-    };
-  }
-}
+    }
+};;
