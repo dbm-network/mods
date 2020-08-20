860ee9c1e719ac16e5e5aa4141f34ae5ae091d09
RigidStudios <34319439+RigidStudios@users.noreply.github.com>
2020-08-20 10:21:51 +0200

    fix(events): reaction removed in DMs

--- events/message_reaction_removed_EVT.js
+++ events/message_reaction_removed_EVT.js
@@ -10,11 +10,14 @@ module.exports = {
     DBM.Events.reactionRemoved = function (reaction, member) {
       if (!Bot.$evts['Message Reaction Removed MOD']) return
       const server = reaction.message.guild || null
-      if (!server) return; // Stops the event since it's clearly being triggered in the DMs.
+      let user = null
+      if (server) {
+        user = server.members.cache.get(member.id)
+      }
       for (const event of Bot.$evts['Message Reaction Removed MOD']) {
         const temp = {}
         if (event.temp) temp[event.temp] = reaction
-        if (event.temp2) temp[event.temp2] = server.members.cache.get(member.id) || member
+        if (event.temp2) temp[event.temp2] = user
         Actions.invokeEvent(event, server, temp)
       }
     }
