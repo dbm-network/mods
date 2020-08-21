41e36b1f4bd6a1ea0203902c280df76667d23f9b
Danno3817 <detroitwhiskey136@gmail.com>
2020-08-21 15:56:19 -0700

    ignoring eval for you guessed it... eslint

--- extensions/dbm_dashboard/dashboard_EXT/actions/mods/eval/index.js
+++ extensions/dbm_dashboard/dashboard_EXT/actions/mods/eval/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   run: (DBM, req, res, Dashboard) => {
     try {
+      // eslint-disable-next-line no-eval
       let evaled = eval(req.body.code)
       if (typeof evaled !== 'string') evaled = require('util').inspect(evaled)
       return evaled.toString()
