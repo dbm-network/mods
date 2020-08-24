29f7ac67f5ec2caeeb3bbd6d6f6dc20085ee80bd
RigidStudios <34319439+RigidStudios@users.noreply.github.com>
2020-08-24 12:24:51 +0200

    style(events): ESLint compliance

--- events/on_audio_end_EVT.js
+++ events/on_audio_end_EVT.js
@@ -7,6 +7,7 @@ module.exports = {
   mod (DBM) {
     DBM.Events.onAudioEnd = function (guild, voiceChannel) {
       const { Bot, Actions } = DBM
+      if (!Bot.$evts['On Audio End']) { return; }
       for (const event of Bot.$evts['On Audio End']) {
         const temp = {}
         if (event.temp) temp[event.temp] = voiceChannel
