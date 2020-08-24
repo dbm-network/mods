85d1e197f4aa992f307b58458e37780ab3f18435
RigidStudios <34319439+RigidStudios@users.noreply.github.com>
2020-08-24 12:19:53 +0200

    style(events): for...of memory optimization

--- events/on_audio_end_EVT.js
+++ events/on_audio_end_EVT.js
@@ -7,10 +7,7 @@ module.exports = {
   mod (DBM) {
     DBM.Events.onAudioEnd = function (guild, voiceChannel) {
       const { Bot, Actions } = DBM
-      const events = Bot.$evts['On Audio End']
-      if (!events) return
-      for (let i = 0; i < events.length; i++) {
-        const event = events[i]
+      for (const event of Bot.$evts['On Audio End']) {
         const temp = {}
         if (event.temp) temp[event.temp] = voiceChannel
         Actions.invokeEvent(event, guild, temp)
