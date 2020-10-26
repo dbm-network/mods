7c40b6763ab9de2164418c9640a697c59e5a0e46
TheMonDon <bforster48@gmail.com>
2020-10-26 01:49:21 -0500

    .

--- actions/add_member_role.js
+++ actions/add_member_role.js
@@ -1,144 +1,144 @@
 module.exports = {
-	// ---------------------------------------------------------------------
-	// Action Name
-	//
-	// This is the name of the action displayed in the editor.
-	// ---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
+  // Action Name
+  //
+  // This is the name of the action displayed in the editor.
+  // ---------------------------------------------------------------------
   
-	name: 'Add Member Role',
+  name: 'Add Member Role',
   
-	// ---------------------------------------------------------------------
-	// Action Section
-	//
-	// This is the section the action will fall into.
-	// ---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
+  // Action Section
+  //
+  // This is the section the action will fall into.
+  // ---------------------------------------------------------------------
   
-	section: 'Member Control',
+  section: 'Member Control',
   
-	// ---------------------------------------------------------------------
-	// Action Subtitle
-	//
-	// This function generates the subtitle displayed next to the name.
-	// ---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
+  // Action Subtitle
+  //
+  // This function generates the subtitle displayed next to the name.
+  // ---------------------------------------------------------------------
   
-	subtitle(data) {
-	  const roles = ['Mentioned Role', '1st Author Role', '1st Server Role', 'Temp Variable', 'Server Variable', 'Global Variable']
-	  const channels = ['Mentioned User', 'Command Author', 'Temp Variable', 'Server Variable', 'Global Variable']
-	  return `${channels[parseInt(data.member)]} - ${roles[parseInt(data.role)]}`
-	},
+  subtitle(data) {
+    const roles = ['Mentioned Role', '1st Author Role', '1st Server Role', 'Temp Variable', 'Server Variable', 'Global Variable']
+    const channels = ['Mentioned User', 'Command Author', 'Temp Variable', 'Server Variable', 'Global Variable']
+    return `${channels[parseInt(data.member)]} - ${roles[parseInt(data.role)]}`
+  },
   
-	// ---------------------------------------------------------------------
-	// Action Fields
-	//
-	// These are the fields for the action. These fields are customized
-	// by creating elements with corresponding IDs in the HTML. These
-	// are also the names of the fields stored in the action's JSON data.
-	// ---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
+  // Action Fields
+  //
+  // These are the fields for the action. These fields are customized
+  // by creating elements with corresponding IDs in the HTML. These
+  // are also the names of the fields stored in the action's JSON data.
+  // ---------------------------------------------------------------------
   
-	fields: ['member', 'varName2', 'role', 'varName', 'reason'],
+  fields: ['member', 'varName2', 'role', 'varName', 'reason'],
   
-	// ---------------------------------------------------------------------
-	// Command HTML
-	//
-	// This function returns a string containing the HTML used for
-	// editting actions.
-	//
-	// The 'isEvent' parameter will be true if this action is being used
-	// for an event. Due to their nature, events lack certain information,
-	// so edit the HTML to reflect this.
-	//
-	// The 'data' parameter stores constants for select elements to use.
-	// Each is an array: index 0 for commands, index 1 for events.
-	// The names are: sendTargets, members, roles, channels,
-	//        messages, servers, variables
-	// ---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
+  // Command HTML
+  //
+  // This function returns a string containing the HTML used for
+  // editting actions.
+  //
+  // The 'isEvent' parameter will be true if this action is being used
+  // for an event. Due to their nature, events lack certain information,
+  // so edit the HTML to reflect this.
+  //
+  // The 'data' parameter stores constants for select elements to use.
+  // Each is an array: index 0 for commands, index 1 for events.
+  // The names are: sendTargets, members, roles, channels,
+  //        messages, servers, variables
+  // ---------------------------------------------------------------------
   
-	html(isEvent, data) {
-	  return `
-	<div>
-	  <div style='float: left width: 35%'>
-		Source Role:<br>
-		<select id='role' class='round' onchange='glob.roleChange(this, 'varNameContainer')'>
-		  ${data.roles[isEvent ? 1 : 0]}
-		</select>
-	  </div>
-	  <div id='varNameContainer' style='display: none float: right width: 60%'>
-		Variable Name:<br>
-		<input id='varName' class='round' type='text' list='variableList'><br>
-	  </div>
-	</div><br><br><br>
-	<div style='padding-top: 8px'>
-	  <div style='float: left width: 35%'>
-		Member:<br>
-		<select id='member' class='round' onchange='glob.memberChange(this, 'varNameContainer2')'>
-		  ${data.members[isEvent ? 1 : 0]}
-		</select>
-	  </div>
-	  <div id='varNameContainer2' style='display: none float: right width: 60%'>
-		Variable Name:<br>
-		<input id='varName2' class='round' type='text' list='variableList'><br>
-	  </div>
-	</div><br><br><br>
-	<div>
-	  Reason:
-	  <input id='reason' placeholder='Optional' class='round' type='text'>
-	</div>`
-	},
+  html(isEvent, data) {
+    return `
+  <div>
+    <div style='float: left width: 35%'>
+    Source Role:<br>
+    <select id='role' class='round' onchange='glob.roleChange(this, 'varNameContainer')'>
+      ${data.roles[isEvent ? 1 : 0]}
+    </select>
+    </div>
+    <div id='varNameContainer' style='display: none float: right width: 60%'>
+    Variable Name:<br>
+    <input id='varName' class='round' type='text' list='variableList'><br>
+    </div>
+  </div><br><br><br>
+  <div style='padding-top: 8px'>
+    <div style='float: left width: 35%'>
+    Member:<br>
+    <select id='member' class='round' onchange='glob.memberChange(this, 'varNameContainer2')'>
+      ${data.members[isEvent ? 1 : 0]}
+    </select>
+    </div>
+    <div id='varNameContainer2' style='display: none float: right width: 60%'>
+    Variable Name:<br>
+    <input id='varName2' class='round' type='text' list='variableList'><br>
+    </div>
+  </div><br><br><br>
+  <div>
+    Reason:
+    <input id='reason' placeholder='Optional' class='round' type='text'>
+  </div>`
+  },
   
-	// ---------------------------------------------------------------------
-	// Action Editor Init Code
-	//
-	// When the HTML is first applied to the action editor, this code
-	// is also run. This helps add modifications or setup reactionary
-	// functions for the DOM elements.
-	// ---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
+  // Action Editor Init Code
+  //
+  // When the HTML is first applied to the action editor, this code
+  // is also run. This helps add modifications or setup reactionary
+  // functions for the DOM elements.
+  // ---------------------------------------------------------------------
   
-	init() {
-	  const {
-		glob,
-		document
-	  } = this
+  init() {
+    const {
+    glob,
+    document
+    } = this
   
-	  glob.roleChange(document.getElementById('role'), 'varNameContainer')
-	  glob.memberChange(document.getElementById('member'), 'varNameContainer2')
-	},
+    glob.roleChange(document.getElementById('role'), 'varNameContainer')
+    glob.memberChange(document.getElementById('member'), 'varNameContainer2')
+  },
   
-	// ---------------------------------------------------------------------
-	// Action Bot Function
-	//
-	// This is the function for the action within the Bot's Action class.
-	// Keep in mind event calls won't have access to the 'msg' parameter,
-	// so be sure to provide checks for variable existance.
-	// ---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
+  // Action Bot Function
+  //
+  // This is the function for the action within the Bot's Action class.
+  // Keep in mind event calls won't have access to the 'msg' parameter,
+  // so be sure to provide checks for variable existance.
+  // ---------------------------------------------------------------------
   
-	action(cache) {
-	  const data = cache.actions[cache.index]
-	  const storage = parseInt(data.role)
-	  const varName = this.evalMessage(data.varName, cache)
-	  const role = this.getRole(storage, varName, cache)
-	  const storage2 = parseInt(data.member)
-	  const varName2 = this.evalMessage(data.varName2, cache)
-	  const member = this.getMember(storage2, varName2, cache)
-	  const reason = this.evalMessage(data.reason, cache)
-	  if (Array.isArray(member)) {
-		this.callListFunc(member.map((m) => m.roles), 'add', [role, reason]).then(() => this.callNextAction(cache))
-	  } else if (this.dest(member, 'roles', 'add')) {
-		member.roles.add(role, reason)
-		  .then(() => this.callNextAction(cache))
-		  .catch(this.displayError.bind(this, data, cache))
-	  } else {
-		this.callNextAction(cache)
-	  }
-	},
+  action(cache) {
+    const data = cache.actions[cache.index]
+    const storage = parseInt(data.role)
+    const varName = this.evalMessage(data.varName, cache)
+    const role = this.getRole(storage, varName, cache)
+    const storage2 = parseInt(data.member)
+    const varName2 = this.evalMessage(data.varName2, cache)
+    const member = this.getMember(storage2, varName2, cache)
+    const reason = this.evalMessage(data.reason, cache)
+    if (Array.isArray(member)) {
+    this.callListFunc(member.map((m) => m.roles), 'add', [role, reason]).then(() => this.callNextAction(cache))
+    } else if (this.dest(member, 'roles', 'add')) {
+    member.roles.add(role, reason)
+      .then(() => this.callNextAction(cache))
+      .catch(this.displayError.bind(this, data, cache))
+    } else {
+    this.callNextAction(cache)
+    }
+  },
   
-	// ---------------------------------------------------------------------
-	// Action Bot Mod
-	//
-	// Upon initialization of the bot, this code is run. Using the bot's
-	// DBM namespace, one can add/modify existing functions if necessary.
-	// In order to reduce conflictions between mods, be sure to alias
-	// functions you wish to overwrite.
-	// ---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
+  // Action Bot Mod
+  //
+  // Upon initialization of the bot, this code is run. Using the bot's
+  // DBM namespace, one can add/modify existing functions if necessary.
+  // In order to reduce conflictions between mods, be sure to alias
+  // functions you wish to overwrite.
+  // ---------------------------------------------------------------------
   
-	mod() {}
+  mod() {}
   } // End of module
