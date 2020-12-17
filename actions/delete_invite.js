155e66ff9365e7111e3edac043489bc6b0dd250a
Dominus Marceau <marceaupatu123@icloud.com>
2020-12-17 22:07:31 +0100

    fixed to eslint

--- actions/delete_invite.js
+++ actions/delete_invite.js
@@ -1,20 +1,20 @@
 module.exports = {
-  name: "Delete Invite",
-  section: "Invite Control",
+  name: 'Delete Invite',
+  section: 'Invite Control',
 
-  subtitle(data) {
-    return `${data.invite}`;
+  subtitle (data) {
+    return `${data.invite}`
   },
 
-  variableStorage(data, varType) {
-    const type = parseInt(data.storage);
-    if (type !== varType) return;
-    return [data.varName, "Invite"];
+  variableStorage (data, varType) {
+    const type = parseInt(data.storage)
+    if (type !== varType) return
+    return [data.varName, 'Invite']
   },
 
-  fields: ["invite", "storage", "varName"],
+  fields: ['invite', 'storage', 'varName'],
 
-  html(isEvent, data) {
+  html (isEvent, data) {
     return `
 <div>
   <div style="padding-top: 8px;">
@@ -25,28 +25,28 @@ module.exports = {
     Source Invite:<br>
     <textarea class="round" id="reason" rows="2" placeholder="Insert a reason.." style="width: 99%; font-family: monospace; white-space: nowrap; resize: none;"></textarea>
   </div><br>
-</div>`;
+</div>`
   },
 
-  init() {},
+  init () {},
 
-  action(cache) {
-    const data = cache.actions[cache.index];
-    const invite = this.evalMessage(data.invite, cache);
-    const reason = this.evalMessage(data.reason, cache);
-    const client = this.getDBM().Bot.bot;
+  action (cache) {
+    const data = cache.actions[cache.index]
+    const invite = this.evalMessage(data.invite, cache)
+    const reason = this.evalMessage(data.reason, cache)
+    const client = this.getDBM().Bot.bot
 
     client
       .fetchInvite(invite)
       .catch(console.error)
       .then(async (invite) => {
-        if (!invite) this.callNextAction(cache);
+        if (!invite) this.callNextAction(cache)
 
-        invite.delete(reason);
-      });
+        invite.delete(reason)
+      })
 
-    this.callNextAction(cache);
+    this.callNextAction(cache)
   },
 
-  mod() {},
-};
+  mod () {}
+}
