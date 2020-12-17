ff0fa7303b1674b76f42b88825a4946ad9b0db43
DanTheComputerMan <47550063+DanTheComputerMan@users.noreply.github.com>
2020-12-16 17:15:02 -0800

    Update publish_message_MOD.js
    Updated publish_message_MOD.js as the prior iteration sent a message and immediately published it, which was not the intended outcome. This version accepts a message as an input, and THEN publishes that same message.

--- actions/publish_message_MOD.js
+++ actions/publish_message_MOD.js
@@ -3,8 +3,8 @@ module.exports = {
 	section: 'Messaging',
 	
 	subtitle (data) {
-		const channels = ["Same Channel", "Mentioned Channel", "1st Server Channel", "Temp Variable", "Server Variable", "Global Variable"];
-		return `${channels[parseInt(data.channel)]}: "${data.message.replace(/[\n\r]+/, '')}"`;
+		const message = ["Command Message", "Temp Variable", "Server Variable", "Global Variable"];
+		return `${message[parseInt(data.message)]}`;
 	},
 	
 	variableStorage (data, varType) {
@@ -13,120 +13,60 @@ module.exports = {
 		return ([data.varName2, 'Message']);
 	},
 	
-	fields: ['channel', 'varName', 'message', 'storage', 'varName2', 'iffalse', 'iffalseVal'],
+	fields: ['message', 'varName', 'storage', 'varName2'],
 	
 	html (isEvent, data) {
 		return `
-<div style="width: 550px; height: 350px; overflow-y: scroll;">
-  <div>
-    <div style="float: left; width: 35%;">
-      Send To:<br>
-      <select id="channel" class="round" onchange="glob.channelChange(this, 'varNameContainer')">
-        ${data.channels[isEvent ? 1 : 0]}
-      </select>
-    </div>
-    <div id="varNameContainer" style="display: none; float: right; width: 60%;">
-      Variable Name:<br>
-      <input id="varName" class="round" type="text" list="variableList"><br>
-    </div>
-  </div><br><br><br>
-  <div style="padding-top: 8px;">
-    Message:<br>
-    <textarea id="message" rows="9" placeholder="Insert message here..." style="width: 99%; font-family: monospace; white-space: nowrap; resize: none;"></textarea>
-  </div><br>
-  <div>
-    <div style="float: left; width: 35%;">
-      Store In:<br>
-      <select id="storage" class="round" onchange="glob.variableChange(this, 'varNameContainer2')">
-        ${data.variables[0]}
-      </select>
-    </div>
-    <div id="varNameContainer2" style="display: none; float: right; width: 60%;">
-      Variable Name:<br>
-      <input id="varName2" class="round" type="text">
-    </div>
-  </div><br><br><br>
-  <div style="padding-top: 8px;">
-    <div style="float: left; width: 35%;">
-      If Message Delivery Fails:<br>
-      <select id="iffalse" class="round" onchange="glob.onChangeFalse(this)">
-        <option value="0" selected>Continue Actions</option>
-        <option value="1">Stop Action Sequence</option>
-        <option value="2">Jump To Action</option>
-        <option value="3">Skip Next Actions</option>
-        <option value="4">Jump To Anchor</option>
-      </select>
-    </div>
-    <div id="iffalseContainer" style="display: none; float: right; width: 60%;">
-      <span id="iffalseName">Action Number</span>:<br>
-      <input id="iffalseVal" class="round" type="text">
-    </div>
-  </div>
+<div>
+	<div style="float: left; width: 35%;">
+		Source Message:<br>
+		<select id="message" class="round" onchange="glob.messageChange(this, 'varNameContainer')">
+			${data.messages[isEvent ? 1 : 0]}
+		</select>
+	</div>
+	<div id="varNameContainer" style="display: none; float: right; width: 60%;">
+		Variable Name:<br>
+		<input id="varName" class="round" type="text" list="variableList"><br>
+	</div>
+</div><br><br><br>
+<div>
+	<div style="float: left; width: 35%;">
+		Store In:<br>
+		<select id="storage" class="round" onchange="glob.variableChange(this, 'varNameContainer2')">
+			${data.variables[0]}
+		</select>
+	</div>
+	<div id="varNameContainer2" style="float: right; width: 60%;">
+		Variable Name:<br>
+		<input id="varName2" class="round" type="text"><br>
+	</div>
 </div>`
 	},
 	
 	init () {
 		const { glob, document } = this;
 		
-		glob.onChangeFalse = function (event) {
-			switch (parseInt(event.value)) {
-				case 0:
-				case 1:
-				  document.getElementById('iffalseContainer').style.display = 'none';
-				  break;
-				case 2:
-				  document.getElementById('iffalseName').innerHTML = 'Action Number';
-				  document.getElementById('iffalseContainer').style.display = null;
-				  break;
-				case 3:
-				  document.getElementById('iffalseName').innerHTML = 'Number of Actions to Skip';
-				  document.getElementById('iffalseContainer').style.display = null;
-				  break;
-				case 4:
-				  document.getElementById('iffalseName').innerHTML = 'Anchor ID';
-				  document.getElementById('iffalseContainer').style.display = null;
-				  break;
-			}
-		}
-		glob.sendTargetChange(document.getElementById('channel'), 'varNameContainer');
+		glob.sendTargetChange(document.getElementById('message'), 'varNameContainer');
 		glob.variableChange(document.getElementById('storage'), 'varNameContainer2');
-		glob.onChangeFalse(document.getElementById('iffalse'));
 	},
 	
 	action (cache) {
 		const data = cache.actions[cache.index];
-		const channel = parseInt(data.channel);
 		const varName = this.evalMessage(data.varName, cache);
-		const target = this.getSendTarget(channel, varName, cache);
+		const message = this.getMessage(parseInt(data.message), varName, cache);
 		const child = require('child_process');
 		const command = "npm list discord.js";
 		const djsversionoutput = child.execSync(command).toString().replace(/\n|\s/g, '').match(/discord\.js\@(\d+\.\d+\.\d+)/);
-		const { message } = data;
-		if (!target || target.type !== 'news' || !message) return;
-		if (djsversionoutput && djsversionoutput[1] && djsversionoutput[1] < "12.4.0") throw new Error(`You need at least Discord.js version 12.4.0 to use this mod. Your Discord.js version is: ${djsversionoutput[1]}`);
 		
-		if (Array.isArray(target)) {
-			this.callListFunc(target, 'send', [this.evalMessage(message, cache)]).then((msg) => {
-				msg.crosspost().then((m) => {
-					const varName2 = this.evalMessage(data.varName2, cache);
-					const storage = parseInt(data.storage);
-					this.storeValue(m, storage, varName2, cache);
-					this.callNextAction(cache);
-				}).catch(console.error);
-			});
-		} else if (target && target.send) {
-			target.send(this.evalMessage(message, cache))
-			.then((msg) => {
-				msg.crosspost().then((m) => {
-					const varName2 = this.evalMessage(data.varName2, cache);
-					const storage = parseInt(data.storage);
-					this.storeValue(msg, storage, varName2, cache);
-					this.callNextAction(cache);
-				}).catch(console.error);
-			});
-		} else {
-		  this.callNextAction(cache);
-		}
+		if (!message) return;
+		if (!message.crosspost) throw new Error(`You need at least Discord.js version 12.4.0 to use this mod. Your Discord.js version is: ${djsversionoutput[1]}`);
+		
+		message.crosspost().then((msg) => {
+			const varName2 = this.evalMessage(data.varName2, cache);
+			const storage = parseInt(data.storage);
+			this.storeValue(msg, storage, varName2, cache);
+			this.callNextAction(cache);
+		}).catch(console.error);
 	},
 	
 	mod () {}
