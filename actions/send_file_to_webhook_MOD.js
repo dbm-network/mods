5778b7f3f83ba9d6060c1eae9f5cb6e273aa293f
TheMonDon <11539895+TheMonDon@users.noreply.github.com>
2021-05-27 03:06:03 -0500

    Code Reformat

--- actions/send_file_to_webhook_MOD.js
+++ actions/send_file_to_webhook_MOD.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'Send File To Webhook',
   section: 'Webhook Control',
 
-  subtitle (data) {
+  subtitle () {
     return 'Send a file to a webhook'
   },
 
@@ -35,7 +35,6 @@ module.exports = {
 
   init () {
     const { glob, document } = this
-
     glob.refreshVariableList(document.getElementById('storage'))
   },
 
@@ -45,6 +44,7 @@ module.exports = {
     const varName = this.evalMessage(data.varName, cache)
     const Mods = this.getMods()
     const webhook = Mods.getWebhook(storage, varName, cache)
+
     if (!webhook) {
       this.callNextAction(cache)
       return
