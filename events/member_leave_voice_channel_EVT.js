6e64097f216029e8c3471778c864198b236ab478
Lucasamiel0406 <49658726+Lucasamiel0406@users.noreply.github.com>
2021-05-30 01:33:29 -0300

    Update member_leave_voice_channel_EVT.js

--- events/member_leave_voice_channel_EVT.js
+++ events/member_leave_voice_channel_EVT.js
@@ -6,25 +6,24 @@ module.exports = {
 
   mod (DBM) {
     DBM.Events = DBM.Events || {}
-    const { Actions, Bot } = DBM
-
+    const { Actions, Bot } = DBM;
     DBM.Events.memberLeaveVoiceChannel = function (oldVoiceState, newVoiceState) {
       if (!Bot.$evts['Member Leave Voice Channel']) return
-      const oldChannel = oldVoiceState.channel
-      const newChannel = newVoiceState.channel
-      const server = (oldChannel || newChannel).guild
-      if (!(oldChannel && !newChannel)) return
+      const oldChannel = oldVoiceState.channel;
+      const newChannel = newVoiceState.channel;
+      if (oldChannel == null || !(oldChannel && !newChannel)) return
+      const server = (oldChannel || newChannel).guild;
       for (const event of Bot.$evts['Member Leave Voice Channel']) {
-        const temp = {}
-        if (event.temp) temp[event.temp] = oldVoiceState.member
-        if (event.temp2) temp[event.temp2] = oldChannel
-        Actions.invokeEvent(event, server, temp)
+        const temp = {};
+        if (event.temp) temp[event.temp] = oldVoiceState.member;
+        if (event.temp2) temp[event.temp2] = oldChannel;
+        Actions.invokeEvent(event, server, temp);
       }
     }
     const onReady = Bot.onReady
     Bot.onReady = function (...params) {
-      Bot.bot.on('voiceStateUpdate', DBM.Events.memberLeaveVoiceChannel)
-      onReady.apply(this, ...params)
+      Bot.bot.on('voiceStateUpdate', DBM.Events.memberLeaveVoiceChannel);
+      onReady.apply(this, ...params);
     }
   }
 }
