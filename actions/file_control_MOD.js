ab0b7ab7c70eb50d2a14e98fa9dd029aa44d17f6
TheMonDon <11539895+TheMonDon@users.noreply.github.com>
2021-06-27 15:40:49 -0500

    Change to giant try..catch

--- actions/file_control_MOD.js
+++ actions/file_control_MOD.js
@@ -3,174 +3,193 @@ module.exports = {
   section: 'File Stuff',
   fields: ['input', 'format', 'filename', 'filepath', 'filepath2', 'filetask', 'input2', 'togglestatus'],
 
-  subtitle (data) {
+  subtitle(data) {
     const filetasks = ['Create', 'Write', 'Append into', 'Delete', 'Insert into', 'Copy']
     return `${filetasks[parseInt(data.filetask)]} ${data.filename}${data.format}`
   },
 
-  html () {
+  html() {
     return `
-<style>
-  ::-webkit-scrollbar {
-    width: 10px !important;
-  }
-  ::-webkit-scrollbar-track {
-    background: inherit !important;
-  }
-  ::-webkit-scrollbar-thumb:hover {
-    background: #3c4035 !important;
-  }
-  .col-20l {
-    float: left;
-    width: 20%;
-  }
-  .col-17l {
-    float: left;
-    width: 17%;
-  }
-  textarea {
-    float: left;
-    width: 100%;
-    resize: 1;
-    padding: 4px 8px;
-  }
-  span.wrexlink {
-    color: #99b3ff;
-    text-decoration:underline;
-    cursor:pointer;
-  }
-  span.wrexlink:hover {
-    color:#4676b9;
-  }
-  .hidden {
-    display: none;
-  }
-  /* The switch - the box around the slider */
-  .switch {
-    position: relative;
-    display: inline-block;
-    width: 40px;
-    height: 20px;
-  }
-  /* Hide default HTML checkbox */
-  .switch input {
-    opacity: 0;
-    width: 0;
-    height: 0;
-  }
-  /* The slider */
-  .slider {
-    position: absolute;
-    cursor: pointer;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: #ccc;
-    -webkit-transition: .4s;
-    transition: .4s;
-  }
-  .slider:before {
-    position: absolute;
-    content: "";
-    height: 20px;
-    width: 20px;
-    left: 1px;
-    bottom: 0px;
-    background-color: white;
-    -webkit-transition: .4s;
-    transition: .4s;
-  }
-  input:checked + .slider {
-    background-color: #2196F3;
-  }
-  input:focus + .slider {
-    box-shadow: 0 0 1px #2196F3;
-  }
-  input:checked + .slider:before {
-    -webkit-transform: translateX(18px);
-    -ms-transform: translateX(18px);
-    transform: translateX(18px);
-  }
-  /* Rounded sliders */
-  .slider.round {
-    border-radius: 34px;
-  }
-  .slider.round:before {
-    border-radius: 50%;
-  }
-</style>
-<div id="wrexdiv" style="width: 550px; height: 350px; overflow-y: scroll;" onload="showInput()">
-  <div style="padding-bottom: 100px; padding: 5px 15px 5px 5px">
-    <div style="padding: 5px 10px 5px 5px">
-      <div style="display: flex; flex-direction: row;">
-        <div class="col-20l" style="display: flex; flex-direction: column; flex: 2; padding-right: 1%;">
-          Format:<br>
-          <select id="format" class="round">
-            <option value="" selected>OTHER</option>
-            <option value=".log">LOG</option>
-            <option value=".txt">TXT</option>
-            <option value=".json">JSON</option>
-            <option value=".js">JS</option>
-          </select>
-        </div>
-        <div class="col-17l" style="display: flex; flex-direction: column; flex: 2; padding-right: 1%;">
-          Task:<br>
-          <select id="filetask" title="99% of the time, you want append" class="round">
-            <option value="0" title="Only makes the file">Create</option>
-            <option value="1" title="Overwrites the files contents with yours">Write</option>
-            <option value="2" title="Add the content to the end of the file" selected>Append</option>
-            <option value="4" title="Inserts a line in a specific place in the file">Insert</option>
-            <option value="3" title="Deletes a file, Be VERY carefull with this option">Delete</option>
-            <option value="5" title="Copies a file">Copy</option>
-          </select>
-        </div>
-        <div style="display: flex; flex-direction: column; flex: 8;">
-          File Name:<br>
-          <textarea id="filename" title="If 'Other' add the file .format to the end of the file name" placeholder="Example file name 'myreallycoollog'" class="round" type="textarea" rows="1"></textarea>
+    <style>
+    ::-webkit-scrollbar {
+      width: 10px !important;
+    }
+  
+    ::-webkit-scrollbar-track {
+      background: inherit !important;
+    }
+  
+    ::-webkit-scrollbar-thumb:hover {
+      background: #3c4035 !important;
+    }
+  
+    .col-20l {
+      float: left;
+      width: 20%;
+    }
+  
+    .col-17l {
+      float: left;
+      width: 17%;
+    }
+  
+    textarea {
+      float: left;
+      width: 100%;
+      resize: 1;
+      padding: 4px 8px;
+    }
+  
+    span.wrexlink {
+      color: #99b3ff;
+      text-decoration: underline;
+      cursor: pointer;
+    }
+  
+    span.wrexlink:hover {
+      color: #4676b9;
+    }
+  
+    .hidden {
+      display: none;
+    }
+  
+    /* The switch - the box around the slider */
+    .switch {
+      position: relative;
+      display: inline-block;
+      width: 40px;
+      height: 20px;
+    }
+  
+    /* Hide default HTML checkbox */
+    .switch input {
+      opacity: 0;
+      width: 0;
+      height: 0;
+    }
+  
+    /* The slider */
+    .slider {
+      position: absolute;
+      cursor: pointer;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: #ccc;
+      -webkit-transition: .4s;
+      transition: .4s;
+    }
+  
+    .slider:before {
+      position: absolute;
+      content: "";
+      height: 20px;
+      width: 20px;
+      left: 1px;
+      bottom: 0px;
+      background-color: white;
+      -webkit-transition: .4s;
+      transition: .4s;
+    }
+  
+    input:checked+.slider {
+      background-color: #2196F3;
+    }
+  
+    input:focus+.slider {
+      box-shadow: 0 0 1px #2196F3;
+    }
+  
+    input:checked+.slider:before {
+      -webkit-transform: translateX(18px);
+      -ms-transform: translateX(18px);
+      transform: translateX(18px);
+    }
+  
+    /* Rounded sliders */
+    .slider.round {
+      border-radius: 34px;
+    }
+  
+    .slider.round:before {
+      border-radius: 50%;
+    }
+  </style>
+  <div id="wrexdiv" style="width: 550px; height: 350px; overflow-y: scroll;" onload="showInput()">
+    <div style="padding-bottom: 100px; padding: 5px 15px 5px 5px">
+      <div style="padding: 5px 10px 5px 5px">
+        <div style="display: flex; flex-direction: row;">
+          <div class="col-20l" style="display: flex; flex-direction: column; flex: 2; padding-right: 1%;">
+            Format:<br>
+            <select id="format" class="round">
+              <option value="" selected>OTHER</option>
+              <option value=".log">LOG</option>
+              <option value=".txt">TXT</option>
+              <option value=".json">JSON</option>
+              <option value=".js">JS</option>
+            </select>
+          </div>
+          <div class="col-17l" style="display: flex; flex-direction: column; flex: 2; padding-right: 1%;">
+            Task:<br>
+            <select id="filetask" title="99% of the time, you want append" class="round">
+              <option value="0" title="Only makes the file">Create</option>
+              <option value="1" title="Overwrites the files contents with yours">Write</option>
+              <option value="2" title="Add the content to the end of the file" selected>Append</option>
+              <option value="4" title="Inserts a line in a specific place in the file">Insert</option>
+              <option value="3" title="Deletes a file, Be VERY carefull with this option">Delete</option>
+              <option value="5" title="Copies a file">Copy</option>
+            </select>
+          </div>
+          <div style="display: flex; flex-direction: column; flex: 8;">
+            File Name:<br>
+            <textarea id="filename" title="If 'Other' add the file .format to the end of the file name" placeholder="Example file name 'myreallycoollog'" class="round" type="textarea" rows="1"></textarea>
+          </div>
         </div>
       </div>
-    </div>
-    <div style="padding: 5px 10px 5px 5px">
+      <div style="padding: 5px 10px 5px 5px">
         <div style="float: left; width: 100%;">
           File Path:<br>
           <textarea class="round col-100" id="filepath" title="./ represents the bots root directory. Use instead of an absolute path > C:/path/to/bot/" placeholder="Example Path = ./logs/date/example-date/" class="round" type="textarea" rows="3"></textarea><br>
         </div>
-    </div>
-    <div id ="newPath" style="padding: 5px 10px 5px 5px">
+      </div>
+      <div id="newPath" style="padding: 5px 10px 5px 5px">
         <div style="float: left; width: 100%;">
           New File Path:<br>
           <textarea class="round col-100" id="filepath2" placeholder="Example Path = ./logs/date/example-date/" class="round" type="textarea" rows="3"></textarea><br>
         </div>
-    </div>
-    <div style="padding: 5px 10px 5px 5px">
-      <div>
-        <span>If you would like to create a directory, leave the filename section empty while setting format to 'OTHER' and task to 'Create'.</span>
+      </div>
+      <div style="padding: 5px 10px 5px 5px">
+        <div>
+          <span>If you would like to create a directory, leave the filename section empty while setting format to 'OTHER' and task to 'Create'.</span>
+        </div>
+      </div>
+      <div style="padding: 5px 10px 5px 5px">
+        <div id="inputArea" class="" style="float: left; width: 100%;">
+          Input Text:<br>
+          <textarea id="input" placeholder="Leave Blank For None." class="round" type="textarea" rows="10"></textarea><br><br><br><br><br><br><br><br><br><br><br>
+        </div>
+        <div style="padding: 5px 10px 5px 5px;display: none;" id='visibot'>
+          <label class="switch">
+            <input onclick="if(checked){value='yes'}else{value='no'}" type="checkbox" id="togglestatus" value='no'>
+            <span class="slider round"></span>
+          </label>
+          <span>Toggle this if your data contains Objects (Json, array, etc...)</span>
+        </div>
+        <div id="lineInsert" class="" style="float: left; width: 65%;">
+          Line to Insert at:<br>
+          <input id="input2" placeholder="1 Adds content at the first line." class="round"></input>
+        </div>
       </div>
     </div>
-    <div style="padding: 5px 10px 5px 5px">
-      <div id="inputArea" class="" style="float: left; width: 100%;">
-        Input Text:<br>
-        <textarea id="input" placeholder="Leave Blank For None." class="round" type="textarea" rows="10"></textarea><br><br><br><br><br><br><br><br><br><br><br>
-      </div>
-      <div style="padding: 5px 10px 5px 5px;display: none;" id='visibot'>
-        <label class="switch">
-          <input onclick="if(checked){value='yes'}else{value='no'}" type="checkbox" id="togglestatus" value='no'>
-          <span class="slider round"></span>
-        </label>
-        <span>Toggle this if your data contains Objects (Json, array, etc...)</span>
-      </div>
-      <div id="lineInsert" class="" style="float: left; width: 65%;">
-        Line to Insert at:<br>
-        <input id="input2" placeholder="1 Adds content at the first line." class="round"></input>
-      </div>
-    </div>
-  </div>
-</div>`
+  </div>`
   },
 
-  init () {
-    const { document } = this
+  init() {
+    const {
+      document
+    } = this
     const selector = document.getElementById('filetask')
     const selector2 = document.getElementById('format')
     const targetfield = document.getElementById('inputArea')
@@ -181,7 +200,7 @@ module.exports = {
 
     selector.onclick = () => showInput()
 
-    function showInput () {
+    function showInput() {
       const selected = selector[selector.selectedIndex].value
       const selected2 = selector2[selector2.selectedIndex].value
       if (selected2 === '.json' && selected !== '5') {
@@ -207,7 +226,7 @@ module.exports = {
     }
   },
 
-  action (cache) {
+  action(cache) {
     const path = require('path')
     const Mods = this.getMods()
     const fs = Mods.require('fs-extra')
@@ -226,62 +245,48 @@ module.exports = {
     const itext = this.evalMessage(data.input, cache)
     const lmg = 'Something went wrong while'
 
-    switch (task) {
-      case 0: // Create File
-        if (fileName === '') break
-        try {
+    try {
+      switch (task) {
+        case 0: // Create File
+          if (fileName === '') break
           fs.writeFileSync(fpath, '')
-        } catch (err) {
-          return console.error(`${lmg} creating: [${err}]`)
-        }
-        break
-      case 1: // Write File
-        if (fileName === '') throw new Error('File Name not Provided:')
-        try {
+          break
+        case 1: // Write File
+          if (fileName === '') throw new Error('File Name not Provided:')
           if (togglestat === 'yes') {
             fs.writeFileSync(fpath, JSON.stringify(itext))
           } else {
             fs.writeFileSync(fpath, itext)
           }
-        } catch (err) {
-          return console.error(`${lmg} writing: [${err}]`)
-        }
-        break
-      case 2: // Append File
-        if (fileName === '') throw new Error('File Name not Provided:')
-        try {
+          break
+        case 2: // Append File
+          if (fileName === '') throw new Error('File Name not Provided:')
           if (togglestat === 'yes') {
             fs.appendFileSync(fpath, `${JSON.stringify(itext)}\r\n`)
           } else {
             fs.appendFileSync(fpath, `${itext}\r\n`)
           }
-        } catch (err) {
-          return console.error(`${lmg} appending: [${err}]`)
-        }
-        break
-      case 4: // Insert Line to File
-        if (fileName === '') throw new Error('File Name not Provided:')
-        insertLine(fpath).content(itext).at(line).then((err) => {
-          if (err) return console.log(`${lmg} inserting: [${err}]`)
-        })
-        break
-      case 3: // Delete File
-        try {
+          break
+        case 4: // Insert Line to File
+          if (fileName === '') throw new Error('File Name not Provided:')
+          insertLine(fpath).content(itext).at(line).then((err) => {
+            if (err) return console.error(`${lmg} inserting: [${err}]`)
+          })
+          break
+        case 3: // Delete File
           fs.unlinkSync(fpath)
           if (!fs.existsSync(dirName)) return this.callNextAction(cache)
-        } catch (err) {
-          return console.error(`${lmg} deleting: [${err}]`)
-        }
-        break
-      case 5: // Copy File
-        try {
+          break
+        case 5: // Copy File
           fs.copySync(fpath, fpath2)
-        } catch (err) {
-          return console.error(`${lmg} copying: [${err}]`)
-        }
-        break
-      default:
-        break
+          // return console.error(`${lmg} copying: [${err}]`)
+          break
+        default:
+          break
+      }
+    } catch (err) {
+      const type = ['creating', 'writing', 'appending', 'deleting', 'copying'][task]
+      return console.error(`${lmg} ${type} [${err}]`)
     }
 
     try {
@@ -296,5 +301,5 @@ module.exports = {
     this.callNextAction(cache)
   },
 
-  mod () {}
+  mod() {}
 }
