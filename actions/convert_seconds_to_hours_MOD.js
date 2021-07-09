2f92d6b9182e9b1a63eae151c441ed5be4a967c0
elnexfalso <40328573+elnexreal@users.noreply.github.com>
2021-07-09 18:31:12 -0300

    Add convert seconds to hours
    I just modified the Convert seconds to day MOD and i used another syntax
    
    Instead of showing <b>1D 24H 60M 60S</b> it shows <b>HH:MM:SS</b>
    
    I think its useful for making a music discord bot or smth lol

---
+++ actions/convert_seconds_to_hours_MOD.js
@@ -0,0 +1,65 @@
+module.exports = {
+  name: 'Convert Seconds To H:M:S',
+  section: 'Other Stuff',
+
+  subtitle (data) {
+    return `Convert ${data.time}`
+  },
+
+  variableStorage (data, varType) {
+    const type = parseInt(data.storage)
+    if (type !== varType) return
+    return ([data.varName, 'Date'])
+  },
+
+  fields: ['time', 'storage', 'varName'],
+
+  html (isEvent, data) {
+    return `
+<div style="float: left; width: 70%; padding-top: 8px;">
+  Seconds to Convert:
+  <input id="time" class="round" type="text" placeholder="e.g. 1522672056 or use Variables">
+</div>
+<div style="float: left; width: 35%; padding-top: 8px;">
+  Store Result In:<br>
+  <select id="storage" class="round" onchange="glob.variableChange(this, 'varNameContainer')">
+  ${data.variables[0]}
+  </select>
+</div>
+<div id="varNameContainer" style="float: right; display: none; width: 60%; padding-top: 8px;">
+  Variable Name:<br>
+  <input id="varName" class="round" type="text">
+</div><br><br>`
+  },
+
+  init () {
+    const { glob, document } = this
+    glob.variableChange(document.getElementById('storage'), 'varNameContainer')
+  },
+
+  action (cache) {
+    const data = cache.actions[cache.index]
+    const time = this.evalMessage(data.time, cache)
+
+    if (isNaN(time)) return this.callNextAction(cache)
+
+    let s = time
+    let m = Math.floor(s / 60)
+    s %= 60
+    let h = Math.floor(m / 60)
+    m %= 60
+
+    let result = `${h}:${m}:${s}`
+
+    if (result.toString() === 'Invalid Date') result = undefined
+
+    if (result !== undefined) {
+      const storage = parseInt(data.storage)
+      const varName = this.evalMessage(data.varName, cache)
+      this.storeValue(result, storage, varName, cache)
+    }
+    this.callNextAction(cache)
+  },
+
+  mod () {}
+}
