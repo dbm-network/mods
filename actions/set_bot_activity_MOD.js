5c4db15b3456b23d1b8eabaa2090094d4d5b302b
shilu <53784981+tlfn@users.noreply.github.com>
2021-07-18 11:26:56 +0900

    Fix calling next action multiple times / Refactor

--- actions/set_bot_activity_MOD.js
+++ actions/set_bot_activity_MOD.js
@@ -101,92 +101,28 @@ module.exports = {
     const nameText = this.evalMessage(data.nameText, cache);
     const url = this.evalMessage(data.url, cache);
 
-    const activity = parseInt(data.activity, 10);
-    const stat = parseInt(data.stat, 10);
+    const target = ['PLAYING', 'LISTENING', 'WATCHING', 'STREAMING', 'COMPETING'][parseInt(data.activity, 10)];
+    const statusTarget = ['online', 'idle', 'invisible', 'dnd'][parseInt(data.stat, 10)];
 
-    let obj;
-
-    let target;
-    if (activity >= 0) {
-      switch (activity) {
-        case 0:
-          target = 'PLAYING';
-          break;
-        case 1:
-          target = 'LISTENING';
-          break;
-        case 2:
-          target = 'WATCHING';
-          break;
-        case 3:
-          target = 'STREAMING';
-          break;
-        case 4:
-          target = 'COMPETING';
-          break;
-        default:
-          break;
-      }
+    if (!botClient) {
+      console.error('ERROR: Please input activity in "Set Bot Activity MOD"');
+      return this.callNextAction(cache);
     }
-
-    let statustarget;
-    if (stat >= 0) {
-      switch (stat) {
-        case 0:
-          statustarget = 'online';
-          break;
-        case 1:
-          statustarget = 'idle';
-          break;
-        case 2:
-          statustarget = 'invisible';
-          break;
-        case 3:
-          statustarget = 'dnd';
-          break;
-        default:
-          break;
-      }
-    }
-
-    if (botClient) {
-      if (nameText) {
-        if (target === 'STREAMING') {
-          obj = {
-            activity: {
-              name: nameText,
-              type: target,
-              url,
-            },
-            status: statustarget,
-          };
-          botClient
-            .setPresence(obj)
-            .then(() => {
-              this.callNextAction(cache);
-            })
-            .catch(console.error);
-        } else {
-          obj = {
-            activity: {
-              name: nameText,
-              type: target,
-            },
-            status: statustarget,
-          };
-          botClient
-            .setPresence(obj)
-            .then(() => {
-              this.callNextAction(cache);
-            })
-            .catch(console.error);
-        }
-      } else {
-        console.log('ERROR: Please input activity in "Set Bot Activity MOD"');
-        this.callNextAction(cache);
-      }
-    }
-    this.callNextAction(cache);
+    if (!nameText) return this.callNextAction(cache);
+    const obj = {
+      activity: {
+        name: nameText,
+        type: target,
+      },
+      status: statusTarget,
+    };
+    if (target === 'STREAMING') Object.assign(obj.activity, { url });
+    botClient
+        .setPresence(obj)
+        .then(() => {
+          this.callNextAction(cache);
+        })
+        .catch(console.error);
   },
 
   mod() {},
