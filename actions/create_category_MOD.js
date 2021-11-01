48ee7d5acfd99e7100a1727146c88f7a871d4dc7
Detroit Whiskey <45453611+DetroitWhiskey136@users.noreply.github.com>
2021-11-01 16:02:50 +0000

    removed this.dest in favour of optional chaining

--- actions/create_category_MOD.js
+++ actions/create_category_MOD.js
@@ -43,7 +43,7 @@ Name:<br>
   action(cache) {
     const data = cache.actions[cache.index];
     const { server } = cache;
-    if (!this.dest(server, 'channels', 'create')) return this.callnextAction(cache);
+    if (!server?.channels?.create) return this.callnextAction(cache);
 
     const name = this.evalMessage(data.channelName, cache);
     const position = parseInt(data.position, 10);
