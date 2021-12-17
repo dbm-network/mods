55a2f4475b19c165584cfc6bfe8919e623bc1e81
Giingu <zorlonlabs@gmail.com>
2021-12-17 11:11:05 +0100

    DBL -> TopGG
    Changes every mention of the old name (DBL) to the current name of the website, TopGG

--- actions/get_stats_from_topgg_MOD.js
+++ actions/get_stats_from_topgg_MOD.js
@@ -1,5 +1,5 @@
 module.exports = {
-  name: 'Get Bot Stats From DBL',
+  name: 'Get Bot Stats From TopGG',
   section: 'Other Stuff',
 
   subtitle(data) {
@@ -31,7 +31,7 @@ module.exports = {
 
   variableStorage(data, varType) {
     if (parseInt(data.storage, 10) !== varType) return;
-    let dataType = 'A DBL Stat';
+    let dataType = 'A TopGG Stat';
     switch (parseInt(data.info, 10)) {
       case 0:
         dataType = 'Invite URL';
@@ -111,7 +111,7 @@ module.exports = {
     <input id="botID" class="round" type="text">
   </div><br>
   <div style="float: left; width: 99%; padding-top: 8px;">
-    Your DBL Token:<br>
+    Your TopGG Token:<br>
     <input id="token" class="round" type="text">
   </div><br>
   <div style="float: left; width: 90%; padding-top: 8px;">
@@ -153,7 +153,7 @@ module.exports = {
   <div id="commentSection" style="padding-top: 8px;">
     <p>
       Some options will only work for certified or special bots. You better use some check variables to check if they exist.
-      <b>Note:</b> DBL is going to update the API and you'll need a token after the update!
+      <b>Note:</b> TopGG is going to update the API and you'll need a token after the update!
     </p>
   </div>
 </div>`;
