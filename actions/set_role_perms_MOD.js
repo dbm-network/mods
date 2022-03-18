91b089a5b8e29c754b66d2dfdf68cfa99fa86e12
EliteArtz <33768066+EliteArtz@users.noreply.github.com>
2022-03-18 11:35:32 +0100

    Add new permissions to Set Role Permissions (#861)
    * Compability with discordjs v13 permissions
    
    discordjs v13 no longer awaits ints as permission numbers, but bigints.
    
    * removed some unnecessary stuff and fixings
    
    * fix linting issues
    
    * Added missing- , sorted and re-arranged options
    
    * Finishing compability touch
    
    * eslint
    
    * compatibility fixes
    
    rearranging values due to compatibility issues + all permissions denied fix
    
    * fixed linting issues
    
    * geez, forgot break..
    
    * Rewrite to use DiscordJS FLAGS
    
    * fix linting errors
    
    * subtitle role undefined fix
    
    Co-authored-by: almeidx <almeidx@pm.me>

--- actions/set_role_perms_MOD.js
+++ actions/set_role_perms_MOD.js
@@ -9,15 +9,7 @@ module.exports = {
     downloadURL: 'https://github.com/dbm-network/mods/blob/master/actions/set_role_perms_MOD.js',
   },
 
-  subtitle(data) {
-    const roles = [
-      'Mentioned Role',
-      '1st Author Role',
-      '1st Server Role',
-      'Temp Variable',
-      'Server Variable',
-      'Global Variable',
-    ];
+  subtitle(data, presets) {
     const index = ['Granted', 'Denied'];
     const perm = [
       'Administrator',
@@ -33,7 +25,7 @@ module.exports = {
       'Priority Speaker',
       'Manage Channel',
       'Manage Webhooks',
-      'Read Messages',
+      'Read Messages (Deprecated)',
       'Send Messages',
       'Send TTS Messages',
       'Manage Messages',
@@ -51,8 +43,18 @@ module.exports = {
       'Use Voice Activity',
       'All Permissions',
       'Stream',
+      'View Channels',
+      'Moderate Member',
+      'Send Messages In Threads',
+      'Create Public Threads',
+      'Create Private Threads',
+      'Use External Stickers',
+      'Manage Threads',
+      'Use Application Commands',
+      'Use Activities',
+      'Manage Events',
     ];
-    return `${roles[data.role]} - ${perm[data.permission]} - ${index[data.state]} ${
+    return `${presets.getRoleText(data.role, data.varName)} - ${perm[data.permission]} - ${index[data.state]} ${
       !data.reason ? '' : `with Reason: <i>${data.reason}<i>`
     }`;
   },
@@ -77,37 +79,64 @@ module.exports = {
   <div style="float: left; width: 45%;">
     Permission:<br>
     <select id="permission" class="round">
-      <option value="0" selected>Administrator</option>
-      <option value="1">Manage Guild</option>
-      <option value="2">Manage Nicknames</option>
-      <option value="3">Manage Roles</option>
-      <option value="4">Manage Emojis</option>
-      <option value="5">Kick Members</option>
-      <option value="6">Ban Members</option>
-      <option value="7">View Audit Log</option>
-      <option value="8">Change Nickname</option>
-      <option value="9">Create Instant Invite</option>
-      <option value="10">Priority Speaker</option>
-      <option value="11">Manage Channel</option>
-      <option value="12">Manage Webhooks</option>
-      <option value="13">Read Messages</option>
-      <option value="14">Send Messages</option>
-      <option value="15">Send TTS Messages</option>
-      <option value="16">Manage Messages</option>
-      <option value="17">Embed Links</option>
-      <option value="18">Attach Files</option>
-      <option value="19">Read Message History</option>
-      <option value="20">Mention Everyone</option>
-      <option value="21">Use External Emojis</option>
-      <option value="22">Add Reactions</option>
-      <option value="23">Connect to Voice</option>
-      <option value="24">Speak in Voice</option>
-      <option value="25">Mute Members</option>
-      <option value="26">Deafen Members</option>
-      <option value="27">Move Members</option>
-      <option value="28">Use Voice Activity</option>
-      <option value="29">All Permissions</option>
-      <option value="30">Stream</option>
+      <optgroup label="General Server Permissions">
+        <option value="31">View Channels</option>
+        <option value="11">Manage Channels</option>
+        <option value="3">Manage Roles</option>
+        <option value="4">Manage Emojis and Stickers</option>
+        <option value="7">View Audit Log</option>
+        <option value="12">Manage Webhooks</option>
+        <option value="1">Manage Server</option>
+      </optgroup>
+      
+      <optgroup label="Membership Permissions">
+        <option value="9">Create Invite</option>
+        <option value="8">Change Nickname</option>
+        <option value="2">Manage Nicknames</option>
+        <option value="5">Kick Members</option>
+        <option value="6">Ban Members</option>
+        <option value="32">Moderate Member</option>
+      </optgroup>
+      
+      <optgroup label="Text Channel Permissions">
+        <option value="13" class="deprecated" disabled>Read Messages (Deprecated)</option>
+        <option value="14">Send Messages</option>
+        <option value="33">Send Messages In Threads</option>
+        <option value="34">Create Public Threads</option>
+        <option value="35">Create Private Threads</option>
+        <option value="17">Embed Links</option>
+        <option value="18">Attach Files</option>
+        <option value="22">Add Reactions</option>
+        <option value="21">Use External Emojis</option>
+        <option value="36">Use External Stickers</option>
+        <option value="20">Mention Everyone</option>
+        <option value="16">Manage Messages</option>
+        <option value="37">Manage Threads</option>
+        <option value="19">Read Message History</option>
+        <option value="15">Send TTS Messages</option>
+        <option value="38">Use Application Commands</option>
+      </optgroup>
+
+      <optgroup label="Voice Channel Permissions">
+        <option value="23">Connect</option>
+        <option value="24">Speak</option>
+        <option value="30">Stream</option>
+        <option value="39">Use Activities</option>
+        <option value="28">Use Voice Activity</option>
+        <option value="10">Priority Speaker</option>
+        <option value="25">Mute Members</option>
+        <option value="26">Deafen Members</option>
+        <option value="27">Move Members</option>
+      </optgroup>
+
+      <optgroup label="Events Permissions">
+        <option value="40">Manage Events</option>
+      </optgroup>
+
+      <optgroup label="Advanced Permissions">
+        <option value="0" selected>Administrator</option>
+        <option value="29">All Permissions</option>
+      </optgroup>
     </select>
   </div>
   <div style="padding-left: 5%; float: left; width: 55%;">
@@ -121,7 +150,15 @@ module.exports = {
 <div style="padding-top: 8px;">
   Reason:<br>
   <textarea id="reason" rows="2" placeholder="Insert reason here... (optional)" style="width: 99%; font-family: monospace; white-space: nowrap; resize: none;"></textarea>
-</div>`;
+</div>
+<style>
+.deprecated[disabled] {
+  color: var(--placeholder-text-color, #aaa);
+}
+.deprecated[disabled]:not(:checked) {
+  display:none;
+}
+</style>`;
   },
 
   init() {
@@ -131,109 +168,137 @@ module.exports = {
 
   async action(cache) {
     const data = cache.actions[cache.index];
+    const { FLAGS, ALL } = this.getDBM().DiscordJS.Permissions;
     const storage = parseInt(data.role, 10);
     const varName = this.evalMessage(data.varName, cache);
     const role = await this.getRole(storage, varName, cache);
     const info = parseInt(data.permission, 10);
     const reason = this.evalMessage(data.reason, cache);
 
-    const options = {};
-    options[data.permission] = data.state === '0';
-
     let result;
     switch (info) {
       case 0:
-        result = 8n;
+        result = FLAGS.ADMINISTRATOR;
         break;
       case 1:
-        result = 32n;
+        result = FLAGS.MANAGE_GUILD;
         break;
       case 2:
-        result = 134217728n;
+        result = FLAGS.MANAGE_NICKNAMES;
         break;
       case 3:
-        result = 268435456n;
+        result = FLAGS.MANAGE_ROLES;
         break;
       case 4:
-        result = 1073741824n;
+        result = FLAGS.MANAGE_EMOJIS_AND_STICKERS;
         break;
       case 5:
-        result = 2n;
+        result = FLAGS.KICK_MEMBERS;
         break;
       case 6:
-        result = 4n;
+        result = FLAGS.BAN_MEMBERS;
         break;
       case 7:
-        result = 128n;
+        result = FLAGS.VIEW_AUDIT_LOG;
         break;
       case 8:
-        result = 67108864n;
+        result = FLAGS.CHANGE_NICKNAME;
         break;
       case 9:
-        result = 1n;
+        result = FLAGS.CREATE_INSTANT_INVITE;
         break;
       case 10:
-        result = 256n;
+        result = FLAGS.PRIORITY_SPEAKER;
         break;
       case 11:
-        result = 16n;
+        result = FLAGS.MANAGE_CHANNELS;
         break;
       case 12:
-        result = 536870912n;
+        result = FLAGS.MANAGE_WEBHOOKS;
         break;
       case 13:
-        result = 1024n;
+        result = FLAGS.VIEW_CHANNEL; // Read Messages (Deprecated) fallback
         break;
       case 14:
-        result = 2048n;
+        result = FLAGS.SEND_MESSAGES;
         break;
       case 15:
-        result = 4096n;
+        result = FLAGS.SEND_TTS_MESSAGES;
         break;
       case 16:
-        result = 8192n;
+        result = FLAGS.MANAGE_MESSAGES;
         break;
       case 17:
-        result = 16384n;
+        result = FLAGS.EMBED_LINKS;
         break;
       case 18:
-        result = 32768n;
+        result = FLAGS.ATTACH_FILES;
         break;
       case 19:
-        result = 65536n;
+        result = FLAGS.READ_MESSAGE_HISTORY;
         break;
       case 20:
-        result = 131072n;
+        result = FLAGS.MENTION_EVERYONE;
         break;
       case 21:
-        result = 262144n;
+        result = FLAGS.USE_EXTERNAL_EMOJIS;
         break;
       case 22:
-        result = 64n;
+        result = FLAGS.ADD_REACTIONS;
         break;
       case 23:
-        result = 1048576n;
+        result = FLAGS.CONNECT;
         break;
       case 24:
-        result = 2097152n;
+        result = FLAGS.SPEAK;
         break;
       case 25:
-        result = 4194304n;
+        result = FLAGS.MUTE_MEMBERS;
         break;
       case 26:
-        result = 8388608n;
+        result = FLAGS.DEAFEN_MEMBERS;
         break;
       case 27:
-        result = 16777216n;
+        result = FLAGS.MOVE_MEMBERS;
         break;
       case 28:
-        result = 33554432n;
+        result = FLAGS.USE_VAD;
         break;
       case 29:
-        result = 1099511627775n;
+        result = ALL;
         break;
       case 30:
-        result = 200n;
+        result = FLAGS.STREAM;
+        break;
+      case 31:
+        result = FLAGS.VIEW_CHANNEL;
+        break;
+      case 32:
+        result = FLAGS.MODERATE_MEMBERS;
+        break;
+      case 33:
+        result = FLAGS.SEND_MESSAGES_IN_THREADS;
+        break;
+      case 34:
+        result = FLAGS.CREATE_PUBLIC_THREADS;
+        break;
+      case 35:
+        result = FLAGS.CREATE_PRIVATE_THREADS;
+        break;
+      case 36:
+        result = FLAGS.USE_EXTERNAL_STICKERS;
+        break;
+      case 37:
+        result = FLAGS.MANAGE_THREADS;
+        break;
+      case 38:
+        result = FLAGS.USE_APPLICATION_COMMANDS;
+        break;
+      case 39:
+        result = FLAGS.START_EMBEDDED_ACTIVITIES;
+        break;
+      case 40:
+        result = FLAGS.MANAGE_EVENTS;
         break;
       default:
         break;
@@ -241,9 +306,7 @@ module.exports = {
 
     if (role?.id) {
       const perms = role.permissions;
-      if (Array.isArray(role)) {
-        this.callListFunc(role, 'setPermissions', [role.id, options]).then(() => this.callNextAction(cache));
-      }
+
       if (data.state === '0') {
         role
           .setPermissions([perms, result], reason)
@@ -251,7 +314,7 @@ module.exports = {
           .catch(this.displayError.bind(this, data, cache));
       } else {
         role
-          .setPermissions([perms - result], reason)
+          .setPermissions(result !== ALL ? [perms - result] : [0n], reason)
           .then(() => this.callNextAction(cache))
           .catch(this.displayError.bind(this, data, cache));
       }
