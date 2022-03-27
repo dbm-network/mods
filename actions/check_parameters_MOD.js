90c84f399190c7963baab19ae91da17637dcc642
Giingu <picofuego@gmail.com>
2022-03-27 19:49:40 +0200

    Update check_parameters_MOD.js

--- actions/check_parameters_MOD.js
+++ actions/check_parameters_MOD.js
@@ -5,14 +5,7 @@ module.exports = {
   subtitle(data, presets) {
     return `${presets.getConditionsText(data)}`;
   },
-  meta: {
-	author:'Giingu', 
-	version: '2.2.0',
-	preciseCheck: true,
-	author: null,
-	authorUrl: 'https://github.com/dbm-network/mods',
-	downloadURL: 'https://github.com/dbm-network/mods/blob/master/actions/check_parameters_MOD.js'
-	},
+  meta: {author:'Giingu',version:'2.2.0',preciseCheck:true,author:null,authorUrl:'https://github.com/dbm-network/mods',downloadURL:'https://github.com/dbm-network/mods/blob/master/actions/check_parameters_MOD.js'},
   
   fields: ['condition', 'comparison', 'value', 'branch'],
 
