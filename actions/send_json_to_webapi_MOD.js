1d8feb1dea97147d0a43f69ecf488dc559ef7d67
Detroit Whiskey <detroitwhiskey136@gmail.com>
2022-06-13 12:49:31 -0700

    updated the version number.

--- actions/send_json_to_webapi_MOD.js
+++ actions/send_json_to_webapi_MOD.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'Send Json to WebAPI',
   section: 'JSON Things',
   meta: {
-    version: '2.1.1',
+    version: '2.1.4',
     preciseCheck: false,
     author: 'DBM Mods',
     authorUrl: 'https://github.com/dbm-network/mods',
