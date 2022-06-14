71d6428c7357478a3e5980297aa6e594627efb37
Detroit Whiskey <detroitwhiskey136@gmail.com>
2022-06-14 16:01:06 -0700

    fixed a lot of the issues with voice channel perms

--- actions/set_member_voice_channel_perms_MOD.js
+++ actions/set_member_voice_channel_perms_MOD.js
@@ -9,63 +9,39 @@ module.exports = {
     downloadURL: 'https://github.com/dbm-network/mods/blob/master/actions/set_member_voice_channel_perms_MOD.js',
   },
 
-  subtitle(data) {
-    const names = [
-      "Command Author's Voice Ch.",
-      "Mentioned User's Voice Ch.",
-      'Default Voice Channel',
-      'Temp Variable',
-      'Server Variable',
-      'Global Variable',
-    ];
-    const index = parseInt(data.vchannel, 10);
-    return index < 3 ? `${names[index]}` : `${names[index]} - ${data.varName}`;
+  subtitle(data, presets) {
+    return `${presets.getVoiceChannelText(data.vchannel, data.varName)}`;
   },
 
   fields: ['vchannel', 'varName', 'member', 'varName2', 'permission', 'state'],
 
   html(isEvent, data) {
     return `
-<div>
-  <div style="float: left; width: 35%;">
-    Source Voice Channel:<br>
-    <select id="vchannel" class="round" onchange="glob.voiceChannelChange(this, 'varNameContainer')">
-      ${data.voiceChannels[isEvent ? 1 : 0]}
-    </select>
+  <div>
+    <voice-channel-input style="padding-top: 8px;" dropdownLabel="Source Voice Channel" selectId="vchannel" variableContainerId="varNameContainer2" variableInputId="varName" selectWidth="45%" variableInputWidth="50%"></voice-channel-input>
+
+    <br><br><br>
+
+    <member-input style="padding-top: 8px;" dropdownLabel="Source Member" selectId="member" variableContainerId="varNameContainer" variableInputId="varName2" selectWidth="45%" variableInputWidth="50%"></member-input>
   </div>
-  <div id="varNameContainer" style="display: none; float: right; width: 60%;">
-    Variable Name:<br>
-    <input id="varName" class="round" type="text" list="variableList"><br>
-  </div>
-</div><br><br><br>
-<div style="padding-top: 8px;">
-  <div style="float: left; width: 35%;">
-    Source Member:<br>
-    <select id="member" name="second-list" class="round" onchange="glob.memberChange(this, 'varNameContainer2')">
-      ${data.members[isEvent ? 1 : 0]}
-    </select>
-  </div>
-  <div id="varNameContainer2" style="display: none; float: right; width: 60%;">
-    Variable Name:<br>
-    <input id="varName2" class="round" type="text" list="variableList2"><br>
-  </div>
-</div><br><br><br>
-<div style="padding-top: 8px;">
-  <div style="float: left; width: 45%;">
-    Permission:<br>
-    <select id="permission" class="round">
-      ${data.permissions[1]}
-    </select>
-  </div>
-  <div style="padding-left: 5%; float: left; width: 55%;">
-    Change To:<br>
-    <select id="state" class="round">
-      <option value="0" selected>Allow</option>
-      <option value="1">Inherit</option>
-      <option value="2">Disallow</option>
-    </select>
-  </div>
-</div>`;
+
+  <br><br><br>
+
+  <div style="padding-top: 8px;">
+    <div style="float: left; width: 45%;">
+      Permission:<br>
+      <select id="permission" class="round">
+        ${data.permissions[1]}
+      </select>
+    </div>
+    <div style="padding-left: 5%; float: left; width: 55%;">
+      Change To:<br>
+      <select id="state" class="round">
+        <option value="0" selected>Allow</option>
+        <option value="1">Inherit</option>
+        <option value="2">Disallow</option>
+      </select>
+    </div>`;
   },
 
   init() {
@@ -78,7 +54,7 @@ module.exports = {
     const data = cache.actions[cache.index];
     const storage = parseInt(data.vchannel, 10);
     const varName = this.evalMessage(data.varName, cache);
-    const channel = await this.getChannel(storage, varName, cache);
+    const channel = await this.getVoiceChannel(storage, varName, cache);
 
     const storage2 = parseInt(data.member, 10);
     const varName2 = this.evalMessage(data.varName2, cache);
@@ -90,13 +66,11 @@ module.exports = {
     if (!member) return this.callNextAction(cache);
     if (!channel) return this.callNextAction(cache);
 
-    return channel.permissionOverwrites
-      .edit(member.id, options, { type: 1 })
-      .then(() => this.callNextAction(cache))
-      .catch(() => {
-        this.displayError(data, cache);
-        this.callNextAction(cache);
-      });
+    channel.permissionOverwrites.edit(member.id, options, { type: 1 }).catch(() => {
+      this.displayError(data, cache);
+    });
+
+    this.callNextAction(cache);
   },
 
   mod() {},
