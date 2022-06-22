070e2b1386e3eadd93dfd8b8b7c81a3fd0eba12e
Detroit Whiskey <detroitwhiskey136@gmail.com>
2022-06-22 15:09:44 -0700

    some indenting

--- actions/set_slowmode_MOD.js
+++ actions/set_slowmode_MOD.js
@@ -35,14 +35,14 @@ module.exports = {
     <div>
       <div style="padding-top: 8px;">
         <div style="float: left; width: 50%;" padding-top: 16px;">
-        <span class="dbminputlabel">Amount</span><br>
+          <span class="dbminputlabel">Amount</span><br>
           <input id="amount" class="round" type="text" steps="5" placeholder="In seconds..."><br>
           <span class="dbminputlabel">Reason</span><br>
           <input id="reason" class="round" type="text" placeholder="Optional"><br>
         </div>
       </div>
       <div id="varNameContainer2" style="display: none; padding-left: 5%; float: left; width: 65%;">
-      <span class="dbminputlabel">Variable Name</span><br>
+        <span class="dbminputlabel">Variable Name</span><br>
         <input id="varName2" class="round" type="text">
       </div>
     </div>`;
