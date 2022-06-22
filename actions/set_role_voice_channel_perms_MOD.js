16ee4ee8b78f4d78bc9a08ed9bc1c72199c9f4c4
Detroit Whiskey <45453611+DetroitWhiskey136@users.noreply.github.com>
2022-06-22 14:56:13 -0700

    Fix Voice Channel permission actions (#878)
    * fixed a lot of the issues with voice channel perms
    
    * forgot to make the old stuff pretty.
    
    * changes alm requested
    
    * Update actions/set_voice_channel_permissions_MOD.js
    
    Co-authored-by: Almeida <almeidx@pm.me>
    
    * Update actions/set_role_voice_channel_perms_MOD.js
    
    Co-authored-by: Almeida <almeidx@pm.me>
    
    * lets hope these changes take.
    
    Co-authored-by: Almeida <almeidx@pm.me>

--- actions/set_role_voice_channel_perms_MOD.js
+++ actions/set_role_voice_channel_perms_MOD.js
@@ -9,77 +9,57 @@ module.exports = {
     downloadURL: 'https://github.com/dbm-network/mods/blob/master/actions/set_role_voice_channel_perms_MOD.js',
   },
 
-  subtitle(data) {
-    const names = [
-      "Command Author's Voice Ch.",
-      "Mentioned User's Voice Ch.",
-      'Default Voice Channel',
-      'Temp Variable',
-      'Server Variable',
-      'Global Variable',
-    ];
-    const index = parseInt(data.vchannel, 10);
-    return index < 3 ? `${names[index]}` : `${names[index]} - ${data.varName}`;
+  subtitle(data, presets) {
+    return `${presets.getVoiceChannelText(data.vchannel, data.varName)}`;
   },
 
   fields: ['vchannel', 'varName', 'role', 'varName2', 'permission', 'state'],
 
-  html(isEvent, data) {
+  html(_isEvent, data) {
     return `
-<div>
-  <div style="float: left; width: 35%;">
-    Source Voice Channel:<br>
-    <select id="vchannel" class="round" onchange="glob.voiceChannelChange(this, 'varNameContainer')">
-      ${data.voiceChannels[isEvent ? 1 : 0]}
-    </select>
-  </div>
-  <div id="varNameContainer" style="display: none; float: right; width: 60%;">
-    Variable Name:<br>
-    <input id="varName" class="round" type="text" list="variableList"><br>
-  </div>
-</div><br><br><br>
-<div style="padding-top: 8px;">
-  <div style="float: left; width: 35%;">
-    Source Role:<br>
-    <select id="role" name="second-list" class="round" onchange="glob.roleChange(this, 'varNameContainer2')">
-      ${data.roles[isEvent ? 1 : 0]}
-    </select>
-  </div>
-  <div id="varNameContainer2" style="display: none; float: right; width: 60%;">
-    Variable Name:<br>
-    <input id="varName2" class="round" type="text" list="variableList2"><br>
-  </div>
-</div><br><br><br>
-<div style="padding-top: 8px;">
-  <div style="float: left; width: 45%;">
-    Permission:<br>
-    <select id="permission" class="round">
-      ${data.permissions[1]}
-    </select>
-  </div>
-  <div style="padding-left: 5%; float: left; width: 55%;">
-    Change To:<br>
-    <select id="state" class="round">
-      <option value="0" selected>Allow</option>
-      <option value="1">Inherit</option>
-      <option value="2">Disallow</option>
-    </select>
-  </div>
-</div>`;
+      <div>
+        <voice-channel-input
+          dropdownLabel="Voice Channel"
+          selectId="vchannel"
+          variableContainerId="varNameContainer"
+          variableInputId="varName"
+          selectWidth="45%"
+          variableInputWidth="50%"/>
+      </div><br><br><br>
+      <div>
+        <role-input
+          dropdownLabel="Role"
+          selectId="role"
+          variableContainerId="varNameContainer2"
+          variableInputId="varName2"
+          selectWidth="45%"
+          variableInputWidth="50%"/>
+      </div><br><br><br>
+      <div style="padding-top: 8px;">
+        <div style="float: left; width: 45%;">
+        <span class="dbminputlabel">Permission</span><br>
+          <select id="permission" class="round">
+            ${data.permissions[1]}
+          </select>
+        </div>
+        <div style="padding-left: 5%; float: left; width: 55%;">
+        <span class="dbminputlabel">Change To</span><br>
+          <select id="state" class="round">
+            <option value="0" selected>Allow</option>
+            <option value="1">Inherit</option>
+            <option value="2">Disallow</option>
+          </select>
+        </div>
+      </div>`;
   },
 
-  init() {
-    const { glob, document } = this;
-
-    glob.voiceChannelChange(document.getElementById('vchannel'), 'varNameContainer');
-    glob.roleChange(document.getElementById('role'), 'varNameContainer2');
-  },
+  init() {},
 
   async action(cache) {
     const data = cache.actions[cache.index];
     const storage = parseInt(data.vchannel, 10);
     const varName = this.evalMessage(data.varName, cache);
-    const channel = await this.getChannel(storage, varName, cache);
+    const channel = await this.getVoiceChannel(storage, varName, cache);
     const storage2 = parseInt(data.role, 10);
     const varName2 = this.evalMessage(data.varName2, cache);
     const role = await this.getRole(storage2, varName2, cache);
@@ -87,18 +67,14 @@ module.exports = {
 
     options[data.permission] = data.state === '0' ? true : data.state === '2' ? false : null;
 
-    if (role && role.id) {
-      if (channel?.permissionOverwrites.edit) {
-        channel.permissionOverwrites
-          .edit(role.id, options, { type: 0 })
-          .then(() => {
-            this.callNextAction(cache);
-          })
-          .catch(this.displayError.bind(this, data, cache));
-      }
+    if (role?.id) {
+      channel.permissionOverwrites
+        .edit(role.id, options, { type: 0 })
+        .then(() => this.callNextAction(cache))
+        .catch((err) => this.displayError(data, cache, err));
+    } else {
       this.callNextAction(cache);
     }
-    this.callNextAction(cache);
   },
 
   mod() {},
--- actions/set_voice_channel_permissions_MOD.js
+++ actions/set_voice_channel_permissions_MOD.js
@@ -9,81 +9,66 @@ module.exports = {
     downloadURL: 'https://github.com/dbm-network/mods/blob/master/actions/set_voice_channel_permissions_MOD.js',
   },
 
-  subtitle(data) {
-    const names = [
-      "Command Author's Voice Ch.",
-      "Mentioned User's Voice Ch.",
-      'Default Voice Channel',
-      'Temp Variable',
-      'Server Variable',
-      'Global Variable',
-    ];
-    const index = parseInt(data.storage, 10);
-    return index < 3 ? `${names[index]}` : `${names[index]} - ${data.varName}`;
+  subtitle(data, presets) {
+    return `${presets.getVoiceChannelText(data.storage, data.varName)}`;
   },
 
   fields: ['storage', 'varName', 'permission', 'state'],
 
-  html(isEvent, data) {
+  html(_isEvent, data) {
     return `
-<div>
-  <div style="float: left; width: 45%;">
-    Source Voice Channel:<br>
-    <select id="storage" class="round" onchange="glob.channelChange(this, 'varNameContainer')">
-      ${data.voiceChannels[isEvent ? 1 : 0]}
-    </select>
-  </div>
-  <div id="varNameContainer" style="padding-left: 5%; float: left; width: 55%;">
-    Variable Name:<br>
-    <input id="varName" class="round" type="text" list="variableList"><br>
-  </div>
-</div><br><br><br>
-<div style="padding-top: 8px;">
-  <div style="float: left; width: 45%;">
-    Permission:<br>
-    <select id="permission" class="round">
-      ${data.permissions[1]}
-    </select>
-  </div>
-  <div style="padding-left: 5%; float: left; width: 55%;">
-    Change To:<br>
-    <select id="state" class="round">
-      <option value="0" selected>Allow</option>
-      <option value="1">Disallow</option>
-    </select>
-  </div>
-</div>`;
+      <div>
+        <voice-channel-input
+          dropdownLabel="Voice Channel"
+          selectId="storage"
+          variableContainerId="varNameContainer"
+          variableInputId="varName"
+          selectWidth="45%"
+          variableInputWidth="50%"/>
+
+      </div><br><br><br>
+
+      <div style="padding-top: 8px;">
+        <div style="float: left; width: 45%;">
+          <span class="dbminputlabel">Permission</span><br>
+          <select id="permission" class="round">
+            ${data.permissions[1]}
+          </select>
+        </div>
+        <div style="padding-left: 5%; float: left; width: 55%;">
+          <span class="dbminputlabel">Change To</span><br>
+          <select id="state" class="round">
+            <option value="0" selected>Allow</option>
+            <option value="2">Inherit</option>
+            <option value="1">Disallow</option>
+          </select>
+        </div>
+      </div>
+      <br><br><br><br>
+      <span>
+        Note: This modifies the everyone role in the voice channel only.
+        In order to edit another roles permission use the Set Role Voice Channel Perms action.
+      </span>`;
   },
 
-  init() {
-    const { glob, document } = this;
-    glob.channelChange(document.getElementById('storage'), 'varNameContainer');
-  },
+  init() {},
 
   async action(cache) {
     const data = cache.actions[cache.index];
     const { server } = cache;
     const storage = parseInt(data.storage, 10);
     const varName = this.evalMessage(data.varName, cache);
-    const channel = await this.getChannel(storage, varName, cache);
+    const channel = await this.getVoiceChannel(storage, varName, cache);
 
     if (!server) return this.callNextAction(cache);
 
     const options = {};
-    options[data.permission] = Boolean(data.state === '0');
-    if (Array.isArray(channel)) {
-      this.callListFunc(channel, 'updateOverwrite', [server.id, options]).then(() => {
-        this.callNextAction(cache);
-      });
-    } else if (channel && channel.updateOverwrite) {
-      channel
-        .updateOverwrite(server.id, options)
-        .then(() => {
-          this.callNextAction(cache);
-        })
-        .catch(this.displayError.bind(this, data, cache));
-    }
-    this.callNextAction(cache);
+    options[data.permission] = data.state === '0' ? true : data.state === '1' ? false : null;
+    channel.permissionOverwrites
+      // providing the server id is the same as providing the everyone role id;
+      .edit(server.id, options, { type: 0 })
+      .then(() => this.callNextAction(cache))
+      .catch((err) => this.displayError(data, cache, err));
   },
   mod() {},
 };
