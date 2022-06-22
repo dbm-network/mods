225f75fac431ddbaf07af70774050fe1dd8238b5
Detroit Whiskey <detroitwhiskey136@gmail.com>
2022-06-22 14:36:33 -0700

    fixed the issue with type and did some v2 cleanup

--- actions/set_slowmode_MOD.js
+++ actions/set_slowmode_MOD.js
@@ -9,17 +9,8 @@ module.exports = {
     downloadURL: 'https://github.com/dbm-network/mods/blob/master/actions/set_slowmode_MOD.js',
   },
 
-  subtitle(data) {
-    const names = [
-      'Same Channel',
-      'Mentioned Channel',
-      'Default Channel',
-      'Temp Variable',
-      'Server Variable',
-      'Global Variable',
-    ];
-    const index = parseInt(data.storage, 10);
-    return index < 3 ? `Set slowmode : ${names[index]}` : `Set slowmode : ${names[index]} - ${data.varName}`;
+  subtitle(data, presets) {
+    return `${presets.getChannelText(data.storage, data.varName)} : ${data.amount} seconds`;
   },
 
   variableStorage(data, varType) {
@@ -29,44 +20,42 @@ module.exports = {
 
   fields: ['storage', 'varName', 'varName2', 'amount', 'reason'],
 
-  html(isEvent, data) {
+  html() {
     return `
-<div style="padding-top: 8px;">
-  <div style="float: left; width: 35%;" padding-top: 16px;">
-    Source Channel:<br>
-    <select id="storage" class="round" onchange="glob.channelChange(this, 'varNameContainer')">
-      ${data.channels[isEvent ? 1 : 0]}
-    </select>
-  </div>
-  <div id="varNameContainer" style="display: none; padding-left: 5%; float: left; width: 65%;">
-    Variable Name:<br>
-    <input id="varName" class="round" type="text" list="variableList"><br>
-  </div>
-</div><br><br><br>
-<div style="padding-top: 8px;">
-  <div style="float: left; width: 50%;" padding-top: 16px;">
-    Amount:<br>
-    <input id="amount" class="round" type="text" steps="5" placeholder="In seconds..."><br>
-    Reason:<br>
-    <input id="reason" class="round" type="text" placeholder="Optional"><br>
-  </div>
-</div>
-  <div id="varNameContainer2" style="display: none; padding-left: 5%; float: left; width: 65%;">
-    Variable Name:<br>
-    <input id="varName2" class="round" type="text">
-  </div>`;
+    <div>
+    <channel-input
+        style="padding-top: 8px;"
+        dropdownLabel="Source Channel"
+        selectId="storage"
+        variableContainerId="varNameContainer"
+        variableInputId="varName"
+        selectWidth="45%"
+        variableInputWidth="50%"/>
+    </div><br><br><br><br>
+    <div>
+      <div style="padding-top: 8px;">
+        <div style="float: left; width: 50%;" padding-top: 16px;">
+          Amount:<br>
+          <input id="amount" class="round" type="text" steps="5" placeholder="In seconds..."><br>
+          Reason:<br>
+          <input id="reason" class="round" type="text" placeholder="Optional"><br>
+        </div>
+      </div>
+      <div id="varNameContainer2" style="display: none; padding-left: 5%; float: left; width: 65%;">
+        Variable Name:<br>
+        <input id="varName2" class="round" type="text">
+      </div>
+    </div>`;
   },
 
   async action(cache) {
     const data = cache.actions[cache.index];
-    const storage = parseInt(data.storage, 10);
-    const varName = this.evalMessage(data.varName, cache);
-    const channel = await this.getChannel(storage, varName, cache);
+    const channel = await this.getChannelFromData(data.storage, data.varName, cache);
     const amount = this.evalMessage(data.amount, cache);
     const reason = this.evalMessage(data.reason, cache);
     const { type } = channel;
 
-    if (type !== 'text') return this.callNextAction(cache);
+    if (type !== 'GUILD_TEXT') return this.callNextAction(cache);
 
     channel.setRateLimitPerUser(amount, reason);
 
