f02e30294785ffc4737c2ae20f5dae5f84393ce9
Gotowka <89851069+Gotowka@users.noreply.github.com>
2022-09-20 07:07:38 +0200

    Fixed member boosted server
    - Added server variable

--- events/member_boosted_server_EVT.js
+++ events/member_boosted_server_EVT.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'Member Boosted Server',
   isEvent: true,
 
-  fields: ['Member (Temp Variable Name):'],
+  fields: ['Member (Temp Variable Name):', 'Server (Temp Variable Name):'],
 
   mod(DBM) {
     DBM.Events = DBM.Events || {};
@@ -11,14 +11,14 @@ module.exports = {
     DBM.Events.boostedGuild = function boostedGuild(old, recent) {
       if (!Bot.$evts['Member Boosted Server']) return;
       const server = recent.guild;
-      if (!(!old.premiumSince && recent.premiumSince)) return;
-
-      for (const event of Bot.$evts['Member Boosted Server']) {
-        const temp = {};
-        if (event.temp) temp[event.temp] = recent;
-        if (event.temp2) temp[event.temp2] = recent.guild;
-        Actions.invokeEvent(event, server, temp);
-      }
+      if (!old.premiumSince && recent.premiumSince) {
+        for (const event of Bot.$evts['Member Boosted Server']) {
+          const temp = {};
+          if (event.temp) temp[event.temp] = recent;
+          if (event.temp2) temp[event.temp2] = recent.guild;
+          Actions.invokeEvent(event, server, temp);
+        }
+      } else return;
     };
 
     const { onReady } = Bot;
