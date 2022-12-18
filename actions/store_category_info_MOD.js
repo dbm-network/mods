6c93cda6934922a93323cfed25679bf96161862c
TheMonDon <11539895+TheMonDon@users.noreply.github.com>
2022-12-17 20:00:35 -0600

    Update store_category_info_MOD.js

--- actions/store_category_info_MOD.js
+++ actions/store_category_info_MOD.js
@@ -193,9 +193,11 @@ module.exports = {
         result = targetCategory.children.size; // Category Channel Count
         break;
       case 8:
-        result = [...targetCategory.children
-          .filter((c) => ['GUILD_TEXT', 'GUILD_NEWS', 'GUILD_STORE'].includes(c.type))
-          .values()]; // Category Text Channel List
+        result = [
+          ...targetCategory.children
+            .filter((c) => ['GUILD_TEXT', 'GUILD_NEWS', 'GUILD_STORE'].includes(c.type))
+            .values(),
+        ]; // Category Text Channel List
         break;
       case 9:
         result = targetCategory.children.filter((c) =>
