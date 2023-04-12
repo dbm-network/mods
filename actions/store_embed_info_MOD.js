a2f20e9a0f70954040c3108bc3233e35ebd64437
TheMonDon <11539895+TheMonDon@users.noreply.github.com>
2023-04-11 21:11:08 -0500

    Add condtional operator to fix crashes (#937)

--- actions/store_embed_info_MOD.js
+++ actions/store_embed_info_MOD.js
@@ -103,22 +103,22 @@ module.exports = {
         result = embed.description;
         break;
       case 3:
-        result = embed.author.name;
+        result = embed.author?.name;
         break;
       case 4:
-        result = embed.author.iconURL;
+        result = embed.author?.iconURL;
         break;
       case 5:
-        result = embed.thumbnail.url;
+        result = embed.thumbnail?.url;
         break;
       case 6:
-        result = embed.footer.text;
+        result = embed.footer?.text;
         break;
       case 7:
-        result = embed.footer.iconURL;
+        result = embed.footer?.iconURL;
         break;
       case 8:
-        result = embed.image.url;
+        result = embed.image?.url;
         break;
       case 9:
         result = embed.color;
