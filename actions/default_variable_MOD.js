8cde43fa8bfeacc00caea9beeee75fc5dbf3bcfb
Finbar <finbar@finbar.xyz>
2023-05-31 14:15:45 -0400

    fix lint and prettier on default variable action

--- actions/default_variable_MOD.js
+++ actions/default_variable_MOD.js
@@ -1,31 +1,34 @@
 module.exports = {
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
   // Action Name
   //
   // This is the name of the action displayed in the editor.
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
 
-  name: "Default Variable",
+  name: 'Default Variable',
 
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
   // Action Section
   //
   // This is the section the action will fall into.
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
 
-  section: "Variable Things",
+  section: 'Variable Things',
 
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
   // Action Subtitle
   //
   // This function generates the subtitle displayed next to the name.
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
 
   subtitle(data, presets) {
-    return `${data.mode.split('').map((c, i) => i === 0 ? c.toUpperCase() : c).join('')} Mode - ${presets.getVariableText(data.storage, data.varName)} - Default: ${data.defaultTo || "None"}`;
+    return `${data.mode
+      .split('')
+      .map((c, i) => (i === 0 ? c.toUpperCase() : c))
+      .join('')} Mode - ${presets.getVariableText(data.storage, data.varName)} - Default: ${data.defaultTo || 'None'}`;
   },
 
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
   // Action Meta Data
   //
   // Helps check for updates and provides info if a custom mod.
@@ -33,21 +36,21 @@ module.exports = {
   //
   // It's highly recommended "preciseCheck" is set to false for third-party mods.
   // This will make it so the patch version (0.0.X) is not checked.
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
 
-  meta: { version: "2.1.7", preciseCheck: true, author: null, authorUrl: null, downloadUrl: null },
+  meta: { version: '2.1.7', preciseCheck: true, author: null, authorUrl: null, downloadUrl: null },
 
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
   // Action Fields
   //
   // These are the fields for the action. These fields are customized
   // by creating elements with corresponding IDs in the HTML. These
   // are also the names of the fields stored in the action's JSON data.
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
 
-  fields: ["storage", "varName", "mode", "defaultTo"],
+  fields: ['storage', 'varName', 'mode', 'defaultTo'],
 
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
   // Command HTML
   //
   // This function returns a string containing the HTML used for
@@ -56,9 +59,9 @@ module.exports = {
   // The "isEvent" parameter will be true if this action is being used
   // for an event. Due to their nature, events lack certain information,
   // so edit the HTML to reflect this.
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
 
-  html(isEvent, data) {
+  html() {
     return `
 <p>This action sets a variable to a default value if it's empty.</p>
 <retrieve-from-variable dropdownLabel="Variable" selectId="storage" variableContainerId="varNameContainer" variableInputId="varName"></retrieve-from-variable>
@@ -74,7 +77,7 @@ module.exports = {
 `;
   },
 
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
   // Action Editor Pre-Init Code
   //
   // Before the fields from existing data in this action are applied
@@ -85,29 +88,29 @@ module.exports = {
   //
   // The "formatters" argument contains built-in functions for formatting
   // the data required for official DBM action compatibility.
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
 
   preInit(data, formatters) {
     return formatters.compatibility_2_0_0_iftruefalse_to_branch(data);
   },
 
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
   // Action Editor Init Code
   //
   // When the HTML is first applied to the action editor, this code
   // is also run. This helps add modifications or setup reactionary
   // functions for the DOM elements.
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
 
   init() {},
 
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
   // Action Bot Function
   //
   // This is the function for the action within the Bot's Action class.
   // Keep in mind event calls won't have access to the "msg" parameter,
   // so be sure to provide checks for variable existence.
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
 
   action(cache) {
     const data = cache.actions[cache.index];
@@ -118,10 +121,9 @@ module.exports = {
 
     const { mode } = data;
 
-    switch(mode){
+    switch (mode) {
       case 'normal':
-        if (variable === undefined || variable === null)
-          this.storeValue(defaultTo, type, varName, cache);
+        if (variable === undefined || variable === null) this.storeValue(defaultTo, type, varName, cache);
         break;
       case 'strict':
         if (variable === undefined || variable === null || variable === 0 || variable === false)
@@ -134,7 +136,7 @@ module.exports = {
     this.callNextAction(cache);
   },
 
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
   // Action Bot Mod Init
   //
   // An optional function for action mods. Upon the bot's initialization,
@@ -145,18 +147,18 @@ module.exports = {
   // If an action provides inputs for more actions within, be sure
   // to call the `this.prepareActions` function to ensure all actions are
   // recursively iterated through.
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
 
-  modInit(data) {},
+  modInit() {},
 
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
   // Action Bot Mod
   //
   // Upon initialization of the bot, this code is run. Using the bot's
   // DBM namespace, one can add/modify existing functions if necessary.
   // In order to reduce conflicts between mods, be sure to alias
   // functions you wish to overwrite.
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
 
   mod() {},
 };
