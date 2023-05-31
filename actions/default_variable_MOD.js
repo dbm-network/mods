94404ff441083411eb423808233ce485808ffa22
OneAndonlyFinbar <finbar@finbar.xyz>
2023-05-31 14:39:39 -0400

    Update default_variable_MOD.js

--- actions/default_variable_MOD.js
+++ actions/default_variable_MOD.js
@@ -1,66 +1,31 @@
 module.exports = {
-  // ---------------------------------------------------------------------
   // Action Name
-  //
-  // This is the name of the action displayed in the editor.
-  // ---------------------------------------------------------------------
-
   name: 'Default Variable',
-
-  // ---------------------------------------------------------------------
+  
   // Action Section
-  //
-  // This is the section the action will fall into.
-  // ---------------------------------------------------------------------
-
   section: 'Variable Things',
-
-  // ---------------------------------------------------------------------
+  
   // Action Subtitle
-  //
-  // This function generates the subtitle displayed next to the name.
-  // ---------------------------------------------------------------------
-
   subtitle(data, presets) {
     return `${data.mode
       .split('')
       .map((c, i) => (i === 0 ? c.toUpperCase() : c))
       .join('')} Mode - ${presets.getVariableText(data.storage, data.varName)} - Default: ${data.defaultTo || 'None'}`;
   },
-
-  // ---------------------------------------------------------------------
+  
   // Action Meta Data
-  //
-  // Helps check for updates and provides info if a custom mod.
-  // If this is a third-party mod, please set "author" and "authorUrl".
-  //
-  // It's highly recommended "preciseCheck" is set to false for third-party mods.
-  // This will make it so the patch version (0.0.X) is not checked.
-  // ---------------------------------------------------------------------
-
-  meta: { version: '2.1.7', preciseCheck: true, author: null, authorUrl: null, downloadUrl: null },
-
-  // ---------------------------------------------------------------------
+    meta: {
+    version: '2.1.7',
+    preciseCheck: false,
+    author: 'DBM Mods',
+    authorUrl: 'https://github.com/dbm-network/mods',
+    downloadURL: 'https://github.com/dbm-network/mods/blob/master/actions/default_variable_MOD.js',
+  },
+  
   // Action Fields
-  //
-  // These are the fields for the action. These fields are customized
-  // by creating elements with corresponding IDs in the HTML. These
-  // are also the names of the fields stored in the action's JSON data.
-  // ---------------------------------------------------------------------
-
   fields: ['storage', 'varName', 'mode', 'defaultTo'],
-
-  // ---------------------------------------------------------------------
+  
   // Command HTML
-  //
-  // This function returns a string containing the HTML used for
-  // editing actions.
-  //
-  // The "isEvent" parameter will be true if this action is being used
-  // for an event. Due to their nature, events lack certain information,
-  // so edit the HTML to reflect this.
-  // ---------------------------------------------------------------------
-
   html() {
     return `
 <p>This action sets a variable to a default value if it's empty.</p>
@@ -77,41 +42,7 @@ module.exports = {
 `;
   },
 
-  // ---------------------------------------------------------------------
-  // Action Editor Pre-Init Code
-  //
-  // Before the fields from existing data in this action are applied
-  // to the user interface, this function is called if it exists.
-  // The existing data is provided, and a modified version can be
-  // returned. The returned version will be used if provided.
-  // This is to help provide compatibility with older versions of the action.
-  //
-  // The "formatters" argument contains built-in functions for formatting
-  // the data required for official DBM action compatibility.
-  // ---------------------------------------------------------------------
-
-  preInit(data, formatters) {
-    return formatters.compatibility_2_0_0_iftruefalse_to_branch(data);
-  },
-
-  // ---------------------------------------------------------------------
-  // Action Editor Init Code
-  //
-  // When the HTML is first applied to the action editor, this code
-  // is also run. This helps add modifications or setup reactionary
-  // functions for the DOM elements.
-  // ---------------------------------------------------------------------
-
-  init() {},
-
-  // ---------------------------------------------------------------------
   // Action Bot Function
-  //
-  // This is the function for the action within the Bot's Action class.
-  // Keep in mind event calls won't have access to the "msg" parameter,
-  // so be sure to provide checks for variable existence.
-  // ---------------------------------------------------------------------
-
   action(cache) {
     const data = cache.actions[cache.index];
     const type = parseInt(data.storage, 10);
@@ -135,30 +66,4 @@ module.exports = {
 
     this.callNextAction(cache);
   },
-
-  // ---------------------------------------------------------------------
-  // Action Bot Mod Init
-  //
-  // An optional function for action mods. Upon the bot's initialization,
-  // each command/event's actions are iterated through. This is to
-  // initialize responses to interactions created within actions
-  // (e.g. buttons and select menus for Send Message).
-  //
-  // If an action provides inputs for more actions within, be sure
-  // to call the `this.prepareActions` function to ensure all actions are
-  // recursively iterated through.
-  // ---------------------------------------------------------------------
-
-  modInit() {},
-
-  // ---------------------------------------------------------------------
-  // Action Bot Mod
-  //
-  // Upon initialization of the bot, this code is run. Using the bot's
-  // DBM namespace, one can add/modify existing functions if necessary.
-  // In order to reduce conflicts between mods, be sure to alias
-  // functions you wish to overwrite.
-  // ---------------------------------------------------------------------
-
-  mod() {},
 };
