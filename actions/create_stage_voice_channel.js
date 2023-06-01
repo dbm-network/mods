669670959ac076decdd393977bb3afc696f8212a
Isaac <isaacspillett@mail.com>
2023-06-01 17:41:23 +0100

    Stage Voice Channel
    Created Stage voice channel action

---
+++ actions/create_stage_voice_channel.js
@@ -0,0 +1,163 @@
+module.exports = {
+  //---------------------------------------------------------------------
+  // Action Name
+  //
+  // This is the name of the action displayed in the editor.
+  //---------------------------------------------------------------------
+
+  name: "Create Stage Voice Channel",
+
+  //---------------------------------------------------------------------
+  // Action Section
+  //
+  // This is the section the action will fall into.
+  //---------------------------------------------------------------------
+
+  section: "Channel Control",
+
+  //---------------------------------------------------------------------
+  // Action Subtitle
+  //
+  // This function generates the subtitle displayed next to the name.
+  //---------------------------------------------------------------------
+
+  subtitle(data, presets) {
+    return `${data.channelName}`;
+  },
+
+  //---------------------------------------------------------------------
+  // Action Storage Function
+  //
+  // Stores the relevant variable info for the editor.
+  //---------------------------------------------------------------------
+
+  variableStorage(data, varType) {
+    const type = parseInt(data.storage, 10);
+    if (type !== varType) return;
+    return [data.varName, "Voice Channel"];
+  },
+
+  //---------------------------------------------------------------------
+  // Action Meta Data
+  //
+  // Helps check for updates and provides info if a custom mod.
+  // If this is a third-party mod, please set "author" and "authorUrl".
+  //
+  // It's highly recommended "preciseCheck" is set to false for third-party mods.
+  // This will make it so the patch version (0.0.X) is not checked.
+  //---------------------------------------------------------------------
+
+  meta: { version: "2.1.7", preciseCheck: true, author: null, authorUrl: null, downloadUrl: null },
+
+  //---------------------------------------------------------------------
+  // Action Fields
+  //
+  // These are the fields for the action. These fields are customized
+  // by creating elements with corresponding IDs in the HTML. These
+  // are also the names of the fields stored in the action's JSON data.
+  //---------------------------------------------------------------------
+
+  fields: ["channelName", "bitrate", "userLimit", "varName", "categoryID", "reason"],
+
+  //---------------------------------------------------------------------
+  // Command HTML
+  //
+  // This function returns a string containing the HTML used for
+  // editing actions.
+  //
+  // The "isEvent" parameter will be true if this action is being used
+  // for an event. Due to their nature, events lack certain information,
+  // so edit the HTML to reflect this.
+  //---------------------------------------------------------------------
+
+  html(isEvent, data) {
+    return `
+<span class="dbminputlabel">Stage Name</span><br>
+<input id="channelName" class="round" type="text">
+
+<br>
+
+<span class="dbminputlabel">Category ID</span><br>
+<input id= "categoryID" class="round" type="text" placeholder="Leave blank for default!">
+
+<br>
+
+<div style="float: left; width: calc(50% - 12px);">
+	<span class="dbminputlabel">Bitrate</span><br>
+	<input id="bitrate" class="round" type="text" placeholder="Leave blank for default!"><br>
+</div>
+<div style="float: right; width: calc(50% - 12px);">
+	<span class="dbminputlabel">User Limit</span><br>
+	<input id="userLimit" class="round" type="text" placeholder="Leave blank for default! Max 10k Limit"><br>
+</div>
+
+<div>
+  <span class="dbminputlabel">Reason</span>
+  <input id="reason" placeholder="Optional" class="round" type="text">
+</div>
+
+<br>
+
+<store-in-variable allowNone selectId="storage" variableInputId="varName" variableContainerId="varNameContainer"></store-in-variable>`;
+  },
+
+  //---------------------------------------------------------------------
+  // Action Editor Init Code
+  //
+  // When the HTML is first applied to the action editor, this code
+  // is also run. This helps add modifications or setup reactionary
+  // functions for the DOM elements.
+  //---------------------------------------------------------------------
+
+  init() {},
+
+  //---------------------------------------------------------------------
+  // Action Bot Function
+  //
+  // This is the function for the action within the Bot's Action class.
+  // Keep in mind event calls won't have access to the "msg" parameter,
+  // so be sure to provide checks for variable existence.
+  //---------------------------------------------------------------------
+
+  action(cache) {
+    const data = cache.actions[cache.index];
+    const server = cache.server;
+    if (!server?.channels) return this.callNextAction(cache);
+    const name = this.evalMessage(data.channelName, cache);
+    const storage = parseInt(data.storage, 10);
+    const reason = this.evalMessage(data.reason, cache);
+    /** @type {import('discord.js').GuildChannelCreateOptions} */
+    const channelData = { reason };
+    if (data.bitrate) {
+      channelData.bitrate = parseInt(this.evalMessage(data.bitrate, cache), 10) * 1000;
+    }
+    if (data.userLimit) {
+      channelData.userLimit = parseInt(this.evalMessage(data.userLimit, cache), 10);
+    }
+    if (data.categoryID) {
+      channelData.parent = this.evalMessage(data.categoryID, cache);
+    }
+    server.channels
+      .create(name, {
+        ...channelData,
+        type: "GUILD_STAGE_VOICE",
+      })
+      .then((channel) => {
+        const varName = this.evalMessage(data.varName, cache);
+        this.storeValue(channel, storage, varName, cache);
+        this.callNextAction(cache);
+      })
+      .catch((err) => this.displayError(data, cache, err));
+  },
+
+  //---------------------------------------------------------------------
+  // Action Bot Mod
+  //
+  // Upon initialization of the bot, this code is run. Using the bot's
+  // DBM namespace, one can add/modify existing functions if necessary.
+  // In order to reduce conflicts between mods, be sure to alias
+  // functions you wish to overwrite.
+  //---------------------------------------------------------------------
+
+  mod() {},
+};
