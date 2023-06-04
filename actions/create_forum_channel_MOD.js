b78508aed0c67eaf7f022edfb6bbabe7636b7dde
OneAndonlyFinbar <finbar@finbar.xyz>
2023-06-04 16:38:17 -0400

    fix directory (#971)
    * Delete action directory
    
    * Create create_forum_channel_MOD.js

--- actions/create_forum_channel_MOD.js
+++ actions/create_forum_channel_MOD.js
@@ -8,19 +8,15 @@ module.exports = {
     authorUrl: 'https://github.com/dbm-network/mods',
     downloadURL: 'https://github.com/dbm-network/mods/blob/master/actions/create_forum_channel_MOD.js',
   },
-
   subtitle(data) {
     return `${data.channelName}`;
   },
-
   variableStorage(data, varType) {
     const type = parseInt(data.storage, 10);
     if (type !== varType) return;
     return [data.varName, 'Channel'];
   },
-
   fields: ['channelName', 'topic', 'position', 'storage', 'varName', 'categoryID', 'slowmodepost', 'reason'],
-
   html() {
     return `
 <div style="height: 350px; overflow-y: scroll; overflow-x: hidden;">
@@ -28,7 +24,6 @@ module.exports = {
     <span class="dbminputlabel">Name</span>
     <input id="channelName" class="round" type="text">
   </div>
-
   <div style="float: left; width: 100%; padding-top: 16px;">
     <span class="dbminputlabel">Guidelines</span>
     <textarea id="topic" rows="3" style="font-family: monospace; white-space: nowrap;"></textarea>
@@ -39,40 +34,31 @@ module.exports = {
       <span class="dbminputlabel">Category ID</span>
       <input id= "categoryID" class="round" type="text" placeholder="Leave blank for no category">
     </div>
-
     <div style="float: right; width: 35%; padding-top: 16px;">
       <span class="dbminputlabel">Position</span>
       <input id="position" class="round" type="text" placeholder="Leave blank for default">
     </div>
   </div>
-
   <div style="float: left; width: 100%; padding-top: 16px;">
     <span class="dbminputlabel">Slowmode</span><br>
     <input id="slowmodepost" class="round" type="text" placeholder="Leave blank to disable">
   </div>
-
   <div style="float: left; width: 100%; padding-top: 16px;">
     <span class="dbminputlabel">Reason</span>
     <input id="reason" placeholder="Optional" class="round" type="text">
   </div>
-
   <div style="float: left; width: 100%; padding-top: 16px;">
     <store-in-variable allowNone dropdownLabel="Store In" selectId="storage" variableContainerId="varNameContainer" variableInputId="varName"></store-in-variable>
   </div>
 </div>
 `;
   },
-
   init() {},
-
   async action(cache) {
     const data = cache.actions[cache.index];
     const { server } = cache;
-
     const name = this.evalMessage(data.channelName, cache);
-
     const channelData = { reason: this.evalMessage(data.reason, cache) };
-
     if (data.topic) {
       channelData.topic = this.evalMessage(data.topic, cache);
     }
@@ -86,7 +72,6 @@ module.exports = {
       channelData.rateLimitPerUser = parseInt(this.evalMessage(data.slowmodepost, cache), 10);
     }
     channelData.type = 15;
-
     server.channels
       .create(name, channelData)
       .then((channel) => {
@@ -97,6 +82,5 @@ module.exports = {
       })
       .catch((err) => this.displayError(data, cache, err));
   },
-
   mod() {},
 };
