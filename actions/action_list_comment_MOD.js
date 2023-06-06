09de6fce4c663cb21b474580318cace09f1b2dd6
TheMonDon <11539895+TheMonDon@users.noreply.github.com>
2023-06-06 18:22:09 -0500

    Fix html

--- actions/action_list_comment_MOD.js
+++ actions/action_list_comment_MOD.js
@@ -17,10 +17,10 @@ module.exports = {
 
   html() {
     return `
-<div style="float: left; width: 99%;">
-  <span class="dbminputlabel">Text Color</span>
-  <input type="color" id="color">
-  <br>
+<div style="float: left;">
+  <span class="dbminputlabel">Text Color</span><br>
+  <input type="color" id="color" style="width: 30%;">
+  <br><br>
   
   <span class="dbminputlabel">Comment To Show: (Supports some HTML Tags)</span>
   <input id="comment" class="round" type="text"><br>
