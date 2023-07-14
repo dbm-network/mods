16a115d217b2fae2e84f777cc0d7f775559e62cc
GoodOne <89701985+GoodOne120@users.noreply.github.com>
2023-07-14 17:02:08 +0300

    Fix invite_used_EVT.js (#992)
    * Fix invite_used_EVT.js
    
    I fix this, because TheMonDon couldn't.

--- events/invite_used_EVT.js
+++ events/invite_used_EVT.js
@@ -1,3 +1,5 @@
+const { Collection } = require('discord.js');
+
 module.exports = {
   name: 'Invite Used',
   isEvent: true,
@@ -7,53 +9,75 @@ module.exports = {
   mod(DBM) {
     DBM.Events = DBM.Events || {};
     const { Bot, Actions } = DBM;
-    const guildInvites = {};
-    DBM.Events.inviteUsed = function inviteUsed(member) {
-      if (!Bot.$evts['Invite Used']) return;
-      const server = member.guild;
-      if (server.me.permissions.has('MANAGE_GUILD')) {
-        server.invites.fetch().then((invites) => {
-          const prior = guildInvites[server.id];
-          const used = prior.filter((c) => c.uses < invites.get(c.code)?.uses).first();
-          if (!used) return;
-          for (const event of Bot.$evts['Invite Used']) {
-            const temp = {};
-            if (event.temp) temp[event.temp] = used.code;
-            if (event.temp2) temp[event.temp2] = used.guild;
-            Actions.invokeEvent(event, server, temp);
-          }
-        });
-      }
-    };
+    const invites = new Collection();
+
     const { onReady } = Bot;
-    Bot.onReady = function inviteUsedOnReady(...params) {
+    Bot.onReady = function inviteUsedOnReady() {
       if (Bot.$evts['Invite Used']) {
         setTimeout(() => {
-          Bot.bot.guilds.cache.forEach((g) => {
-            if (g.me.permissions.has('MANAGE_GUILD')) {
-              g.invites.fetch().then((invites) => {
-                guildInvites[g.id] = invites;
-              });
+          Bot.bot.guilds.cache.forEach(async (guild) => {
+            if (guild.me.permissions.has('MANAGE_GUILD')) {
+              const serverinvites = await guild.invites.fetch();
+              invites.set(guild.id, new Collection(serverinvites.map((invite) => [invite.code, invite.uses])));
             }
           });
         }, 1000).unref();
-        Bot.bot.on('guildMemberAdd', DBM.Events.inviteUsed);
-        Bot.bot.on('inviteDelete', (inv) => {
-          if (inv.guild.members.me.permissions.has('MANAGE_GUILD')) {
-            inv.guild.invites.fetch().then((invites) => {
-              guildInvites[inv.guild.id] = invites;
+
+        Bot.bot.on('guildMemberAdd', async (member) => {
+          if (member.guild.me.permissions.has('MANAGE_GUILD')) {
+            const newinvites = await member.guild.invites.fetch();
+            const oldinvites = invites.get(member.guild.id);
+            const invite = newinvites.find((i) => i.uses > oldinvites.get(i.code));
+            for (const event of Bot.$evts['Invite Used']) {
+              const temp = {};
+              if (event.temp) temp[event.temp] = invite.code;
+              if (event.temp2) temp[event.temp2] = invite.guild;
+              Actions.invokeEvent(event, member.guild, temp);
+            }
+          }
+        });
+
+        Bot.bot.on('inviteDelete', (invite) => {
+          if (invite.guild.members.me.permissions.has('MANAGE_GUILD')) {
+            invites.get(invite.guild.id).delete(invite.code);
+          }
+        });
+
+        Bot.bot.on('inviteCreate', (invite) => {
+          if (invite.guild.members.me.permissions.has('MANAGE_GUILD')) {
+            invites.get(invite.guild.id).set(invite.code, invite.uses);
+          }
+        });
+
+        Bot.bot.on('guildCreate', (guild) => {
+          if (guild.members.me.permissions.has('MANAGE_GUILD')) {
+            guild.invites.fetch().then((guildInvites) => {
+              invites.set(guild.id, new Map(guildInvites.map((invite) => [invite.code, invite.uses])));
             });
           }
         });
-        Bot.bot.on('inviteCreate', (inv) => {
-          if (inv.guild.members.me.permissions.has('MANAGE_GUILD')) {
-            inv.guild.invites.fetch().then((invites) => {
-              guildInvites[inv.guild.id] = invites;
-            });
+
+        Bot.bot.on('guildDelete', (guild) => {
+          invites.delete(guild.id);
+        });
+
+        Bot.bot.on('roleUpdate', (oldrole, newrole) => {
+          if (!newrole.tags || !newrole.tags.botId) return;
+          if (newrole.tags.botId === Bot.bot.user.id) {
+            const oldroleperms = oldrole.permissions.has('MANAGE_GUILD');
+            const newroleperms = newrole.permissions.has('MANAGE_GUILD');
+            if (!oldroleperms && newroleperms) {
+              newrole.guild.invites.fetch().then((guildInvites) => {
+                invites.set(newrole.guild.id, new Map(guildInvites.map((invite) => [invite.code, invite.uses])));
+              });
+            } else if (oldroleperms && !newroleperms) {
+              invites.delete(newrole.guild.id);
+            }
           }
         });
       }
-      onReady.apply(this, ...params);
+
+      onReady.apply(this);
     };
   },
 };
