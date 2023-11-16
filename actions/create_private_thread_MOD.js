43bc26000d7a0ad81d6082471d08c60596d8d596
Phyrok <69722956+Phyrok@users.noreply.github.com>
2023-11-16 16:45:16 +0100

    Update create_private_thread_MOD.js

--- actions/create_private_thread_MOD.js
+++ actions/create_private_thread_MOD.js
@@ -1,85 +1,63 @@
 module.exports = {
-  name: "Create Private Thread",
-  section: "Channel Control",
-  meta: {
-    version: '2.1.7',
+    name: "Add Member to Thread MOD",
+    section: "Channel Control",
+    meta: {
+        version: '2.1.7',
     preciseCheck: true,
     author: 'DBM Mods',
     authorUrl: 'https://github.com/dbm-network/mods',
-    downloadURL: 'https://github.com/dbm-network/mods/blob/master/actions/create_private_thread_MOD.js',
-  },
+    downloadURL: 'https://github.com/dbm-network/mods/blob/master/actions/add_member_to_thread_MOD.js',
+    },
 
-  fields: ["channel", "channelVarName", "threadName", "autoArchiveDuration", "reason", "storage", "storageVarName"],
-  subtitle(data, presets) {
-    return `Create Private Thread In ${presets.getChannelText(data.channel ?? 0, data.channelVarName)}`;
-  },
+    subtitle(data, presets) {
 
-  variableStorage(data, varType) {
-    const type = parseInt(data.storage, 10);
-    if (type !== varType) return;
-    return [data.storageVarName, "Canal"];
-  },
+        if(data.descriptionx == true){
+          desccor = data.descriptioncolor
+          } else {
+            desccor = 'none'
+          }
+    
+        const storeTypes = presets.variables;
 
-  html(isEvent, data) {
-    return `
-  <channel-input dropdownLabel="Source Channel" selectId="channel" variableContainerId="varNameContainerChannel" variableInputId="channelVarName"></channel-input>
-  
-  <br><br><br><br>
-  
-  <div style="float: left; width: calc(50% - 12px);">
-  
-    <span class="dbminputlabel">Thread Name</span><br>
-    <input id="threadName" class="round" type="text"><br>
-  
-  </div>
-  <div style="float: right; width: calc(50% - 12px);">
-  
-    <span class="dbminputlabel">Auto-Archive Duration</span><br>
-    <select id="autoArchiveDuration" class="round">
-      <option value="60" selected>1 Hour</option>
-      <option value="1440">24 Hours</option>
-      <option value="4320">3 Days</option>
-      <option value="10080">1 Week</option>
-      <option value="max">Maximum</option>
-    </select><br>
-  
-  </div>
-  
-  <br><br><br><br>
-  
-  <hr class="subtlebar" style="margin-top: 0px;">
-  
-  <br>
-  
-  <div>
-    <span class="dbminputlabel">Reason</span>
-    <input id="reason" placeholder="Opcional" class="round" type="text">
-  </div>
-  
-  <br>
-  
-  <store-in-variable allowNone selectId="storage" variableInputId="storageVarName" variableContainerId="varNameContainer2"></store-in-variable>`;
-  },
+        return data.description
+        ? `<font style="color:${desccor}">${data.description}</font>`
+        : `<font style="color:${desccor}">${storeTypes[parseInt(data.storage, 10)]} (${data.varName})</font>`
+      },
 
-  init() { },
+    fields: ["storage", "varName", "member", "varName2", "iffalse", "iffalseVal","descriptioncolor","description","descriptionx"],
 
-  async action(cache) {
-    const data = cache.actions[cache.index];
-    const channel = await this.getChannelFromData(data.channel, data.channelVarName, cache);
 
-    const thread = await channel.threads.create({
-      name: this.evalMessage(data.threadName, cache),
-      autoArchiveDuration: data.autoArchiveDuration === "max" ? 10080 : parseInt(data.autoArchiveDuration, 10),
-      type: "PrivateThread",
-      reason: this.evalMessage(data.reason, cache) || null
-    });
+    html(isEvent, data) {
+        return `
+      
+        <div style="width: 100%; padding:5px 5px;height: calc(100vh - 160px);overflow:auto">
 
-    const storage = parseInt(data.storage, 10);
-    const storageVarName = this.evalMessage(data.storageVarName, cache);
-    this.storeValue(thread, storage, storageVarName, cache);
+      <retrieve-from-variable dropdownLabel="Source Thread" selectId="storage" variableContainerId="varNameContainer" variableInputId="varName"></retrieve-from-variable>
+      
+      <br><br><br><br>
 
-    this.callNextAction(cache);
-  },
+      <member-input dropdownLabel="Member" selectId="member" variableContainerId="varNameContainer2" variableInputId="varName2"></member-input>
 
-  mod() { },
+<style>
+
+</style>
+    `;
+    },
+
+    async action(cache) {
+        const data = cache.actions[cache.index];
+        const storage = parseInt(data.storage, 10);
+        const varName = this.evalMessage(data.varName, cache);
+        const th = this.getVariable(storage, varName, cache);
+        const member = await this.getMemberFromData(data.member, data.varName2, cache);
+
+        try { 
+            await th.members.add(member.id);
+            this.callNextAction(cache);
+        } catch {
+            this.executeResults(false, data, cache);
+        }
+    },
+
+    mod() { },
 };
