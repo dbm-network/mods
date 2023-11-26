b29e31d71e55843c6b0e95a187e33451335acbe0
Phyrok <phyrok0001@gmail.com>
2023-11-26 03:14:26 +0100

    Update & lint
    Update the action to use "store-in-variable" instead as to be similar to other mods.
    
    lint has been run, file has been adjusted.

--- actions/store_unix_timestamp_MOD.js
+++ actions/store_unix_timestamp_MOD.js
@@ -1,53 +1,50 @@
 module.exports = {
-    name: "Store Unix Timestamp",
-    section: "Other Stuff",
-    meta: {
-        version: "2.1.7",
-        preciseCheck: true,
-        author: 'DBM Mods',
-        authorUrl: 'https://github.com/dbm-network/mods',
-        downloadUrl: 'https://github.com/dbm-network/mods/blob/master/actions/store_unix_timestamp_MOD.js'
-    },
+  name: 'Store Unix Timestamp',
+  section: 'Other Stuff',
+  meta: {
+    version: '2.1.7',
+    preciseCheck: true,
+    author: 'DBM Mods',
+    authorUrl: 'https://github.com/dbm-network/mods',
+    downloadUrl: 'https://github.com/dbm-network/mods/blob/master/actions/store_unix_timestamp_MOD.js',
+  },
 
-    subtitle() {
-        return `Current Unix Timestamp`;
-    },
+  subtitle() {
+    return `Current Unix Timestamp`;
+  },
 
-    fields: ["type", "varName"],
+  fields: ['storage', 'varName'],
 
-    html() {
-        return `
+  html() {
+    return `
 <div style="padding-top: 8px;">
-    <span class="dbminputlabel">Time Info</span>
-    <input value="Current Timestamp" class="round" disabled></input>
+  <span class="dbminputlabel">Time Info</span>
+  <input value="Current Timestamp" class="round" disabled></input>
 </div>
 
 <br>
 
-<div style="float: left; padding-top: 8px; width: 30%;">
-    <span class="dbminputlabel">Store In</span>
-    <select id="type" class="round">
-        <option value="1" selected>Temp Variable</option>
-        <option value="2">Server Variable</option>
-        <option value="3">Global Variable</option>
-    </select>
-</div>
-
-<div style="float: right; padding-top: 8px; width: 65%">
-    <span class="dbminputlabel">Variable Name</span>
-    <input id="varName" class="round"></input>
+<div>
+  <store-in-variable dropdownLabel="Store In" selectId="storage" variableContainerId="varNameContainer" variableInputId="varName"></store-in-variable>
 </div>
 `;
-    },
+  },
 
-    init() { },
+  init() {
+    const { glob, document } = this;
+    glob.variableChange(document.getElementById('storage'), 'varNameContainer');
+  },
 
-    action(cache) {
-        const data = cache.actions[cache.index];
-        const result = Math.floor(new Date().getTime() / 1000);
-        this.storeValue(result, data.type.value, data.varName, cache);
-        this.callNextAction(cache);
-    },
+  action(cache) {
+    const data = cache.actions[cache.index];
+    const result = Math.floor(new Date().getTime() / 1000);
 
-    mod() { },
+    const storage = parseInt(data.storage, 10);
+    const varName = this.evalMessage(data.varName, cache);
+
+    this.storeValue(result, storage, varName, cache);
+    this.callNextAction(cache);
+  },
+
+  mod() {},
 };
