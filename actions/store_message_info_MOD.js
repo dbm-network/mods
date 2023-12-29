c9bc9f1dbaf14e552efb6eb7ae504a2277a17024
Finbar <finbar@finbar.xyz>
2023-12-28 19:00:43 -0500

    lint/prettier fix

--- actions/store_message_info_MOD.js
+++ actions/store_message_info_MOD.js
@@ -1,137 +1,137 @@
 module.exports = {
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
   // Action Name
   //
   // This is the name of the action displayed in the editor.
-  //---------------------------------------------------------------------
-  name: "Store Message Info",
+  // ---------------------------------------------------------------------
+  name: 'Store Message Info',
 
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
   // Action Section
   //
   // This is the section the action will fall into.
-  //---------------------------------------------------------------------
-  section: "Messaging",
+  // ---------------------------------------------------------------------
+  section: 'Messaging',
 
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
   // Action Subtitle
   //
   // This function generates the subtitle displayed next to the name.
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
   subtitle(data, presets) {
     const info = [
-      "Message Object",
-      "Message ID",
-      "Message Text",
-      "Message Author",
-      "Message Channel",
-      "Message Timestamp",
-      "Message is Pinned",
-      "Message is TTS",
-      "Message Attachments List",
-      "Message Edits",
-      "",
-      "",
-      "Messages Reactions Count",
-      "Mentioned Users List",
-      "Mentioned Users Count",
-      "Message URL",
-      "Message Creation Date",
-      "Message Content Length",
-      "Message Attachments Count",
-      "Message Guild",
-      "Message Type",
-      "Message Webhook ID",
-      "Message Embed Object",
-      "Message Reference Object",
-      "Replied-to Message ID",
-      "Replied-to Message Channel ID",
-      "Replied-to Message Guild ID"
+      'Message Object',
+      'Message ID',
+      'Message Text',
+      'Message Author',
+      'Message Channel',
+      'Message Timestamp',
+      'Message is Pinned',
+      'Message is TTS',
+      'Message Attachments List',
+      'Message Edits',
+      '',
+      '',
+      'Messages Reactions Count',
+      'Mentioned Users List',
+      'Mentioned Users Count',
+      'Message URL',
+      'Message Creation Date',
+      'Message Content Length',
+      'Message Attachments Count',
+      'Message Guild',
+      'Message Type',
+      'Message Webhook ID',
+      'Message Embed Object',
+      'Message Reference Object',
+      'Replied-to Message ID',
+      'Replied-to Message Channel ID',
+      'Replied-to Message Guild ID',
     ];
     return `${presets.getMessageText(data.message, data.varName)} - ${info[parseInt(data.info, 10)]}`;
   },
 
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
   // Action Storage Function
   //
   // Stores the relevant variable info for the editor.
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
   variableStorage(data, varType) {
     const type = parseInt(data.storage, 10);
     if (type !== varType) return;
     const info = parseInt(data.info, 10);
-    let dataType = "Unknown Type";
+    let dataType = 'Unknown Type';
     switch (info) {
       case 0:
-        dataType = "Message";
+        dataType = 'Message';
         break;
       case 1:
-        dataType = "Message ID";
+        dataType = 'Message ID';
         break;
       case 2:
-        dataType = "Text";
+        dataType = 'Text';
         break;
       case 3:
-        dataType = "Server Member";
+        dataType = 'Server Member';
         break;
       case 4:
-        dataType = "Channel";
+        dataType = 'Channel';
         break;
       case 5:
-        dataType = "Text";
+        dataType = 'Text';
         break;
       case 6:
       case 7:
-        dataType = "Boolean";
+        dataType = 'Boolean';
         break;
       case 8:
-        dataType = "Date";
+        dataType = 'Date';
         break;
       case 9:
-        dataType = "Messages List";
+        dataType = 'Messages List';
         break;
       case 12:
-        dataType = "Number";
+        dataType = 'Number';
         break;
       case 13:
-        dataType = "Array";
+        dataType = 'Array';
         break;
       case 14:
-        dataType = "Number";
+        dataType = 'Number';
         break;
       case 15:
-        dataType = "URL";
+        dataType = 'URL';
         break;
       case 16:
-        dataType = "Date";
+        dataType = 'Date';
         break;
       case 17:
       case 18:
-        dataType = "Number";
+        dataType = 'Number';
         break;
       case 19:
-        dataType = "Guild";
+        dataType = 'Guild';
         break;
       case 20:
-        dataType = "Message Type";
+        dataType = 'Message Type';
         break;
       case 21:
-        dataType = "Webhook ID";
+        dataType = 'Webhook ID';
         break;
       case 22:
-        dataType = "Embed Message";
+        dataType = 'Embed Message';
         break;
       case 23:
-        dataType = "Message Reference Object";
+        dataType = 'Message Reference Object';
         break;
       case 24:
-        dataType = "Message ID";
+        dataType = 'Message ID';
         break;
       case 25:
-        dataType = "Channel ID";
+        dataType = 'Channel ID';
         break;
       case 26:
-        dataType = "Guild ID";
+        dataType = 'Guild ID';
         break;
       default:
         break;
@@ -139,7 +139,7 @@ module.exports = {
     return [data.varName2, dataType];
   },
 
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
   // Action Meta Data
   //
   // Helps check for updates and provides info if a custom mod.
@@ -147,20 +147,20 @@ module.exports = {
   //
   // It's highly recommended "preciseCheck" is set to false for third-party mods.
   // This will make it so the patch version (0.0.X) is not checked.
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
 
-  meta: { version: "2.1.7", preciseCheck: true, author: null, authorUrl: null, downloadUrl: null },
+  meta: { version: '2.1.7', preciseCheck: true, author: null, authorUrl: null, downloadUrl: null },
 
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
   // Action Fields
   //
   // These are the fields for the action. These fields are customized
   // by creating elements with corresponding IDs in the HTML. These
   // are also the names of the fields stored in the action's JSON data.
-  //---------------------------------------------------------------------
-  fields: ["message", "varName", "info", "storage", "varName2"],
+  // ---------------------------------------------------------------------
+  fields: ['message', 'varName', 'info', 'storage', 'varName2'],
 
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
   // Command HTML
   //
   // This function returns a string containing the HTML used for
@@ -169,8 +169,8 @@ module.exports = {
   // The "isEvent" parameter will be true if this action is being used
   // for an event. Due to their nature, events lack certain information,
   // so edit the HTML to reflect this.
-  //---------------------------------------------------------------------
-  html(isEvent, data) {
+  // ---------------------------------------------------------------------
+  html() {
     return `
 <message-input dropdownLabel="Source Message" selectId="message" variableContainerId="varNameContainer" variableInputId="varName"></message-input>
 
@@ -212,22 +212,22 @@ module.exports = {
 <store-in-variable dropdownLabel="Store In" selectId="storage" variableContainerId="varNameContainer2" variableInputId="varName2"></store-in-variable>`;
   },
 
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
   // Action Editor Init Code
   //
   // When the HTML is first applied to the action editor, this code
   // is also run. This helps add modifications or setup reactionary
   // functions for the DOM elements.
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
   init() {},
 
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
   // Action Bot Function
   //
   // This is the function for the action within the Bot's Action class.
   // Keep in mind event calls won't have access to the "msg" parameter,
   // so be sure to provide checks for variable existence.
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
   async action(cache) {
     const data = cache.actions[cache.index];
     const msg = await this.getMessageFromData(data.message, data.varName, cache);
@@ -327,13 +327,13 @@ module.exports = {
     this.callNextAction(cache);
   },
 
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
   // Action Bot Mod
   //
   // Upon initialization of the bot, this code is run. Using the bot's
   // DBM namespace, one can add/modify existing functions if necessary.
   // In order to reduce conflicts between mods, be sure to alias
   // functions you wish to overwrite.
-  //---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
   mod() {},
 };
